1422ff9aedee978369d159ca66d8b808604d68ba
sangwook <sangwook>
2012-02-24 14:59:26 +0000

    the boost build script file is updated.

--- sw_dev/cpp/ext/src/boost/boost_1_48_0_build.sh
+++ sw_dev/cpp/ext/src/boost/boost_1_48_0_build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# bjam {stage/install/clean} debug release --toolset=msvc-8.0 optimization={off/full/space/speed} link=static,shared threading=single,multi runtime-link=single,shared --build-type=complete
+# bjam {stage/install/clean} debug release --toolset=gcc optimization={off/full/space/speed} link=static,shared threading=single,multi runtime-link=single,shared --build-type=complete
 
-# bjam stage --toolset=msvc link=static,shared --with-thread --with-regex --with-python
-# bjam stage --toolset=msvc link=static,shared --without-python --without-mpi
-# bjam stage -sICU_PATH=${ICU_ROOT} --toolset=msvc link=static,shared
+# bjam stage --toolset=gcc link=static,shared --with-thread --with-regex --with-python
+# bjam stage --toolset=gcc link=static,shared --without-python --without-mpi
+# bjam stage -sICU_PATH=${ICU_ROOT} --toolset=gcc link=static,shared
 
 ./bjam stage release --toolset=gcc --without-mpi link=static,shared
 ./bjam stage debug --toolset=gcc --without-mpi link=static,shared
 
-# ./bjam install --prefix=/bin/local
+# sudo ./bjam install --prefix=/bin/local --toolset=gcc --without-mpi
 # ./bjam --clean debug release
 
 # ${BOOST_ROOT}/tools/build/v2/user-config.jam
