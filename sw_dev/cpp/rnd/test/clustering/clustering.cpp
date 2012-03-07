cab3a32a69e49cc5968775e731ed8bff02d32ec7
sangwook <sangwook>
2012-03-07 17:46:51 +0000

    clustering project is added in sw_dev/rnd/test.

---
+++ sw_dev/cpp/rnd/test/clustering/clustering.cpp
@@ -0,0 +1,38 @@
+//#include "stdafx.h"
+#if defined(WIN32)
+#include <vld/vld.h>
+#endif
+#include <iostream>
+#include <ctime>
+
+#if defined(UNICODE) || defined(_UNICODE)
+int wmain(int argc, wchar_t **argv)
+#else
+int main(int argc, char **argv)
+#endif
+{
+	void k_means();
+	void k_medoids();
+
+	try
+	{
+		std::srand((unsigned int)time(NULL));
+
+		k_means();
+		k_medoids();
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "exception occurred !!!: " << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::wcout << L"unknown exception occurred !!!" << std::endl;
+	}
+
+	std::wcout << L"press any key to exit ..." << std::endl;
+	std::wcout.flush();
+	std::wcin.get();
+
+	return 0;
+}
---
+++ sw_dev/cpp/rnd/test/clustering/k_means.cpp
@@ -0,0 +1,21 @@
+//#include "stdafx.h"
+#include <stdexcept>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+namespace {
+namespace local {
+
+}  // namespace local
+}  // unnamed namespace
+
+void k_means()
+{
+	throw std::runtime_error("not yet implemented");
+}
