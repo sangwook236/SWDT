ad269c78d4b196c06cd800dc851335174674f095
sangwook <sangwook>
2012-05-04 06:52:14 +0000

    a file, von_mises_pdf_test.m is changed.

--- sw_dev/matlab/rnd/test/directional_statistics/von_mises_pdf_test.m
+++ sw_dev/matlab/rnd/test/directional_statistics/von_mises_pdf_test.m
@@ -2,7 +2,7 @@
 
 mean_dir = 0.0;
 
-kappa = 100.0;
+kappa = 30.0;
 %sigma = besseli(1, kappa) / besseli(0, kappa);  % as kappa -> inf
 sigma = 1 / sqrt(kappa);
 %scalefactor = 1;
@@ -13,13 +13,13 @@ scalefactor = 1.05 * von_mises_pdf(pi, mean_dir, kappa) / normpdf(pi, mean_dir, 
 %scalefactor = 1.05 * von_mises_pdf(pi, mean_dir, kappa) / normpdf(pi, mean_dir, sigma);
 
 figure;
-dir1 = linspace(-pi, pi, 1000);
+dir1 = linspace(-pi, pi, 10000);
 vonmisespdf1 = von_mises_pdf(dir1, mean_dir, kappa);
 normpdf1 = normpdf(dir1, mean_dir, sigma) * scalefactor;
 plot(dir1, vonmisespdf1, 'r', dir1, normpdf1, 'b');
  
 figure;
-dir2 = linspace(0, 2 * pi, 1000);
+dir2 = linspace(0, 2 * pi, 10000);
 vonmisespdf2 = von_mises_pdf(dir2, mean_dir, kappa);
 normpdf2 = normpdf(dir2, mean_dir, sigma) * scalefactor;
 plot(dir2, vonmisespdf2, 'r', dir2, normpdf2, 'b');
