86b431fa72a8157624517e8b33b4352db6a90d72
sangwook <sangwook>
2012-05-30 09:37:13 +0000

    a test project for Thread Build Blocks (TBB) library is added.

---
+++ sw_dev/cpp/ext/test/tbb/complex_loop_parallelization.cpp
@@ -0,0 +1,18 @@
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+
+namespace {
+namespace local {
+
+}  // namespace local
+}  // unnamed namespace
+
+void complex_loop_parallelization()
+{
+	throw std::runtime_error("not yet implemented");
+}
