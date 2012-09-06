2abfe4e1c1e276b09286b9d50b37af75ecd1821e
sangwook <sangwook>
2012-09-06 08:55:22 +0000

    Encoding to handle strings is added.

---
+++ sw_dev/csharp/basic/test/CSharpBasic/Encoding.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpBasic
+{
+    class Encoding
+    {
+        public static void run()
+        {
+            string str = "한글 변환 123 ABC abc";
+            {
+                //System.Text.Encoding encoding = new System.Text.ASCIIEncoding();
+                System.Text.Encoding encoding = System.Text.Encoding.ASCII;
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("ASCII : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                //System.Text.Encoding encoding = new System.Text.UTF7Encoding();
+                System.Text.Encoding encoding = System.Text.Encoding.UTF7;
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("UTF7 : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                //System.Text.Encoding encoding = new System.Text.UTF8Encoding();
+                System.Text.Encoding encoding = System.Text.Encoding.UTF8;
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("UTF8 : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                //System.Text.Encoding encoding = new System.Text.UnicodeEncoding();
+                System.Text.Encoding encoding = System.Text.Encoding.Unicode;
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("UTF16 : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                //System.Text.Encoding encoding = new System.Text.UTF32Encoding();
+                System.Text.Encoding encoding = System.Text.Encoding.UTF32;
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("UTF32 : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                // [ref] http://msdn.microsoft.com/en-us/library/system.text.encoding.issinglebyte
+
+                System.Text.Encoding encoding = System.Text.Encoding.GetEncoding(949);
+                //System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("ks_c_5601-1987");
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("ks_c_5601-1987 : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                // [ref] http://msdn.microsoft.com/en-us/library/system.text.encoding.issinglebyte
+
+                System.Text.Encoding encoding = System.Text.Encoding.GetEncoding(51949);
+                //System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("euc-kr");
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("euc-kr : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                // [ref] http://msdn.microsoft.com/en-us/library/system.text.encoding.issinglebyte
+
+                System.Text.Encoding encoding = System.Text.Encoding.GetEncoding(50225);
+                //System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("iso-2022-kr");
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("iso-2022-kr : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                // [ref] http://msdn.microsoft.com/en-us/library/system.text.encoding.issinglebyte
+
+                System.Text.Encoding encoding = System.Text.Encoding.GetEncoding(20833);
+                //System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("x-EBCDIC-KoreanExtended");
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("x-EBCDIC-KoreanExtended : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+
+            {
+                // [ref] http://msdn.microsoft.com/en-us/library/system.text.encoding.issinglebyte
+
+                System.Text.Encoding encoding = System.Text.Encoding.GetEncoding(10003);
+                //System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("x-mac-korean");
+                byte[] bytes = encoding.GetBytes(str);
+                Console.Write("x-mac-korean : Len = {0} : ", bytes.Length);
+                foreach (byte item in bytes)
+                    Console.Write("{0:X02} ", item);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ sw_dev/csharp/basic/test/CSharpBasic/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // data formatting
+                Console.WriteLine("Data Formatting -------------------------------------------------------------");
+                runDataFormatting();
+
+                // date & time
+                Console.WriteLine("\nDate & Time -----------------------------------------------------------------");
+                runDateTime();
+
+                // encoding
+                Console.WriteLine("\nEncoding --------------------------------------------------------------------");
+                Encoding.run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("System.Exception occurred: {0}", e);
+            }
+
+            Console.WriteLine("press any key to exit ...");
+            Console.ReadKey(true);
+        }
+
+        static void runDataFormatting()
+        {
+            int hex = 0x3E5;
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendFormat("{0:X}", (hex & 0xF));
+            Console.WriteLine(builder.ToString());
+
+            builder.Clear();
+            builder.AppendFormat("{0:X2}", (hex & 0xF));
+            Console.WriteLine(builder.ToString());
+
+            builder.Clear();
+            builder.AppendFormat("{0:X02}", (hex & 0xF));
+            Console.WriteLine(builder.ToString());
+
+            builder.Clear();
+            builder.AppendFormat("{0:X02}", (hex & 0xFF));
+            Console.WriteLine(builder.ToString());
+
+            Console.WriteLine();
+
+            //
+            int dec = 654321;
+
+            builder.Clear();
+            builder.AppendFormat("{0:D01}", (dec % 10));
+            Console.WriteLine(builder.ToString());
+
+            builder.Clear();
+            builder.AppendFormat("{0:D02}", (dec % 100));
+            Console.WriteLine(builder.ToString());
+
+            builder.Clear();
+            builder.AppendFormat("{0:D03}", (dec % 1000));
+            Console.WriteLine(builder.ToString());
+
+            builder.Clear();
+            builder.AppendFormat("{0:D04}", (dec % 10000));
+            Console.WriteLine(builder.ToString());
+        }
+
+        static void runDateTime()
+        {
+            DateTime timestamp;
+            //bool ret = DateTime.TryParse("2012-08-21 오후 4:45:37", out eventTimestamp);
+            bool ret = DateTime.TryParse("2012-08-21 4:45:37 PM", out timestamp);
+
+            Console.WriteLine("Timestamp: {0}", timestamp);
+            Console.WriteLine("Now: {0}", DateTime.Now);
+        }
+    }
+}
