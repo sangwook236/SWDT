7827d7f7bb544fdd80ad5b3da3ebc9f0e2a4738c
sangwook <sangwook>
2012-12-01 09:13:48 +0000

    JavaCPP is updated.

--- sw_dev/java/ext/test/javacpp/src/LegacyLibrary.java
+++ sw_dev/java/ext/test/javacpp/src/LegacyLibrary.java
@@ -1,7 +1,52 @@
 import com.googlecode.javacpp.*;
 import com.googlecode.javacpp.annotation.*;
 
-@Platform(include="LegacyLibrary.h")
+@Platform(
+	// for linking static libraries
+	include = {
+	    "LegacyLibrary.h"
+	},
+	includepath={
+		"../LegacyLibrary_static/",
+		//"D:/work_center/sw_dev/java/ext/test/javacpp/LegacyLibrary_static/",
+		"D:/work_center/sw_dev/cpp/ext/inc",
+		"D:/work_center/sw_dev/cpp/rnd/inc",
+		"C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Include"
+	},
+	link={
+		"LegacyLibrary"
+	},
+	linkpath = {
+		"../LegacyLibrary_static/x64/Release/",
+		//"D:/work_center/sw_dev/java/ext/test/javacpp/src",
+		"D:/work_center/sw_dev/java/ext/test/javacpp/LegacyLibrary_static/x64/Release",
+		"D:/work_center/sw_dev/cpp/ext/lib",
+		"D:/work_center/sw_dev/cpp/rnd/lib"
+	}
+
+/*
+	// for linking shared libraries
+	// shared library files exist in the same directory as a java source file
+	include = {
+	    "LegacyLibrary.h"
+	},
+	includepath={
+		"../LegacyLibrary_shared/",
+		"D:/work_center/sw_dev/cpp/ext/inc",
+		"D:/work_center/sw_dev/cpp/rnd/inc",
+		"C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Include"
+	},
+	link={
+		"LegacyLibrary"
+	},
+	linkpath = {
+		"../LegacyLibrary_shared/x64/Release/",
+		"D:/work_center/sw_dev/cpp/ext/lib",
+		"D:/work_center/sw_dev/cpp/rnd/lib"
+	}
+*/
+)
+
 @Namespace("LegacyLibrary")
 public class LegacyLibrary {
     public static class LegacyClass extends Pointer {
@@ -19,6 +64,15 @@ public class LegacyLibrary {
     }
 
     public static void main(String[] args) {
+    	java.io.File dir1 = new java.io.File(".");
+    	java.io.File dir2 = new java.io.File("..");
+        try {
+            System.out.println("Current dir : " + dir1.getCanonicalPath());
+            System.out.println("Parent  dir : " + dir2.getCanonicalPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         // Pointer objects allocated in Java get deallocated once they become unreachable,
         // but C++ destructors can still be called in a timely fashion with Pointer.deallocate()
         LegacyClass l = new LegacyClass();
