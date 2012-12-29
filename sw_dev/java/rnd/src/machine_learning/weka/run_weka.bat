b639ec877bb5a63173ccc3e051bf5a579bf038d7
sangwook <sangwook>
2012-12-29 08:38:59 +0000

    jQuery Mobile library is added: sw_dev/mobile/ext/src/jquery_mobile.

--- sw_dev/java/rnd/src/machine_learning/weka/run_weka.bat
+++ sw_dev/java/rnd/src/machine_learning/weka/run_weka.bat
@@ -1,11 +1,18 @@
 @echo off
 setlocal
 
-set JAVA_HOME=C:\Program Files\Java\jre7
-rem set WEKA_HOME=D:\work_center\sw_dev\java\rnd\src\machine_learning\weka\weka-3-6-8
+rem export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+rem set JAVA_HOME=C:\Program Files\Java\jre7
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_04
+
+rem export WEKA_HOME=/work_center/sw_dev/java/\rnd/src/machine_learning/weka/weka-3-7-7
 set WEKA_HOME=D:\work_center\sw_dev\java\rnd\src\machine_learning\weka\weka-3-7-7
 
-java -Xmx1000M -jar %WEKA_HOME%\weka.jar
+rem export HEAP_OPTION=-Xmx1000M
+set HEAP_OPTION=-Xmx1000M
+
+rem java $HEAP_OPTION -jar $WEKA_HOME\weka.jar
+java %HEAP_OPTION% -jar %WEKA_HOME%\weka.jar
 
 endlocal
 echo on
