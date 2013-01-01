2110a95eac12f5aa5e44e4ed5d9193ad93659ccb
sangwook <sangwook>
2013-01-01 15:35:07 +0000

    batch files to run java applications are updated.

--- sw_dev/java/rnd/src/machine_learning/rapid_i/run_rapidminer.bat
+++ sw_dev/java/rnd/src/machine_learning/rapid_i/run_rapidminer.bat
@@ -1,11 +1,29 @@
 @echo off
 setlocal
 
-set JAVA_HOME=C:\Program Files\Java\jre7
+rem export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+rem set JAVA_HOME=C:\Program Files\Java\jre7
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_04
+
+rem export LIB_PATH1=lib
+set LIB_PATH1=D:\work_center\sw_dev\java\ext\lib
+
+rem export CLASSPATH=.:$LIB_PATH1/runnable_jar.jar:$CLASSPATH
+rem set CLASSPATH=.;%LIB_PATH1%\runnable_jar.jar;%CLASSPATH%
+
+rem export MAX_JAVA_MEMORY=800
 set MAX_JAVA_MEMORY=800
+
+rem export HEAP_OPTION=-Xms4096m -Xmx8192m
+rem set HEAP_OPTION=-Xms4096m -Xmx8192m
+
+rem export RAPIDMINER_HOME=/usr/lib/Rapid-I/RapidMiner5
+rem export RAPIDMINER_HOME=/home/sangwook/work_center/sw_dev/java/rnd/src/machine_learning/rapid_i/rapidminer-5.2.008/rapidminer
+rem set RAPIDMINER_HOME=D:\MyProgramFiles\Rapid-I\RapidMiner5
 set RAPIDMINER_HOME=D:\work_center\sw_dev\java\rnd\src\machine_learning\rapid_i\rapidminer-5.2.008\rapidminer
 
-java -Drapidminer.home=D:\MyProgramFiles\RapidMiner-5.2.008 -jar %RAPIDMINER_HOME%\lib\rapidminer.jar
+rem java $HEAP_OPTION -Drapidminer.home=$RAPIDMINER_HOME -jar $RAPIDMINER_HOME/lib/rapidminer.jar
+java %HEAP_OPTION% -Drapidminer.home=%RAPIDMINER_HOME% -jar %RAPIDMINER_HOME%\lib\rapidminer.jar
 
 endlocal
 echo on
--- sw_dev/java/run_jar.bat
+++ sw_dev/java/run_jar.bat
@@ -5,26 +5,31 @@ rem export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 rem set JAVA_HOME=C:\Program Files\Java\jre7
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_04
 
-rem export LIB_PATH1=lib
-set LIB_PATH1=D:\work_center\sw_dev\java\ext\lib
+rem export LIB_PATH1=/usr/local/lib
+set LIB_PATH1=D:\work_center\sw_dev\java
 
 rem export CLASSPATH=.:$LIB_PATH1/runnable_jar.jar:$CLASSPATH
 rem set CLASSPATH=.;%LIB_PATH1%\runnable_jar.jar;%CLASSPATH%
 
+rem export MAX_JAVA_MEMORY=800
+rem set MAX_JAVA_MEMORY=800
+
 rem export HEAP_OPTION=-Xms4096m -Xmx8192m
 rem set HEAP_OPTION=-Xms4096m -Xmx8192m
 
-rem error
+rem java %HEAP_OPTION% -classpath %LIB_PATH1%\nonrunnable_jar.jar java_jar.Hello
+rem java %HEAP_OPTION% -classpath %LIB_PATH1%\nonrunnable_jar.jar java_jar.Hi
+rem java %HEAP_OPTION% -classpath %LIB_PATH1%\runnable_jar.jar java_jar.Hello
+rem java %HEAP_OPTION% -classpath %LIB_PATH1%\runnable_jar.jar java_jar.Hi
+
+rem java $HEAP_OPTION -jar $LIB_PATH1/runnable_jar.jar java_jar.Hello
+rem java $HEAP_OPTION -jar $LIB_PATH1/runnable_jar.jar java_jar.Hi
+rem java %HEAP_OPTION% -jar %LIB_PATH1%\runnable_jar.jar java_jar.Hello
+java %HEAP_OPTION% -jar %LIB_PATH1%\runnable_jar.jar java_jar.Hi
+
+rem << error >>
 rem java $HEAP_OPTION -jar $LIB_PATH/nonrunnable_jar.jar
 rem java %HEAP_OPTION% -jar %LIB_PATH%\nonrunnable_jar.jar
-
-rem java $HEAP_OPTION -jar $LIB_PATH/runnable_jar.jar
-java %HEAP_OPTION% -jar %LIB_PATH%\runnable_jar.jar
-
-java %HEAP_OPTION% -classpath %LIB_PATH%\nonrunnable_jar.jar java_jar.Hello
-java %HEAP_OPTION% -classpath %LIB_PATH%\nonrunnable_jar.jar java_jar.Hi
-java %HEAP_OPTION% -classpath %LIB_PATH%\runnable_jar.jar java_jar.Hello
-java %HEAP_OPTION% -classpath %LIB_PATH%\runnable_jar.jar java_jar.Hi
 
 endlocal
 echo on
