10bc8243101b18490dfa6561fe80b0871be7e8d3
sangwook <sangwook>
2013-03-08 19:37:12 +0000

    a project for hand analysis is added: sw_dev/cpp/rnd/test/hand_analysis.

---
+++ sw_dev/cpp/rnd/test/hand_analysis/openhpe/openhpe_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdexcept>
+
+
+namespace {
+namespace local {
+
+}  // namespace local
+}  // unnamed namespace
+
+namespace my_openhpe {
+
+}  // namespace my_openhpe
+
+int openhpe_main(int argc, char *argv[])
+{
+	throw std::runtime_error("not yet implemented");
+
+	return 0;
+}
