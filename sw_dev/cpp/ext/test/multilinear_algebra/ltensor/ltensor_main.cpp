f7bf44ba8884301970970d24dfbe2e013ff318e3
sangwook <sangwook>
2013-04-04 04:20:28 +0000

    Open MPI library is added: sw_dev/cpp/ext/src/parallel_computing/openmpi.

--- sw_dev/cpp/ext/test/multilinear_algebra/ltensor/ltensor_main.cpp
+++ sw_dev/cpp/ext/test/multilinear_algebra/ltensor/ltensor_main.cpp
@@ -8,17 +8,34 @@ namespace local {
 
 void basic_operation()
 {
+	Marray<double, 1> p(2);
+	Marray<double, 2> A(2, 2);
+
+	p(0) = 1;
+	p(1) = 2;
+	A(0, 0) = 1;  A(0, 1) = 2;
+	A(1, 0) = 3;  A(1, 1) = 4;
+
+	//
 	Index<'i'> i;
 	Index<'j'> j;
-	Marray<double, 1> q(2, 1);
-	Marray<double, 1> p(2, 2);
-	Marray<double, 2> A(2, 2, 3);
 
+	Marray<double, 1> q(2);
 	q(i) = A(i, j) * p(j);
 	std::cout << q;
 	
 	p(i) = q(i) + p(i);
 	std::cout << p;
+
+	//
+	p(0) = 1;
+	p(1) = 2;
+	q(0) = 3;
+	q(1) = 4;
+
+	Marray<double, 2> B(2, 2);
+	B(i, j) = p(i) * q(j);  // [ 1 2 ]^T * [ 3 4 ]
+	std::cout << B;
 }
 
 }  // namespace local
