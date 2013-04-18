0eaf9e64b1e8c2e419e6856208796b26e306e328
sangwook <sangwook>
2013-04-18 04:08:06 +0000

    a test for slice sampling is added.

--- sw_dev/matlab/rnd/test/statistical_analysis/slicesample_basic_test.m
+++ sw_dev/matlab/rnd/test/statistical_analysis/slicesample_basic_test.m
@@ -1,21 +1,41 @@
 %----------------------------------------------------------
-% normal distribution (1-dim)
+% example in Matlab
 
-% target distribution
-pdf = @(x) normpdf(x);
-
-% proposal distribution, q(x | y)
-delta = .5;
-proppdf = @(x, y) unifpdf(y - x, -delta, delta);
-proprnd = @(x) x + rand * 2 * delta - delta;
+f = @(x) exp(-x.^2 / 2) .* (1 + (sin(3*x)).^2) .* (1 + (cos(5*x).^2));
 
 num_samples = 10000;
 burn_in_period = 1000;
 thinning_period = 5;
-smpl1 = mhsample(1, num_samples, 'pdf', pdf, 'proprnd', proprnd, 'symmetric', 1, 'thin', thinning_period, 'burnin', burn_in_period);
+smpl0 = slicesample(1, num_samples, 'pdf', f, 'thin', thinning_period, 'burnin', burn_in_period);
+
+[binheight, bincenter] = hist(x, 100);
+h1 = bar(bincenter, binheight, 'hist');
+set(h1, 'facecolor', [0.8 0.8 1]);
+
+hold on;
+xd = get(gca, 'XLim');
+area = quad(f, -5, 5);
+binwidth = bincenter(2) - bincenter(1);
+%xgrid = linspace(xd(1), xd(2), 1000);
+%y = (num_samples * binwidth / area) * f(xgrid);
+%plot(xgrid, y, 'r', 'LineWidth', 2)
+h2 = ezplot(@(x) (num_samples * binwidth / area) * f(x), [xd(1), xd(2)]);
+set(h2, 'color', [1 0 0], 'linewidth', 2);
+hold off;
+
+%----------------------------------------------------------
+% normal distribution (1-dim)
+
+% target funcation
+f = @(x) normpdf(x);
+
+num_samples = 10000;
+burn_in_period = 1000;
+thinning_period = 5;
+smpl2 = slicesample(1, num_samples, 'pdf', f, 'thin', thinning_period, 'burnin', burn_in_period);
 
 figure;
-histfit(smpl1, 100);
+histfit(smpl2, 100);
 h = get(gca, 'Children');
 set(h(2), 'FaceColor', [0.8 0.8 1]);
 
@@ -30,21 +50,16 @@ set(h(2), 'FaceColor', [0.8 0.8 1]);
 % target distribution
 mu = [1 1];
 sigma = [1 0 ; 0 1];
-pdf = @(x) mvnpdf(x, mu, sigma);
-
-% proposal distribution, q(x | y) -> q(x)
-delta = 0.5;
-proppdf = @(x, y) @(x) mvnrnd(x, eye(2));
-proprnd = @(x) x + (rand(1,2) * 2 * delta - delta);
+f = @(x) mvnpdf(x, mu, sigma);
 
 num_samples = 100000;
 burn_in_period = 1000;
 thinning_period = 5;
-smpl2 = mhsample([1 1], num_samples, 'pdf', pdf, 'proprnd', proprnd, 'symmetric', 1, 'thin', thinning_period, 'burnin', burn_in_period);
+smpl3 = slicesample([1 1], num_samples, 'pdf', f, 'thin', thinning_period, 'burnin', burn_in_period);
 
 xedges = linspace(-1, 3, 50);
 yedges = linspace(-1, 3, 50); 
-histmat = hist2(smpl2(:,1), smpl2(:,2), xedges, yedges);
+histmat = hist2(smpl3(:,1), smpl3(:,2), xedges, yedges);
 
 figure;
 pcolor(xedges, yedges, histmat');
@@ -57,33 +72,29 @@ axis square tight;
 % target distribution
 alpha = 2.43;
 beta = 1;
-pdf = @(x) gampdf(x, alpha, beta);
-
-% proposal distribution, q(x | y) -> q(x)
-proppdf = @(x, y) gampdf(x, floor(alpha), floor(alpha) / alpha);
-proprnd = @(x) sum(exprnd(floor(alpha) / alpha, floor(alpha), 1));
+f = @(x) gampdf(x, alpha, beta);
 
 num_samples = 10000;
 burn_in_period = 1000;
 thinning_period = 5;
-smpl3 = mhsample(1, num_samples, 'pdf', pdf, 'proprnd', proprnd, 'proppdf', proppdf, 'thin', thinning_period, 'burnin', burn_in_period);
+smpl4 = slicesample(1, num_samples, 'pdf', f, 'thin', thinning_period, 'burnin', burn_in_period);
 
 figure;
 if 0
-	%xxhat = cumsum(smpl3.^2) ./ (1:num_samples)';
+	%xxhat = cumsum(smpl4.^2) ./ (1:num_samples)';
 	%plot(1:num_samples, xxhat);
-	subplot(2,1,1), hist(smpl3, 100);
+	subplot(2,1,1), hist(smpl4, 100);
 	axis_val = axis;
-	subplot(2,1,2), ezplot(pdf, [axis_val(1) axis_val(2)]);
+	subplot(2,1,2), ezplot(f, [axis_val(1) axis_val(2)]);
 else
 	hold on;
-	[binheight, bincenter] = hist(smpl3, 100);
+	[binheight, bincenter] = hist(smpl4, 100);
 	h1 = bar(bincenter, binheight, 'hist');
 	set(h1, 'facecolor', [0.8 0.8 1]);
 	axis_val = axis;
-	area = quad(pdf, axis_val(1), axis_val(2));
+	area = quad(f, axis_val(1), axis_val(2));
 	binwidth = bincenter(2) - bincenter(1);
-	h2 = ezplot(@(x) (num_samples * binwidth / area) * pdf(x), [axis_val(1) axis_val(2)]);
+	h2 = ezplot(@(x) (num_samples * binwidth / area) * f(x), [axis_val(1) axis_val(2)]);
 	set(h2, 'color', [1 0 0], 'linewidth', 2);
 	hold off;
 end;
@@ -102,39 +113,30 @@ theta_n = pi;
 c = 5;
 n = 100;
 kappa0 = 1;
-pdf = @(theta) exp(kappa0 * R_n * cos(theta - theta_n)) / besseli(0, kappa0)^(c + n);
-
-% proposal distribution, q(x | y)
-delta = .5;
-%a_c = 1;
-%b_c = 1;
-%proppdf = @(theta, kappa) unifpdf(theta, 0, 2*pi) * gampdf(kappa0, a_c, b_c);
-proppdf = @(x, y) unifpdf(y - x, -delta, delta);
-%proprnd = @(x) x + rand * 2 * pi;
-proprnd = @(x) x + rand * 2 * delta - delta;
+f = @(theta) exp(kappa0 * R_n * cos(theta - theta_n)) / besseli(0, kappa0)^(c + n);
 
 num_samples = 10000;
 burn_in_period = 1000;
 thinning_period = 5;
-smpl4 = mhsample(0, num_samples, 'pdf', pdf, 'proprnd', proprnd, 'proppdf', proppdf, 'thin', thinning_period, 'burnin', burn_in_period);
-smpl4 = mod(smpl4, 2*pi);
+smpl5 = slicesample(1, num_samples, 'pdf', f, 'thin', thinning_period, 'burnin', burn_in_period);
+smpl5 = mod(smpl5, 2*pi);
 
 figure;
 if 0
-	subplot(2,1,1), hist(smpl4, 100)
+	subplot(2,1,1), hist(smpl5, 100)
 	axis_val = axis;
 	axis([0 2*pi axis_val(3) axis_val(4)]);
-	subplot(2,1,2), ezplot(pdf, [0 2*pi]);
-	%subplot(2,1,2), ezplot(pdf, [axis_val(1) axis_val(2)]);
+	subplot(2,1,2), ezplot(f, [0 2*pi]);
+	%subplot(2,1,2), ezplot(f, [axis_val(1) axis_val(2)]);
 else
 	hold on;
-	[binheight, bincenter] = hist(smpl4, 50);
+	[binheight, bincenter] = hist(smpl5, 50);
 	h1 = bar(bincenter, binheight, 'hist');
 	set(h1, 'facecolor', [0.8 0.8 1]);
 	axis_val = axis;
-	area = quad(pdf, 0, 2*pi);
+	area = quad(f, 0, 2*pi);
 	binwidth = bincenter(2) - bincenter(1);
-	h2 = ezplot(@(x) (num_samples * binwidth / area) * pdf(x), [0 2*pi]);
+	h2 = ezplot(@(x) (num_samples * binwidth / area) * f(x), [0 2*pi]);
 	set(h2, 'color', [1 0 0], 'linewidth', 2);
 	axis([0 2*pi axis_val(3) axis_val(4)]);
 	hold off;
