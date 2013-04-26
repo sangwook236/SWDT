d886a1dac526cb1679d6e3e26282f026b9ad3c87
sangwook <sangwook>
2013-04-26 15:24:28 +0000

    move remotely.

---
+++ sw_dev/matlab/rnd/test/probabilistic_graphical_model/hcrf/simple_test_1.m
@@ -0,0 +1,15 @@
+%----------------------------------------------------------
+%addpath('D:\work_center\sw_dev\cpp\rnd\src\probabilistic_graphical_model\crf\hcrf\HCRF2.0b\bin\openMP');
+%addpath('D:\work_center\sw_dev\cpp\rnd\src\probabilistic_graphical_model\crf\hcrf\HCRF2.0b\samples\matlab');
+
+%----------------------------------------------------------
+load sampleData;
+
+matHCRF('createToolbox', 'crf', 'lbfgs', 0, 0);
+
+intLabels = cellInt32(trainLabels);
+matHCRF('setData', trainSeqs, intLabels);
+
+matHCRF('train');
+
+[modelCRF.model modelCRF.features] = matHCRF('getModel');
