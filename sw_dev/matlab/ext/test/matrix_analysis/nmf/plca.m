04885f583514e1651c7b2c36d7195bbb179419a0
sangwook <sangwook>
2013-05-01 14:25:02 +0000

    move remotely.

--- sw_dev/matlab/ext/test/matrix_analysis/nmf/plca.m
+++
@@ -1,9 +0,0 @@
-function plca()
-
-%%---------------------------------------------------------
-%% [ref]
-%% "Sparse and Shift-invariant Feature Extraction from Non-negative Data"
-%% by P. Smaragdis, B. Raj, and M. Shashanka, ICASSP 2008
-%% "Probabilistic Latent Variable Model for Sparse Decompositions of Non-negative Data"
-%% by M. Shashanka, B. Raj, and P. Smaragdis, TPAMI 2008
-
