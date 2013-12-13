9e279a62b9521e09269b32443e2fabf09a6468ed
sangwook <sangwook>
2013-12-13 11:54:37 +0000

    The usage guide for PLplot is updated.

--- sw_dev/cpp/ext/test/plotting/plplot/plplot_main.cpp
+++ sw_dev/cpp/ext/test/plotting/plplot/plplot_main.cpp
@@ -16,11 +16,14 @@ void x21_example(int argc, const char **argv);
 
 int plplot_main(int argc, char *argv[])
 {
-	// caution: for execution, driver files have to be loaded.
+	// [NOTICE] caution >> for execution, driver files have to be loaded.
+	//	set PLPLOT_DRV_DIR=${PLPLOT_INSTALL}/lib/plplot5.9.9/driversd
+	//	set PLPLOT_LIB=${PLPLOT_INSTALL}\share\plplot5.9.9
+	//	[ref] ${PLPLOT_HOME}/plplot_build_guide.txt.
 
 	// examples
-	//my_plplot::x01_example(argc, (const char **)argv);
-	my_plplot::x21_example(argc, (const char **)argv);
+	my_plplot::x01_example(argc, (const char **)argv);
+	//my_plplot::x21_example(argc, (const char **)argv);
 
 	return 0;
 }
