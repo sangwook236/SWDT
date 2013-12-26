c42d3af84adf5c2dc7dc1bdcd26258f18fb47d83
sangwook <sangwook>
2013-12-26 14:57:55 +0000

    Source and test directories for Jayrock library are added: sw_dev/csharp/rnd/src/serialization/jayrock & sw_dev/csharp/rnd/test/serialization/jayrock.

---
+++ sw_dev/csharp/ext/test/serialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Jayrock library -----------------------------------------------------");
+                jayrock.Jayrock_Main.run(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("System.Exception occurred: {0}", e);
+            }
+
+            Console.WriteLine("press any key to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sw_dev/csharp/ext/test/serialization/jayrock/Jayrock_Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace serialization.jayrock
+{
+    class Jayrock_Main
+    {
+        public static void run(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
