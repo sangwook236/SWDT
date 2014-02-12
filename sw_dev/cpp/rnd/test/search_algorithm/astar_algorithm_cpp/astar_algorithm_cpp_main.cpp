4c70c5b39e2aadfe11d5f269b23d92979bc14f02
sangwook <sangwook>
2014-02-12 15:33:16 +0000

    Directories for astar-algorithm-cpp algorithm are added: sw_dev/cpp/rnd/src/search_algorithm/astar_algorithm_cpp & sw_dev/cpp/rnd/test/search_algorithm/astar_algorithm_cpp.

---
+++ sw_dev/cpp/rnd/test/search_algorithm/astar_algorithm_cpp/astar_algorithm_cpp_main.cpp
@@ -0,0 +1,38 @@
+//#include "stdafx.h"
+#include "../astar_algorithm_cpp_lib/stlastar.h"
+#include <boost/timer/timer.hpp>
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+
+
+namespace {
+namespace local {
+
+// ${ASTAR_ALGORITHM_CPP_HOME}/8puzzle.cpp.
+void eight_puzzle_example()
+{
+	throw std::runtime_error("not yet implemented");
+}
+
+// ${ASTAR_ALGORITHM_CPP_HOME}/findpath.cpp.
+void find_path_example()
+{
+	throw std::runtime_error("not yet implemented");
+}
+
+}  // namespace local
+}  // unnamed namespace
+
+namespace my_astar_algorithm_cpp {
+
+}  // namespace my_astar_algorithm_cpp
+
+int astar_algorithm_cpp_main(int argc, char *argv[])
+{
+	local::eight_puzzle_example();
+	local::find_path_example();
+
+	return 0;
+}
