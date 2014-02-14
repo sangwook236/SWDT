96a9512fba839ad4905810bb81cfbccb53e2f861
sangwook <sangwook>
2014-02-14 16:30:50 +0000

    A test directory for Dlib C++ library is added: sw_dev/cpp/ext/test/dlib.

---
+++ sw_dev/cpp/ext/test/dlib/main.cpp
@@ -0,0 +1,43 @@
+#if defined(WIN32)
+#include <vld/vld.h>
+#endif
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <stdexcept>
+
+
+int main(int argc, char *argv[])
+{
+	int retval = EXIT_SUCCESS;
+	try
+	{
+		// threading, networking.
+		// graphical user interfaces (GUI).
+		// numerical algorithms, image processing, machine learning algorithms, graphical model inference algorithms.
+		// data compression and integrity algorithms.
+		// testing, general utilities.
+
+		throw std::runtime_error("not yet implemented");
+	}
+    catch (const std::bad_alloc &e)
+	{
+		std::cout << "std::bad_alloc caught: " << e.what() << std::endl;
+		retval = EXIT_FAILURE;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "std::exception caught: " << e.what() << std::endl;
+		retval = EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		std::cout << "unknown exception caught" << std::endl;
+		retval = EXIT_FAILURE;
+	}
+
+	std::cout << "press any key to exit ..." << std::endl;
+	std::cin.get();
+
+	return retval;
+}
