f63910de8ea24ce6d335d4deb5073a6b0ba89e65
sangwook <sangwook>
2014-02-17 14:40:39 +0000

    Directories for Hydra MCMC library are added: sw_dev/java/rnd/src/statistical_analysis/hydra_mcmc & sw_dev/java/rnd/test/statistical_analysis/src/hydra_mcmc.

--- sw_dev/java/rnd/test/statistical_analysis/src/Main.java
+++ sw_dev/java/rnd/test/statistical_analysis/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		try
 		{
-			System.err.println("not yet implemented");
+			System.out.println("HYDRA MCMC Library --------------------------------------------------");
+			hydra_mcmc.HydraMCMC_Main.run(args);  // not yet implemented.
 		}
 		catch (Exception e)
 		{
---
+++ sw_dev/java/rnd/test/statistical_analysis/src/hydra_mcmc/HydraMCMC_Main.java
@@ -0,0 +1,10 @@
+package hydra_mcmc;
+
+public class HydraMCMC_Main {
+	
+	public static void run(String[] args)
+	{
+		System.err.println("not yet implemented");
+	}
+	
+}
