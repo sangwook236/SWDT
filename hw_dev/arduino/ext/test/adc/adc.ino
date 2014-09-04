f15d98c08b09c7331eae2752ea077fbb08ce3849
sangwook <sangwook>
2014-09-04 11:37:30 +0000

    An example for ADC is added.

---
+++ hw_dev/arduino/ext/test/adc/adc.ino
@@ -0,0 +1,15 @@
+int analogPin = 0;     // connected to analog pin 0
+                       // outside leads to ground and +5V
+int val = 0;           // variable to store the value read
+
+void setup()
+{
+  Serial.begin(115200);  // setup serial.
+}
+
+void loop()
+{
+  val = analogRead(analogPin);  // read the input pin.
+  Serial.println(val);  // debug value.
+}
+
