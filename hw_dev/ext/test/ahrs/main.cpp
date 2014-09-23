84124fd766a73d7fd353a851069b09e2eb12cdf9
sangwook <sangwook>
2014-09-23 07:30:02 +0000

    Directories are restructured. Three projects are added : IMU, GPS, AHRS.

---
+++ hw_dev/ext/test/ahrs/main.cpp
@@ -0,0 +1,41 @@
+//#include "stdafx.h"
+#if defined(WIN32)
+#include <vld/vld.h>
+#endif
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+
+int main(int argc, char *argv[])
+{
+	int ahrs_main(int argc, char *argv[]);
+
+	int retval = EXIT_SUCCESS;
+	try
+	{
+		std::cout << "Attitude and Heading Reference System (AHRS) library ---------------" << std::endl;
+		//retval = ahrs_main(argc, argv);
+		throw std::runtime_error("not yet implemented");
+	}
+    catch (const std::bad_alloc &e)
+	{
+		std::cout << "std::bad_alloc caught: " << e.what() << std::endl;
+		retval = EXIT_FAILURE;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "std::exception caught: " << e.what() << std::endl;
+		retval = EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		std::cout << "unknown exception caught" << std::endl;
+		retval = EXIT_FAILURE;
+	}
+
+	std::cout << "press any key to exit ..." << std::endl;
+	std::cin.get();
+
+	return retval;
+}
