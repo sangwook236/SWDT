e66cf9cd66ed0dfe9b83e408b2a5a80b5a1944f6
Sang-Wook Lee <sangwook236@gmail.com>
2015-10-11 22:49:35 -0800

    Converted dlib project into general purpose library project.

--- sw_dev/cpp/ext/test/general_purpose_library/main.cpp
+++ sw_dev/cpp/ext/test/general_purpose_library/main.cpp
@@ -2,27 +2,31 @@
 #include <vld/vld.h>
 #endif
 #include <iostream>
-#include <stdexcept>
 #include <cstdlib>
-#include <stdexcept>
 
 
 int main(int argc, char *argv[])
 {
+	int dlib_main(int argc, char *argv[]);
+	int loki_main(int argc, char *argv[]);
+
 	int retval = EXIT_SUCCESS;
 	try
 	{
-		// algorithm, threading, networking, parsing.
-		// graphical user interfaces (GUI).
-		// linear algebra, numerical algorithms.
-		// optimization.
-		// image processing.
-		// machine learning algorithms.
-		// graph tools, Bayesian networks, graphical model inference algorithms.
-		// data compression and integrity algorithms.
-		// testing, general utilities.
+		std::cout << "Dlib library --------------------------------------------------------" << std::endl;
+		//  algorithm, threading, networking, parsing.
+		//  graphical user interfaces (GUI).
+		//  linear algebra, numerical algorithms.
+		//  optimization.
+		//  image processing.
+		//  machine learning algorithms.
+		//  graph tools, Bayesian networks, graphical model inference algorithms.
+		//  data compression and integrity algorithms.
+		//  testing, general utilities.
+		//retval = dlib_main(argc, argv);  // not yet implemented.
 
-		throw std::runtime_error("not yet implemented");
+		std::cout << "\nLoki library --------------------------------------------------------" << std::endl;
+		//retval = loki_main(argc, argv);  // not yet implemented.
 	}
     catch (const std::bad_alloc &e)
 	{
