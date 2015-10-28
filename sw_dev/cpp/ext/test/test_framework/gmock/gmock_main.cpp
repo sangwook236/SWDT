bc47ff86899278c41b963676228b3e2b7eec5eb4
Sang-Wook Lee <sangwook236@gmail.com>
2015-10-28 10:05:51 -0800

    Minor change of files in gmock.

--- sw_dev/cpp/ext/test/test_framework/gmock/gmock_main.cpp
+++ sw_dev/cpp/ext/test/test_framework/gmock/gmock_main.cpp
@@ -12,13 +12,13 @@ namespace my_gmock {
 
 }  // namespace my_gmock
 
-// REF [site] >> https://code.google.com/p/googlemock/wiki/ForDummies
 int gmock_main(int argc, char *argv[])
 {
+    // REF [site] >> https://github.com/google/googletest/blob/master/googlemock/docs/ForDummies.md
 #if 1
     // The following line must be executed to initialize Google Mock (and Google Test) before running the tests.
     testing::InitGoogleMock(&argc, argv);
-    //testing::InitGoogleTest(&argc, argv);
+    //testing::InitGoogleTest(&argc, argv);  // do not need to be called.
     return RUN_ALL_TESTS();
 #else
     // If you want to use something other than Google Test (e.g. CppUnit or CxxTest) as your testing framework.
--- sw_dev/cpp/ext/test/test_framework/gmock/gmock_turtlemock_unittest.cpp
+++ sw_dev/cpp/ext/test/test_framework/gmock/gmock_turtlemock_unittest.cpp
@@ -43,6 +43,7 @@ namespace my_gmock {
 
 }  // namespace my_gmock
 
+// REF [site] >> https://github.com/google/googletest/blob/master/googlemock/docs/ForDummies.md
 TEST(PainterTest, CanDrawSomething)
 {
     const int cx = 0, cy = 0, radius = 10;
@@ -53,7 +54,12 @@ TEST(PainterTest, CanDrawSomething)
     EXPECT_CALL(turtle, PenDown()).Times(testing::AtLeast(1));
     EXPECT_CALL(turtle, Forward(radius / 2)).Times(testing::AtLeast(1));
     EXPECT_CALL(turtle, Turn(0)).Times(testing::AtLeast(1));
-    EXPECT_CALL(turtle, GoTo(cx, cy)).Times(testing::AtLeast(1));
+    EXPECT_CALL(turtle, GoTo(testing::_, testing::Ge(cy))).Times(testing::AtLeast(1));
+    EXPECT_CALL(turtle, GetX())
+        .Times(5)
+        .WillOnce(testing::Return(100))
+        .WillOnce(testing::Return(150))
+        .WillRepeatedly(testing::Return(200));
 
     local::Painter painter(&turtle);
 
