89f7ed06ed5242ee26ae0aaa3bef2b914a58d73c
Sang-Wook Lee <sangwook236@gmail.com>
2016-03-25 02:05:15 +0900

    Add an empty example for tiny-cnn library. Rename sw_dev/cpp/rnd/test/hand_analysis project to sw_dev/cpp/rnd/test/object_tracking project.

---
+++ sw_dev/cpp/rnd/test/machine_learning/tiny_cnn/tiny_cnn_main.cpp
@@ -0,0 +1,22 @@
+//include "stdafx.h"
+#include <tiny_cnn/tiny_cnn.h>
+#include <iostream>
+#include <stdexcept>
+
+
+namespace {
+namespace local {
+
+}  // namespace local
+}  // unnamed namespace
+
+namespace my_tiny_cnn {
+
+}  // namespace my_tiny_cnn
+
+int tiny_cnn_main(int argc, char *argv[])
+{
+	throw std::runtime_error("not yet implemented");
+
+	return 0;
+}
