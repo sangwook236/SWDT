21ae6fe06ab2e62b27df60aadc4cdf0f224a3658
Sang-Wook Lee <sangwook236@gmail.com>
2016-10-27 01:23:56 +0900

    Add an example of syslog.

--- sw_dev/cpp/ext/test/logging/main.cpp
+++ sw_dev/cpp/ext/test/logging/main.cpp
@@ -1,10 +1,60 @@
 #if defined(WIN32) || defined(_WIN32)
 #include <vld/vld.h>
 #endif
+#if defined(_WIN64) || defined(_WIN32)
+#include <windows.h>
+#include <winevt.h>
+#else
+#include <syslog.h>
+#endif
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+
+namespace {
+namespace local {
+
+#if defined(_WIN64) || defined(_WIN32)
+#pragma comment(lib, "wevtapi.lib")
+
+// REF [site] >>
+//	Windows Event Logging (recommended): https://msdn.microsoft.com/en-us/library/windows/desktop/aa385780(v=vs.85).aspx
+//	Event Logging (deprecated): https://msdn.microsoft.com/en-us/library/windows/desktop/aa363652(v=vs.85).aspx
+
+// REF [site] >> https://msdn.microsoft.com/en-us/library/windows/desktop/dd996928(v=vs.85).aspx
+void event_log_example()
+{
+	// REF [site] >> https://msdn.microsoft.com/en-us/library/windows/desktop/dd996928(v=vs.85).aspx
+	throw std::runtime_error("Not yet implemented");
+}
+#else
+// Config file: /etc/rsyslog.conf or /etc/syslog.conf.
+// Log file: /var/log/syslog.
+//	tail -f /var/log/syslog
+//	tail -f /var/log/syslog | grep process_name
+//	less /var/log/syslog
+
+// REF [site] >>
+//	https://www.gnu.org/software/libc/manual/html_node/Syslog.html
+//	https://www.gnu.org/software/libc/manual/html_node/Submitting-Syslog-Messages.html
+void syslog_example()
+{
+	//setlogmask(LOG_MASK(LOG_EMERG) | LOG_MASK(LOG_ERROR));
+	//setlogmask(~(LOG_MASK(LOG_INFO)));
+	setlogmask(LOG_UPTO(LOG_NOTICE));
+
+	openlog("exampleprog", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+	syslog(LOG_NOTICE, "Program started by User %d", getuid());
+	syslog(LOG_MAKEPRI(LOG_USER, LOG_INFO), "A tree falls in a forest");
+
+	closelog();
+}
+#endif
+
+}  // namespace local
+}  // unnamed namespace
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +64,15 @@ int main(int argc, char *argv[])
 	int retval = EXIT_SUCCESS;
 	try
 	{
-		std::cout << "Boost.Log libary ----------------------------------------------------" << std::endl;
+#if defined(_WIN64) || defined(_WIN32)
+		std::cout << "Windows Event Log ---------------------------------------------------" << std::endl;
+		//local::event_log_example();  // Not yet implemented.
+#else
+		std::cout << "syslog --------------------------------------------------------------" << std::endl;
+		local::syslog_example();
+#endif
+
+		std::cout << "\nBoost.Log libary ----------------------------------------------------" << std::endl;
 		// REF [library] >> Boost library.
 
 		std::cout << "\nlog4cxx libary ------------------------------------------------------" << std::endl;
