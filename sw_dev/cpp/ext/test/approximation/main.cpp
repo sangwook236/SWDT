2e88476a57a3139a090c1adc428043877d9dfe57
Sang-Wook Lee <sangwook236@gmail.com>
2016-11-01 22:36:14 +0900

    Add comments on boost & approximation.

--- sw_dev/cpp/ext/test/approximation/main.cpp
+++ sw_dev/cpp/ext/test/approximation/main.cpp
@@ -8,11 +8,13 @@
 
 
 // NOTICE [important] >>
-//	If it is impossible to use an explicit form y = y(x), a parametric form x = x(t) & y = y(t) may be anothor choice.
+//	When it is impossible to use an explicit form, a parametric form may be anothor choice.
 //		y = y(x) --> x = x(t), y = y(t).
 //		z = z(x, y) --> x = x(s), y = y(t), z = z(s, t).
+//	Parameters may be (data) indices (t = 1,2,3,...).
 //	e.g.)
-//		In case of splines, x values must not be identical. If x values have duplicate ones, evaluating a spline becomes to fail.
+//		In case of splines, x values must not be identical.
+//		If x values have duplicate ones, evaluating a spline becomes to fail.
 //		In this case, we can use a parametric form and do other jobs like plotting an x-y graph.
 
 int main(int argc, char *argv[])
