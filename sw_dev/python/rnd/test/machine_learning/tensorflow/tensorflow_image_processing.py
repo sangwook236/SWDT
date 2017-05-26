1a0d7913419382b48d405291406332839e939073
Sang-Wook Lee <sangwook236@gmail.com>
2017-05-26 14:55:26 +0900

    Add an example to load files in TensorFlow. Enhance image handling in TensorFlow.

--- sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_image_processing.py
+++ sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_image_processing.py
@@ -6,15 +6,15 @@ import numpy as np
 
 #%%------------------------------------------------------------------
 
-#filenames = ['D:/dataset/pattern_recognition/street1.png']
-filenames = tf.train.match_filenames_once('D:/dataset/pattern_recognition/*.png')
-#filenames = tf.train.match_filenames_once('D:/dataset/pattern_recognition/*.jpg')
+filenames = ['D:/dataset/pattern_recognition/street1.png', 'D:/dataset/pattern_recognition/street2.png']
+#filenames = tf.train.match_filenames_once("D:/dataset/pattern_recognition/*.png")  # Not working.
+#filenames = tf.train.match_filenames_once('D:/dataset/pattern_recognition/*.jpg')  # Not working.
 
 count_num_files = tf.size(filenames)
 filename_queue = tf.train.string_input_producer(filenames)
 
 reader = tf.WholeFileReader()
-key, img_value = reader.read(filename_queue)
+_, img_value = reader.read(filename_queue)
 
 #%%------------------------------------------------------------------
 # Encode and decode.
@@ -23,23 +23,32 @@ key, img_value = reader.read(filename_queue)
 rgb_img = tf.image.decode_png(img_value)
 #rgb_img = tf.image.decode_jpeg(img_value)
 
-init = tf.global_variables_initializer()
+initializer = tf.global_variables_initializer()
 with tf.Session() as sess:
-	sess.run(init)
+	# Required to get the filename matching to run.
+	sess.run(initializer)
+
+	# Coordinate the loading of image files.
 	coord = tf.train.Coordinator()
 	threads = tf.train.start_queue_runners(coord = coord)
+
+	# Get an image tensor and print its value.
 	num_files = sess.run(count_num_files)
 	for i in range(num_files):
-		image = rgb_img.eval()
+		image = sess.run(rgb_img)  # numpy.ndarray.
 		print(image.shape)
 		#Image.fromarray(np.asarray(image)).save('temp.jpeg')
 		Image.fromarray(np.asarray(image)).show()
 
-tf.image.encode_png(rgb_img)
-tf.image.encode_jpeg(rgb_img)
+	# Finish off the filename queue coordinator.
+	coord.request_stop()
+	coord.join(threads)
+
+#tf.image.encode_png(rgb_img)
+#tf.image.encode_jpeg(rgb_img)
 
 #%%------------------------------------------------------------------
 # Converting between colorspaces.
 
 rgb_img_float = tf.image.convert_image_dtype(rgb_img, tf.float32)
-hsv_img = tf.image.rgb_to_hsv(rgb_img)
+hsv_img = tf.image.rgb_to_hsv(tf.to_float(rgb_img))
