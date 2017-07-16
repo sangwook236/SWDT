0d5a6f6f2a615e7849e5dd8ac7f5cb9f6b08dfaa
Sang-Wook Lee <sangwook236@gmail.com>
2017-07-16 23:59:23 +0900

    Add image handling examples in matplotlib.

---
+++ sw_dev/python/ext/test/visualization/matplotlib/matplotlib_image.py
@@ -0,0 +1,31 @@
+# REF [site] >> https://matplotlib.org/users/image_tutorial.html
+
+%matplotlib inline
+%matplotlib notebook
+
+#import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+#%%-------------------------------------------------------------------
+
+# Loading image data is supported by the Pillow library.
+# Natively, matplotlib only supports PNG images.
+
+img = mpimg.imread('./data/stinkbug.png')
+
+imgplot = plt.imshow(img)
+
+lum_img = img[:,:,0]
+plt.imshow(lum_img)
+plt.imshow(lum_img, cmap='hot')
+plt.imshow(lum_img, cmap='gray')
+
+imgplot = plt.imshow(lum_img)
+imgplot.set_cmap('nipy_spectral')
+
+imgplot = plt.imshow(lum_img)
+plt.colorbar()
+
+plt.imsave('./data/img_gray.png', lum_img, cmap='gray')
