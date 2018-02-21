8537587652a90aab2fbe29513c2c07c50b8be6e0
Sang-Wook Lee <sangwook236@gmail.com>
2018-02-21 18:00:22 +0900

    Updated the example of TF session & graph.

--- sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_session_graph.py
+++ sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_session_graph.py
@@ -14,18 +14,18 @@ graph3 = tf.Graph()
 with graph1.as_default() as graph:
 	a1 = tf.get_variable('a1', shape=(3, 3))
 	a2 = tf.get_variable('a2', shape=(2))
+	a3 = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='a3')
+	a4 = tf.placeholder(tf.float32, shape=(2, 3), name='a4')
 
-	c = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='c')
+	x1_1 = tf.constant(2, dtype=tf.float32, shape=[2, 3], name='x1_1')
+	x1_2 = tf.constant(3, dtype=tf.float32, shape=[2, 3], name='x1_2')
+	y1_1 = tf.sqrt(x1_1, name='y1_1')
+	y1_2 = tf.sin(x1_2, name='y1_2')
+	z1 = tf.add(y1_1, y1_2, name='z1')
 
-	x1 = tf.placeholder(tf.float32, shape=(2, 3), name='x1')
-	x2 = tf.placeholder(tf.float32, shape=(2, 3), name='x2')
-	y1 = tf.sqrt(x1, name='sqrt')
-	y2 = tf.sin(x2, name='sin')
-	z = tf.add(y1, y2, name='add')
-
-	A = tf.placeholder(tf.float32, shape=(5, 10), name='A')
-	B = tf.placeholder(tf.float32, shape=(10, 3), name='B')
-	C = tf.matmul(A, B, name='matmul')
+	A1 = tf.constant(2, dtype=tf.float32, shape=(5, 10), name='A1')
+	B1 = tf.constant(3, dtype=tf.float32, shape=(10, 3), name='B1')
+	C1 = tf.matmul(A1, B1, name='C1')
 
 	print('Global variables =', tf.global_variables())
 	print('Local variables =', tf.local_variables())
@@ -34,14 +34,13 @@ with graph1.as_default() as graph:
 
 with graph3.as_default() as graph:
 	c1 = tf.get_variable('c1', shape=(3, 3))
+	c3 = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='c3')
 
-	c = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='c')
-
-	x1 = tf.placeholder(tf.float32, shape=(2, 3), name='x1')
-	x2 = tf.placeholder(tf.float32, shape=(2, 3), name='x2')
-	y1 = tf.sqrt(x1, name='sqrt')
-	y2 = tf.sin(x2, name='sin')
-	z = tf.add(y1, y2, name='add')
+	x3_1 = tf.constant(2, dtype=tf.float32, shape=[2, 3], name='x3_1')
+	x3_2 = tf.constant(3, dtype=tf.float32, shape=[2, 3], name='x3_2')
+	y3_1 = tf.sqrt(x3_1, name='y3_1')
+	y3_2 = tf.sin(x3_2, name='y3_2')
+	z3 = tf.add(y3_1, y3_2, name='z3')
 
 	print('Global variables =', tf.global_variables())
 	print('Local variables =', tf.local_variables())
@@ -66,18 +65,18 @@ print('[2] Graph id =', id(tf.get_default_graph()))
 with sess2:
 	b1 = tf.get_variable('b1', shape=(3, 3))
 	b2 = tf.get_variable('b2', shape=(2))
+	b3 = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='b3')
+	b4 = tf.placeholder(tf.float32, shape=(2, 3), name='b4')
 
-	c = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='c')
+	x2_1 = tf.constant(2, dtype=tf.float32, shape=[2, 3], name='x2_1')
+	x2_2 = tf.constant(3, dtype=tf.float32, shape=[2, 3], name='x2_2')
+	y2_1 = tf.sqrt(x2_1, name='y2_1')
+	y2_2 = tf.sin(x2_2, name='y2_2')
+	z2 = tf.add(y2_1, y2_2, name='z2')
 
-	x1 = tf.placeholder(tf.float32, shape=(2, 3), name='x1')
-	x2 = tf.placeholder(tf.float32, shape=(2, 3), name='x2')
-	y1 = tf.sqrt(x1, name='sqrt')
-	y2 = tf.sin(x2, name='sin')
-	z = tf.add(y1, y2, name='add')
-
-	A = tf.placeholder(tf.float32, shape=(5, 10), name='A')
-	B = tf.placeholder(tf.float32, shape=(10, 3), name='B')
-	C = tf.matmul(A, B, name='matmul')
+	A2 = tf.constant(2, dtype=tf.float32, shape=(5, 10), name='A2')
+	B2 = tf.constant(3, dtype=tf.float32, shape=(10, 3), name='B2')
+	C2 = tf.matmul(A1, B1, name='C2')
 
 	print('Global variables =', tf.global_variables())
 	print('Local variables =', tf.local_variables())
@@ -87,10 +86,11 @@ with sess2:
 #with graph3.as_default() as graph:
 with sess3:
 	c2 = tf.get_variable('c2', shape=(2))
+	c3 = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name='c3')
 
-	A = tf.placeholder(tf.float32, shape=(5, 10), name='A')
-	B = tf.placeholder(tf.float32, shape=(10, 3), name='B')
-	C = tf.matmul(A, B, name='matmul')
+	A3 = tf.constant(2, dtype=tf.float32, shape=(5, 10), name='A3')
+	B3 = tf.constant(3, dtype=tf.float32, shape=(10, 3), name='B3')
+	C3 = tf.matmul(A3, B3, name='C3')
 
 	print('Global variables =', tf.global_variables())
 	print('Local variables =', tf.local_variables())
@@ -101,19 +101,26 @@ print('[3] Session id =', id(tf.get_default_session()))
 print('[3] Graph id =', id(tf.get_default_graph()))
 print('Global variables =', tf.global_variables())
 
-with sess1:
+with sess1 as sess:
 	print('[4] Session id =', id(tf.get_default_session()))  # default session == sess1.
 	print('[4] Graph id =', id(tf.get_default_graph()))  # default graph == graph1.
 	print('Global variables =', tf.global_variables())
-with sess2:
+with sess2 as sess:
 	print('[5] Session id =', id(tf.get_default_session()))  # default session == sess2.
 	print('[5] Graph id =', id(tf.get_default_graph()))  # default graph == graph2.
 	print('Global variables =', tf.global_variables())
-with sess3:
+with sess3 as sess:
 	print('[6] Session id =', id(tf.get_default_session()))  # default session == sess32.
 	print('[6] Graph id =', id(tf.get_default_graph()))  # default graph == graph3.
 	print('Global variables =', tf.global_variables())
-	
+
+	# NOTICE [caution] >> If tf.Tensor.eval() or tf.Session.run() is called in with statement, the session (sess3) will be closed in the end of with statement
+	#print(C3.eval(session=sess))
+	#print(sess.run(C3))
+
+print(C3.eval(session=sess3))
+print(sess3.run(C3))
+
 with graph1.as_default():
 	print('[7] Session id =', id(tf.get_default_session()))  # default session != sess1.
 	print('[7] Graph id =', id(tf.get_default_graph()))  # default graph == graph1.
@@ -129,3 +136,12 @@ with graph3.as_default():
 
 print('[10] Session id =', id(tf.get_default_session()))
 print('[10] Graph id =', id(tf.get_default_graph()))
+
+with sess3 as sess:
+	# NOTICE [caution] >> If tf.Tensor.eval() or tf.Session.run() is called in with statement, the session (sess3) will be closed in the end of with statement
+	#print(C3.eval(session=sess))
+	#print(sess.run(C3))
+	print('aaa')
+
+print(C3.eval(session=sess3))
+print(sess3.run(C3))
