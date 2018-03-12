2af365ee4c8aae0975bdccfee55944ba8402c42f
Sang-Wook Lee <sangwook236@gmail.com>
2018-03-12 15:47:47 +0900

    Added examples for argparser & logging in Python.

---
+++ sw_dev/python/ext/test/logging_test.py
@@ -0,0 +1,20 @@
+import logging
+
+def main():
+	#format = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+	format = '%(asctime)-15s %(message)s'
+	logging.basicConfig(format=format)
+
+	logger = logging.getLogger(__name__)
+	logger.setLevel(logging.INFO)
+
+	logger.log(logging.WARNING, '[Warning] Warning.')
+	logger.debug('[Debug] Debug.')
+	logger.info('[Info] Info.')
+	logger.warning('[Warning] Warning.')
+	logger.error('[Error] Error.')
+	logger.critical('[Critical] Critical.')
+	logger.exception('[Exception] Exception.')
+
+if __name__ == '__main__':
+	main()
