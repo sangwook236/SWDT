5891c1209c41a68ef245c16dcb4675b6f6240764
Sang-Wook Lee <sangwook236@gmail.com>
2018-04-05 18:02:03 +0900

    Updated the usage guides for KNIME & H2O, Added a usage guide for pandas. Modifying the example of GridRNN in TensorFlow.

--- sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_grid_lstm.py
+++ sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_grid_lstm.py
@@ -1,5 +1,3 @@
-# REF [site] >> https://www.tensorflow.org/get_started/get_started
-
 import tensorflow as tf
 from PIL import Image
 import numpy as np
@@ -13,7 +11,8 @@ img = Image.open(img_filename)
 img = np.asarray(img, dtype='uint8') / 255
 #img = img.reshape((-1,) + img.shape)
 img = img.reshape((-1,) + (img.shape[0] // 4, img.shape[1] // 4, img.shape[2]))
-img 
+#img = np.random.randn(10, 100, 100, 3)
+#img = np.random.rand(10, 100, 100, 3)
 
 num_dims = 2
 num_classes = 2
@@ -27,17 +26,22 @@ keep_prob = 0.5
 import grid_rnn_cell
 
 def create_unit_cell(num_hidden_units, tied=False, non_recurrent_fn=None):
-	#return tf.contrib.rnn.GridLSTMCell(num_hidden_units, num_frequency_blocks)
+	#num_frequency_blocks = [1]
+	#start_freqindex_list = [0]
+	#end_freqindex_list = [1]
+	#return tf.contrib.rnn.GridLSTMCell(num_hidden_units, num_frequency_blocks=num_frequency_blocks, start_freqindex_list=start_freqindex_list, end_freqindex_list=end_freqindex_list, reuse=True)
+
 	# NOTICE [info] >> tf.contrib.grid_rnn.Grid2LSTMCell receives input data from 0-th dimension only.
 	#return tf.contrib.grid_rnn.Grid2LSTMCell(num_hidden_units, tied, non_recurrent_fn)
-	def cell_fn(num_hidden_units):
-		return tf.contrib.rnn.LSTMCell(num_units=num_hidden_units, forget_bias=1.0, use_peepholes=False)
+
+	#def cell_fn(num_hidden_units):
+	#	return tf.contrib.rnn.LSTMCell(num_units=num_hidden_units, forget_bias=1.0, use_peepholes=False)
 	#return tf.contrib.grid_rnn.GridRNNCell(num_hidden_units, num_dims=num_dims, input_dims=[0, 1], output_dims=[0, 1], priority_dims=None, non_recurrent_dims=None, tied=tied, cell_fn=cell_fn, non_recurrent_fn=non_recurrent_fn)
 	#return tf.contrib.grid_rnn.GridRNNCell(num_hidden_units, num_dims=num_dims, input_dims=[0, 1], output_dims=[0, 1], priority_dims=None, non_recurrent_dims=None, tied=tied, cell_fn=None, non_recurrent_fn=non_recurrent_fn)
 	#return tf.contrib.grid_rnn.GridRNNCell(num_hidden_units, num_dims=num_dims, input_dims=0, output_dims=[0, 1], priority_dims=None, non_recurrent_dims=None, tied=tied, cell_fn=None, non_recurrent_fn=non_recurrent_fn)
 	# NOTICE [info] >> 'input_dims=None & output_dims=None' means that there are no input and no output.
 	#return tf.contrib.grid_rnn.GridRNNCell(num_hidden_units, num_dims=num_dims, input_dims=None, output_dims=None, priority_dims=None, non_recurrent_dims=None, tied=tied, cell_fn=None, non_recurrent_fn=non_recurrent_fn)
-	return grid_rnn_cell.GridRNNCell(num_hidden_units, num_dims=num_dims, input_dims=0, output_dims=[0, 1], priority_dims=None, non_recurrent_dims=None, tied=tied, cell_fn=None, non_recurrent_fn=non_recurrent_fn)
+	return grid_rnn_cell.GridRNNCell(num_hidden_units, num_dims=num_dims, input_dims=0, output_dims=0, priority_dims=None, non_recurrent_dims=None, tied=tied, cell_fn=None, non_recurrent_fn=non_recurrent_fn)
 
 # Defines a cell.
 cell = create_unit_cell(num_hidden_units)
@@ -65,3 +69,38 @@ print('\tTotal time = {}'.format(time.time() - total_elapsed_time))
 
 #%%
 sess = tf.Session()
+
+#%%
+
+# REF [site] >> https://github.com/KendallWeihe/Tensorflow-GridRNNCell-example/blob/master/GridLSTM_ex.py
+
+num_dims = 3
+num_classes = 2
+num_hidden_units = 128
+keep_prob = 0.8
+
+input_shape = (20, 30, 4, 3)  # (height, width, depth, channels).
+ouput_shape = input_shape[:3] + (num_classes,)
+
+x_ph = tf.placeholder(tf.float32, input_shape)
+y_ph = tf.placeholder(tf.float32, ouput_shape)
+
+# Generates random data.
+x = np.random.rand(input_shape[0], input_shape[1], input_shape[2], input_shape[3])
+y_gt = np.random.rand(ouput_shape[0], ouput_shape[1], ouput_shape[2], ouput_shape[3])
+
+def run_grid_cell(x, num_dims, num_hidden_units, keep_prob):
+	#import pdb
+	#pdb.set_trace()
+
+	cell = tf.contrib.grid_rnn.GridRNNCell(num_hidden_units, num_dims=num_dims)
+	cell = tf.contrib.rnn.MultiRNNCell([cell] * num_hidden_units)
+	cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+
+	x = tf.unstack(x, num_time_steps, axis=1)
+	outputs, state = tf.nn.static_rnn(cell, x, dtype=tf.float32)
+	outputs = tf.stack(outputs, axis=1)
+	return outputs
+
+with tf.variable_scope('gridrnn', reuse=tf.AUTO_REUSE):
+	pred = run_grid_cell(x_ph, num_dims, num_hidden_units, keep_prob)
