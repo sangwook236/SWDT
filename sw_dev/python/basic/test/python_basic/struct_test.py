3905cd5d0d8c2139e9eab56aa750a522d33a85bd
Sang-Wook Lee <sangwook236@gmail.com>
2018-06-18 03:30:53 +0900

    Added an example for Python struct module which performs conversions between Python values and C structs represented as Python bytes objects. Added an example for SVM in sklearn.

---
+++ sw_dev/python/basic/test/python_basic/struct_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# REF [site] >> https://docs.python.org/3/library/struct.html
+#	struct module performs conversions between Python values and C structs represented as Python bytes objects.
+
+import struct
+import sys
+
+def main():
+	print('byte order = {} endian.'.format(sys.byteorder))
+
+	packet = struct.pack('hhl', 1, 2, 3)  # bytes.
+	print('packet =', packet)
+
+	packet1 = struct.unpack('hhl', packet)
+	print('unpacked packet =', packet1)  # tuple: (1, 2, 3).
+
+	record = b'raymond   \x32\x12\x08\x01\x08'
+	name, serialnum, school, gradelevel = struct.unpack('<10sHHb', record)
+	print(name, serialnum, school, gradelevel)
+
+	# The ordering of format characters may have an impact on size since the padding needed to satisfy alignment requirements is different.
+	pack1 = struct.pack('ci', b'*', 0x12131415)
+	print(struct.calcsize('ci'), pack1)
+	pack2 = struct.pack('ic', 0x12131415, b'*')
+	print(struct.calcsize('ic'), pack2)
+
+	# The following format 'llh0l' specifies two pad bytes at the end, assuming longs are aligned on 4-byte boundaries.
+	print(struct.pack('llh0l', 1, 2, 3))
+
+#%%------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
---
+++ sw_dev/python/rnd/test/machine_learning/sklearn/sklearn_svm.py
@@ -0,0 +1,50 @@
+# REF [site] >> http://scikit-learn.org/stable/modules/ensemble.html
+# REF [site] >> http://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_iris.html
+
+from sklearn.svm import SVC, SVR
+from sklearn.svm import libsvm
+from sklearn.datasets import make_blobs
+from sklearn.datasets import load_iris
+import numpy as np
+
+#%%-------------------------------------------------------------------
+
+#X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+#Y = np.array([1, 1, 2, 2])
+iris = load_iris()
+X = iris.data
+Y = iris.target
+#X, Y = make_blobs(n_samples=10000, n_features=10, centers=100, random_state=0)
+
+clf = SVC()
+clf.fit(X, Y) 
+
+#print('Prediction =', clf.predict([[-0.8, -1]]))
+print('Prediction =', clf.predict([[5.1, 3.5, 1.4, 0.2]]))
+
+#%%-------------------------------------------------------------------
+
+num_samples, num_features = 10, 5
+np.random.seed(0)
+Y = np.random.randn(num_samples)
+X = np.random.randn(num_samples, num_features)
+
+clf = SVR(C=1.0, epsilon=0.2)
+clf.fit(X, Y) 
+
+print('Prediction =', clf.predict(np.random.randn(1, num_features)))
+
+#%%-------------------------------------------------------------------
+# libsvm.
+
+iris = load_iris()
+X = iris.data.astype(float)
+Y = iris.target.astype(float)
+#X, Y = make_blobs(n_samples=10000, n_features=10, centers=100, random_state=0)
+
+support, SV, n_class_SV, sv_coef, intercept, probA, probB, fit_status = libsvm.fit(X, Y, svm_type=0, kernel='rbf', degree=3, gamma=0.1, coef0=0, tol=1e-3, C=1.0, nu=0.5, epsilon=0.1, class_weight=np.empty(0), sample_weight=np.empty(0), shrinking=1, probability=1, cache_size=100.0, max_iter=-1, random_seed=0) 
+#print(probA, probB)
+
+pred1 = libsvm.predict(X[50,:].reshape((1, -1)), support, SV, n_class_SV, sv_coef, intercept, probA, probB, svm_type=0, kernel='rbf', degree=3, gamma=0.1, coef0=0, class_weight=np.empty(0), sample_weight=np.empty(0), cache_size=100.0)
+pred2 = libsvm.predict_proba(X[50,:].reshape((1, -1)), support, SV, n_class_SV, sv_coef, intercept, probA, probB, svm_type=0, kernel='rbf', degree=3, gamma=0.1, coef0=0, class_weight=np.empty(0), sample_weight=np.empty(0), cache_size=100.0)
+print('Prediction =', pred1, pred2)
