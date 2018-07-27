690b9ac193c2634bdd035774bfd16adeffff42ee
Sang-Wook Lee <sangwook236@gmail.com>
2018-07-27 15:37:42 +0900

    [New, Scala] Added a new simple example for Spark.

---
+++ sw_dev/scala/ext/test/distributed_computing/spark/simple_example/build.sbt
@@ -0,0 +1,10 @@
+name := "simple-example"
+
+version := "1.0.0"
+
+scalaVersion := "2.11.8"
+
+// Additional libraries.
+libraryDependencies ++= Seq(
+	"org.apache.spark" %% "spark-core" % "2.3.1" % "provided"
+)
---
+++ sw_dev/scala/ext/test/distributed_computing/spark/simple_example/src/main/scala/com/sangwook/spark/Spark_Main.scala
@@ -0,0 +1,36 @@
+package com.sangwook.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+object Spark_Main {
+	def run(args: Array[String])
+	{
+		runWordCount()
+	}
+
+	def runWordCount()
+	{
+		// Create a Scala Spark Context.
+		val conf = new SparkConf().setMaster("local").setAppName("wordCount")
+		val sc = new SparkContext(conf)
+
+		var inputFile = "./README.md"
+		var outputFile = "./wordcount"
+
+		// Load our input data.
+		val input = sc.textFile(inputFile)
+
+		// Split it up into words.
+		val words = input.flatMap(line => line.split(" "))
+
+		// Transform into pairs and count.
+		val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
+
+		// Save the word count back out to a text file, causing evaluation.
+		counts.saveAsTextFile(outputFile)
+
+		sc.stop()
+	}
+}
