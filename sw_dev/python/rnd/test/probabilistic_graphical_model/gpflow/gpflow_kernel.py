535f96c2fdb900ec58328595527383036ec95d86
Sang-Wook Lee <sangwook236@gmail.com>
2018-08-22 16:38:23 +0900

    [New, Python] Added two examples for kernel handling and multi-class classification in GPflow library.

---
+++ sw_dev/python/rnd/test/probabilistic_graphical_model/gpflow/gpflow_kernel.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import gpflow
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+
+def plotkernelsample(k, ax, xmin=-3, xmax=3):
+	xx = np.linspace(xmin, xmax, 100)[:, None]
+	K = k.compute_K_symm(xx)
+	ax.plot(xx, np.random.multivariate_normal(np.zeros(100), K, 3).T)
+	ax.set_title(k.__class__.__name__)
+
+def plotkernelfunction(k, ax, xmin=-3, xmax=3, other=0):
+	xx = np.linspace(xmin, xmax, 100)[:, None]
+	#K = k.compute_K_symm(xx)
+	ax.plot(xx, k.compute_K(xx, np.zeros((1, 1)) + other))
+	ax.set_title(k.__class__.__name__ + ' k(x, %f)' % other)
+
+# REF [site] >> https://gpflow.readthedocs.io/en/latest/notebooks/kernels.html
+def kernel_example():
+	#%matplotlib inline
+	plt.style.use('ggplot')
+
+	# Kernel choices.
+	f, axes = plt.subplots(2, 4, figsize=(12, 6), sharex=True, sharey=True)
+	plotkernelsample(gpflow.kernels.Matern12(1), axes[0,0])
+	plotkernelsample(gpflow.kernels.Matern32(1), axes[0,1])
+	plotkernelsample(gpflow.kernels.Matern52(1), axes[0,2])
+	plotkernelsample(gpflow.kernels.RBF(1), axes[0,3])
+	plotkernelsample(gpflow.kernels.Constant(1), axes[1,0])
+	plotkernelsample(gpflow.kernels.Linear(1), axes[1,1])
+	plotkernelsample(gpflow.kernels.Cosine(1), axes[1,2])
+	plotkernelsample(gpflow.kernels.Periodic(1), axes[1,3])
+	axes[0,0].set_ylim(-3, 3)
+
+	# Combine kernels.
+	k1 = gpflow.kernels.Matern12(input_dim=1)
+	k2 = gpflow.kernels.Linear(input_dim=1)
+
+	k3 = k1 + k2
+	k4 = k1 * k2
+
+	# Kernels on multiple dimemensions.
+	k = gpflow.kernels.Matern52(input_dim=5)
+	print(k.as_pandas_table())
+
+	k = gpflow.kernels.Matern52(input_dim=5, ARD=True)
+	print(k.as_pandas_table())
+
+	# Active dimensions.
+	k1 = gpflow.kernels.Linear(1, active_dims=[0])
+	k2 = gpflow.kernels.Matern52(1, active_dims=[1])
+	k = k1 + k2
+	print(k.as_pandas_table())
+
+class Brownian(gpflow.kernels.Kernel):
+	def __init__(self):
+		super().__init__(input_dim=1, active_dims=[0])
+		self.variance = gpflow.Param(1.0, transform=gpflow.transforms.positive)
+
+	def K(self, X, X2=None):
+		if X2 is None:
+			X2 = X
+		return self.variance * tf.minimum(X, tf.transpose(X2))
+
+	def Kdiag(self, X):
+		return self.variance * tf.reshape(X, (-1,))
+
+def making_new_kernel_example():
+	#%matplotlib inline
+	plt.style.use('ggplot')
+
+	X = np.random.rand(5, 1)
+	Y = np.sin(X * 6) + np.random.randn(*X.shape) * 0.001
+
+	k1 = Brownian()
+	k2 = gpflow.kernels.Constant(1)
+	k = k1 + k2
+	print(k.as_pandas_table())
+
+	model = gpflow.models.GPR(X, Y, kern=k)
+	print(model.as_pandas_table())
+
+	opt = gpflow.train.ScipyOptimizer()
+	opt.minimize(model)
+	print(model.kern.as_pandas_table())
+
+	xx = np.linspace(0, 1.1, 100).reshape(100, 1)
+	mean, var = model.predict_y(xx)
+	plt.plot(X, Y, 'kx', mew=2)
+	line, = plt.plot(xx, mean, lw=2)
+	_ = plt.fill_between(xx[:,0], mean[:,0] - 2 * np.sqrt(var[:,0]), mean[:,0] + 2 * np.sqrt(var[:,0]), color=line.get_color(), alpha=0.2)
+
+def main():
+	kernel_example()
+	making_new_kernel_example()
+
+#%%------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
---
+++ sw_dev/python/rnd/test/probabilistic_graphical_model/gpflow/gpflow_multiclass_classification.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+import gpflow
+import tensorflow as tf
+import numpy as np
+from scipy.cluster.vq import kmeans
+from matplotlib import pyplot as plt
+import matplotlib
+import csv, sys
+
+def plot(m):
+	f = plt.figure(figsize=(12, 6))
+	a1 = f.add_axes([0.05, 0.05, 0.9, 0.6])
+	a2 = f.add_axes([0.05, 0.7, 0.9, 0.1])
+	a3 = f.add_axes([0.05, 0.85, 0.9, 0.1])
+
+	xx = np.linspace(m.X.read_value().min(), m.X.read_value().max(), 200).reshape(-1,1)
+	mu, var = m.predict_f(xx)
+	mu, var = mu.copy(), var.copy()
+	p, _ = m.predict_y(xx)
+
+	a3.set_xticks([])
+	a3.set_yticks([])
+
+	a3.set_xticks([])
+	a3.set_yticks([])
+
+	for i in range(m.likelihood.num_classes):
+		x = m.X.read_value()[m.Y.read_value().flatten() == i]
+		points, = a3.plot(x, x * 0, '.')
+		color = points.get_color()
+		a1.plot(xx, mu[:,i], color=color, lw=2)
+		a1.plot(xx, mu[:,i] + 2 * np.sqrt(var[:,i]), '--', color=color)
+		a1.plot(xx, mu[:,i] - 2 * np.sqrt(var[:,i]), '--', color=color)
+		a2.plot(xx, p[:,i], '-', color=color, lw=2)
+
+	a2.set_ylim(-0.1, 1.1)
+	a2.set_yticks([0, 1])
+	a2.set_xticks([])
+
+# REF [site] >> https://gpflow.readthedocs.io/en/latest/notebooks/multiclass.html
+def sparse_variational_gaussian_approximation_example():
+	#%matplotlib inline
+	plt.style.use('ggplot')
+
+	np.random.seed(1)
+
+	# Make a one dimensional classification problem.
+	X = np.random.rand(100, 1)
+	K = np.exp(-0.5 * np.square(X - X.T) / 0.01) + np.eye(100) * 1e-6
+	f = np.dot(np.linalg.cholesky(K), np.random.randn(100, 3))
+	Y = np.array(np.argmax(f, 1).reshape(-1, 1), dtype=float)
+
+	plt.figure(figsize=(12,6))
+	plt.plot(X, f, '.')
+
+	model = gpflow.models.SVGP(
+	    X, Y,
+	    kern=gpflow.kernels.Matern32(1) + gpflow.kernels.White(1, variance=0.01),
+	    likelihood=gpflow.likelihoods.MultiClass(3),
+	    Z=X[::5].copy(), num_latent=3, whiten=True, q_diag=True
+	)
+
+	model.kern.kernels[1].variance.trainable = False
+	model.feature.trainable = False
+	print(model.as_pandas_table())
+
+	opt = gpflow.train.ScipyOptimizer()
+	opt.minimize(model)
+
+	plot(model)
+
+	print(model.kern.as_pandas_table())
+
+def plot_from_samples(m, samples):
+	f = plt.figure(figsize=(12, 6))
+	a1 = f.add_axes([0.05, 0.05, 0.9, 0.6])
+	a2 = f.add_axes([0.05, 0.7, 0.9, 0.1])
+	a3 = f.add_axes([0.05, 0.85, 0.9, 0.1])
+
+	xx = np.linspace(m.X.read_value().min(), m.X.read_value().max(), 200).reshape(-1, 1)
+
+	Fpred, Ypred = [], []
+	for s in samples[100::10].iterrows():  # Burn 100, thin 10.
+		m.assign(s[1])
+		Ypred.append(m.predict_y(xx)[0])
+		Fpred.append(m.predict_f_samples(xx, 1).squeeze())
+
+	for i in range(m.likelihood.num_classes):
+		x = m.X.read_value()[m.Y.read_value().flatten() == i]
+		points, = a3.plot(x, x * 0, '.')
+		color = points.get_color()
+		for F in Fpred:
+			a1.plot(xx, F[:,i], color=color, lw=0.2, alpha=1.0)
+		for Y in Ypred:
+			a2.plot(xx, Y[:,i], color=color, lw=0.5, alpha=1.0)
+
+	a2.set_ylim(-0.1, 1.1)
+	a2.set_yticks([0, 1])
+	a2.set_xticks([])
+
+	a3.set_xticks([])
+	a3.set_yticks([])
+
+def sparse_mcmc_example():
+	#%matplotlib inline
+	plt.style.use('ggplot')
+
+	np.random.seed(1)
+
+	# Make a one dimensional classification problem.
+	X = np.random.rand(100, 1)
+	K = np.exp(-0.5 * np.square(X - X.T) / 0.01) + np.eye(100) * 1e-6
+	f = np.dot(np.linalg.cholesky(K), np.random.randn(100, 3))
+	Y = np.array(np.argmax(f, 1).reshape(-1, 1), dtype=float)
+
+	plt.figure(figsize=(12,  6))
+	plt.plot(X, f, '.')
+
+	with gpflow.defer_build():
+		model = gpflow.models.SGPMC(
+			X, Y,
+			kern=gpflow.kernels.Matern32(1, lengthscales=0.1) + gpflow.kernels.White(1, variance=0.01),
+			likelihood=gpflow.likelihoods.MultiClass(3),
+			Z=X[::5].copy(), num_latent=3
+		)
+		model.kern.kernels[0].variance.prior = gpflow.priors.Gamma(1.0, 1.0)
+		model.kern.kernels[0].lengthscales.prior = gpflow.priors.Gamma(2.0, 2.0)
+		model.kern.kernels[1].variance.trainables = False
+
+	model.compile()
+	print(model.as_pandas_table())
+
+	opt = gpflow.train.ScipyOptimizer()
+	opt.minimize(model, maxiter=10)
+	print(model.kern.as_pandas_table())
+
+	hmc = gpflow.train.HMC()
+	samples = hmc.sample(model, num_samples=500, epsilon=0.04, lmax=15, logprobs=False)  # pands.DataFrame.
+	#print('Columns =', samples.columns.values)
+
+	plot_from_samples(model, samples)
+	print(samples.head())
+
+	_ = plt.hist(np.vstack(samples['SGPMC/kern/kernels/0/lengthscales']).flatten(), 50, density=True)
+	plt.xlabel('lengthscale')
+
+def main():
+	sparse_variational_gaussian_approximation_example()
+	sparse_mcmc_example()
+
+#%%------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
