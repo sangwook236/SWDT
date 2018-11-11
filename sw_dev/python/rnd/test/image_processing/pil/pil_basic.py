c04a768c738ff8ceb6fe91ba7ec514b217b3a78d
Sang-Wook Lee <sangwook236@gmail.com>
2018-11-11 21:36:06 +0900

    [Update, Python] Added an example for Draw in pillow library.

--- sw_dev/python/rnd/test/image_processing/pil/pil_basic.py
+++ sw_dev/python/rnd/test/image_processing/pil/pil_basic.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+import os
 from PIL import Image, ImageDraw, ImageFont
 
 # REF [site] >> http://pillow.readthedocs.io/en/3.1.x/reference/Image.html
-def simple_example():
+def image_example():
 	img_filename = 'D:/dataset/pattern_recognition/street1.png'
 	#img_filename = 'D:/dataset/pattern_recognition/street2.jpg'
 
@@ -11,32 +12,72 @@ def simple_example():
 
 	img.rotate(45).show()
 
-	img.save('tmp.jpg')
+	#img.show()
+	img.save('./pil_image.jpg')
+
+def draw_example():
+	img = Image.new(mode='RGB', size=(500, 500), color=(255, 255, 255))
+	draw = ImageDraw.Draw(img)
+
+	draw.line((50, 50, 400, 400), fill=(255, 0, 255, 0), width=2)
+	draw.rectangle((100, 100, 300, 300), fill=(0, 0, 255, 0), outline=(255, 0, 0, 255))
+
+	#img.show()
+	img.save('./pil_draw.png')
 
 def text_example():
-	img = Image.new(mode='RGB', size=(100, 30), color=(73, 109, 137))
+	img = Image.new(mode='RGB', size=(100, 30), color=(128, 128, 128))
+	draw = ImageDraw.Draw(img)
 
-	d = ImageDraw.Draw(img)
-	d.text(xy=(10, 10), text='Hello World', fill=(255, 255, 0))
+	text = 'Hello World'
+
+	offset = (10, 10)
+	text_size = draw.textsize(text)
+	text_area = (offset[0], offset[1], offset[0] + text_size[0], offset[1] + text_size[1])
+
+	draw.text(xy=offset, text=text, fill=(0, 0, 0))
+
+	# Draws rectangle surrounding text.
+	draw.rectangle(text_area, outline='red', width=1)
+
+	# Crops text area.
+	#img = img.crop(text_area)
 
 	img.save('./pil_text.png')
 
 	#--------------------
-	#font_type = 'C:/Windows/Fonts/Arial.ttf'
-	font_type = 'C:/Windows/Fonts/gulim.ttc'
-	#font_type = '/Library/Fonts/Arial.ttf'
+	if 'posix' == os.name:
+		font_type = '/usr/share/fonts/truetype/gulim.ttf'  # 굴림, 굴림체, 돋움, 돋움체.
+		#font_type = '/usr/share/fonts/truetype/batang.ttf'  # 바탕, 바탕체, 궁서, 궁서체.
+		#font_type = '/usr/share/fonts/truetype/FreeSans.ttf'
+	else:
+		font_type = 'C:/Windows/Fonts/gulim.ttc'  # 굴림, 굴림체, 돋움, 돋움체.
+		#font_type = 'C:/Windows/Fonts/Arial.ttf'
 	font = ImageFont.truetype(font=font_type, size=50, index=0)
 
-	img = Image.new(mode='RGB', size=(200, 100), color=(0, 0, 255))
+	img = Image.new(mode='RGB', size=(200, 100), color=(128, 128, 128))
+	draw = ImageDraw.Draw(img)
 
-	d = ImageDraw.Draw(img)
-	#d.text(xy=(10, 10), text='Hello World', font=font, fill=(255, 255, 255))
-	d.text(xy=(10, 10), text='안녕', font=font, fill=(255, 255, 255))
+	text = '안녕'
+
+	offset = (10, 10)
+	text_size = draw.textsize(text, font=font)
+	font_offset = font.getoffset(text)
+	text_area = (offset[0], offset[1], offset[0] + text_size[0] + font_offset[0], offset[1] + text_size[1] + font_offset[1])
+
+	draw.text(xy=offset, text=text, font=font, fill=(0, 0, 0))
+
+	# Draws rectangle surrounding text.
+	draw.rectangle(text_area, outline='red', width=1)
+
+	# Crops text area.
+	#img = img.crop(text_area)
 
 	img.save('./pil_text_font.png')
 
 def main():
-	#simple_example()
+	#image_example()
+	draw_example()
 	text_example()
 
 #%%------------------------------------------------------------------
