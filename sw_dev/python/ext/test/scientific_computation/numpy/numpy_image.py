8b4378906bb45ee06a06dfb23f5b72a0224c02fa
Sang-Wook Lee <sangwook236@gmail.com>
2018-11-21 13:38:49 +0900

    [Update, Python] Enhanced an example which generates PIL images from numpy array.py

--- sw_dev/python/ext/test/scientific_computation/numpy/numpy_image.py
+++ sw_dev/python/ext/test/scientific_computation/numpy/numpy_image.py
@@ -9,15 +9,22 @@ from PIL import Image
 
 img = Image.open('/path/to/image')
 
-img_array = np.asarray(img, dtype='uint8')
-#img_array = np.uint8(img_array_float * 255)  # For RGB (3-dim).
-#img_array = np.uint8(img_array_float * 255, mode='L')  # For grayscale (2-dim).
+img_array = np.asarray(img, dtype=np.uint8)
+#img_array = (img_array_float * 255).astype(np.uint8)
 
 # Do something.
 
 img = Image.fromarray(img_array)
+#img_gray = Image.fromarray(img_array, mode='L')
+#img_rgb = Image.fromarray(img_array, mode='RGB')
 img.show()
-img.save('tmp.jpg')
+img.save('./tmp.jpg')
+
+img_array_float = np.asarray(img, dtype=np.float)
+img_ubyte = Image.fromarray((img_array_float * 255).astype(np.uint8), mode='L')
+img_ubyte.save('./tmp_ubyte.png')
+img_float = Image.fromarray(img_array_float.astype(np.float32), mode='F')
+img_float.save('./tmp_float.tif')
 
 #%%-------------------------------------------------------------------
 # scipy.
@@ -27,7 +34,7 @@ import scipy.misc
 
 img_array = scipy.ndimage.imread('/path/to/image', mode='RGB')
 #scipy.misc.imshow(img_array)  # Use the environment variable, SCIPY_PIL_IMAGE_VIEWER.
-scipy.misc.imsave('tmp.jpg', img_array)
+scipy.misc.imsave('./tmp.jpg', img_array)
 
 #%%-------------------------------------------------------------------
 # matplotlib.
@@ -37,5 +44,5 @@ import matplotlib.image as mpimg
 
 img_array = mpimg.imread('/path/to/image')
 plt.imshow(img_array)
-plt.imsave('tmp.png', img_array)
-#plt.imsave('tmp_gray.png', img_array, cmap='gray')
+plt.imsave('./tmp.png', img_array)
+#plt.imsave('./tmp_gray.png', img_array, cmap='gray')
