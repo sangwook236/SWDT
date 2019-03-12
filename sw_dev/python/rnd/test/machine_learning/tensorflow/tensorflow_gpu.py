7ec62fd44072da9377f35a3a1c6ae8254f958304
Sang-Wook Lee <sangwook236@gmail.com>
2019-03-12 17:28:35 +0900

    [Update, Python] Added an example to use TensorFlow graph and device assignment simultaneously.

--- sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_gpu.py
+++ sw_dev/python/rnd/test/machine_learning/tensorflow/tensorflow_gpu.py
@@ -1,99 +1,122 @@
+#!/usr/bin/env python
+
 # REF [site] >> https://www.tensorflow.org/tutorials/using_gpu
 
 import tensorflow as tf
 
-#%%-------------------------------------------------------------------
-# Log device placement.
+def log_device_placement():
+	# To find out which devices your operations and tensors are assigned to, create the session with log_device_placement configuration option set to True.
 
-# To find out which devices your operations and tensors are assigned to, create the session with log_device_placement configuration option set to True.
-
-# Creates a graph.
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = tf.matmul(a, b)
-
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-
-# Runs the op.
-print(sess.run(c))
-
-#%%-------------------------------------------------------------------
-# Manual device placement.
-
-# Creates a graph.
-with tf.device('/cpu:0'):
+	# Creates a graph.
 	a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 	b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 	c = tf.matmul(a, b)
 
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+	# Creates a session with log_device_placement set to True.
+	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-# Runs the op.
-print(sess.run(c))
+	# Runs the op.
+	print(sess.run(c))
 
-#%%-------------------------------------------------------------------
-# Allow GPU memory growth.
+def manual_device_placement_1():
+	# Creates a graph.
+	with tf.device('/device:CPU:0'):
+	#with tf.device('/device:GPU:0'):
+		a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+		b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+		c = tf.matmul(a, b)
 
-# Attempt to allocate only as much GPU memory based on runtime allocations:
-# it starts out allocating very little memory, and as Sessions get run and more GPU memory is needed, we extend the GPU memory region needed by the TensorFlow process.
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-#session = tf.Session(config=config, ...)
+	# Creates a session with log_device_placement set to True.
+	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-# Bound the amount of GPU memory available to the TensorFlow process.
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
-#session = tf.Session(config=config, ...)
+	# Runs the op.
+	print(sess.run(c))
 
-#%%-------------------------------------------------------------------
-# Use a single GPU on a multi-GPU system.
+def manual_device_placement_2():
+	graph = tf.Graph()
 
-# If the device you have specified does not exist, you will get InvalidArgumentError.
+	# Creates a graph.
+	with graph.as_default():
+		with tf.device('/device:GPU:1'):
+			a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+			b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+			c = tf.matmul(a, b)
 
-# Creates a graph.
-with tf.device('/gpu:2'):
-	a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-	b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-	c = tf.matmul(a, b)
+	# Creates a session with log_device_placement set to True.
+	sess = tf.Session(graph=graph, config=tf.ConfigProto(log_device_placement=True))
 
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+	# Runs the op.
+	print(sess.run(c))
 
-# Runs the op.
-print(sess.run(c))
+def allow_gpu_memory_growth():
+	# Attempt to allocate only as much GPU memory based on runtime allocations:
+	# it starts out allocating very little memory, and as Sessions get run and more GPU memory is needed, we extend the GPU memory region needed by the TensorFlow process.
+	config = tf.ConfigProto()
+	config.gpu_options.allow_growth = True
+	#session = tf.Session(config=config, ...)
 
-# If you would like TensorFlow to automatically choose an existing and supported device to run the operations in case the specified one doesn't exist, 
-# set allow_soft_placement to True in the configuration option when creating the session.
+	# Bound the amount of GPU memory available to the TensorFlow process.
+	config = tf.ConfigProto()
+	config.gpu_options.per_process_gpu_memory_fraction = 0.4
+	#session = tf.Session(config=config, ...)
 
-# Creates a graph.
-with tf.device('/gpu:2'):
-	a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-	b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-	c = tf.matmul(a, b)
+def use_a_single_gpu_on_a_multi_gpu_system():
+	# If the device you have specified does not exist, you will get InvalidArgumentError.
 
-# Creates a session with allow_soft_placement and log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
+	# Creates a graph.
+	with tf.device('/gpu:2'):
+		a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+		b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+		c = tf.matmul(a, b)
 
-# Runs the op.
-print(sess.run(c))
+	# Creates a session with log_device_placement set to True.
+	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-#%%-------------------------------------------------------------------
-# Use multiple GPUs.
+	# Runs the op.
+	print(sess.run(c))
 
-# Creates a graph.
-c = []
-for d in ['/gpu:2', '/gpu:3']:
-	with tf.device(d):
-		a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
-		b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
-		c.append(tf.matmul(a, b))
-with tf.device('/cpu:0'):
-    sum = tf.add_n(c)
+	# If you would like TensorFlow to automatically choose an existing and supported device to run the operations in case the specified one doesn't exist, 
+	# set allow_soft_placement to True in the configuration option when creating the session.
 
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+	# Creates a graph.
+	with tf.device('/gpu:2'):
+		a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+		b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+		c = tf.matmul(a, b)
 
-# Runs the op.
-print(sess.run(sum))
+	# Creates a session with allow_soft_placement and log_device_placement set to True.
+	sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
+
+	# Runs the op.
+	print(sess.run(c))
+
+def use_multiple_gpus():
+	# Creates a graph.
+	c = []
+	for d in ['/gpu:2', '/gpu:3']:
+		with tf.device(d):
+			a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
+			b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
+			c.append(tf.matmul(a, b))
+	with tf.device('/cpu:0'):
+		sum = tf.add_n(c)
+
+	# Creates a session with log_device_placement set to True.
+	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+	# Runs the op.
+	print(sess.run(sum))
+
+def main():
+	log_device_placement()
+	manual_device_placement_1()
+	manual_device_placement_2()
+
+	allow_gpu_memory_growth()
+	use_a_single_gpu_on_a_multi_gpu_system()
+	use_multiple_gpus()
+
+#%%------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
