41661be6ac079d40089a0eca1bf12c98a6a0d1f7
Sang-Wook Lee <sangwook236@gmail.com>
2019-09-11 01:26:42 +0900

    [Update, Text] Updated the usage guides for ocropy and clstm libraries. [New, Text] Added an usage guide for kraken library.

--- sw_dev/python/rnd/test/signal_processing/librosa_test.py
+++ sw_dev/python/rnd/test/signal_processing/librosa_test.py
@@ -23,14 +23,16 @@ def mel_filter_bank_test():
 # REF [site] >> https://librosa.github.io/librosa/generated/librosa.feature.melspectrogram.html
 def melspectrogram_test():
 	y, sr = librosa.load(librosa.util.example_audio_file())
-	librosa.feature.melspectrogram(y=y, sr=sr)
 
-	D = np.abs(librosa.stft(y))**2
-	S = librosa.feature.melspectrogram(S=D, sr=sr)
-
-	# Passing through arguments to the Mel filters.
-	S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-	#S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=512, n_mels=128, fmax=8000)
+	if False:
+		S = librosa.feature.melspectrogram(y=y, sr=sr)
+	elif False:
+		D = np.abs(librosa.stft(y))**2
+		S = librosa.feature.melspectrogram(S=D, sr=sr)
+	else:
+		# Passing through arguments to the Mel filters.
+		S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+		#S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=512, n_mels=128, fmax=8000)
 
 	#--------------------
 	plt.figure(figsize=(10, 4))
