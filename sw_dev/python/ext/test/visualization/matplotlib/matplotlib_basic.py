31fdbd51da5a261e1e81c3785a2b1992423fa914
Sang-Wook Lee <sangwook236@gmail.com>
2019-11-08 19:48:03 +0900

    [Update, Python] Added examples of bar chart & histogram in matplotlib.

--- sw_dev/python/ext/test/visualization/matplotlib/matplotlib_basic.py
+++ sw_dev/python/ext/test/visualization/matplotlib/matplotlib_basic.py
@@ -171,6 +171,58 @@ def logarithmic_and_other_nonlinear_axes():
 
 	plt.show()
 
+def bar_chart_example():
+	langs = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+	performance = [10, 8, 6, 4, 2, 1]
+	x_pos = np.arange(len(langs))
+
+	plt.bar(x_pos, performance, align='center', alpha=0.5)
+	plt.xticks(x_pos, langs)
+	plt.ylabel('Usage')
+	plt.title('Programming language usage')
+
+	plt.show()
+
+# REF [site] >> https://matplotlib.org/3.1.1/gallery/statistics/hist.html
+def histogram_example():
+	# Generate data and plot a simple histogram
+	N_points = 100000
+	n_bins = 20
+
+	# Generate a normal distribution, center at x=0 and y=5.
+	x = np.random.randn(N_points)
+	y = .4 * x + np.random.randn(100000) + 5
+
+	fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+	# We can set the number of bins with the 'bins' kwarg.
+	axs[0].hist(x, bins=n_bins)
+	axs[1].hist(y, bins=n_bins)
+
+	#--------------------
+	# Update histogram colors.
+	fig, axs = plt.subplots(1, 2, tight_layout=True)
+
+	# N is the count in each bin, bins is the lower-limit of the bin.
+	N, bins, patches = axs[0].hist(x, bins=n_bins)
+
+	# We'll color code by height, but you could use any scalar.
+	fracs = N / N.max()
+
+	# we need to normalize the data to 0..1 for the full range of the colormap.
+	norm = colors.Normalize(fracs.min(), fracs.max())
+
+	# Now, we'll loop through our objects and set the color of each accordingly.
+	for thisfrac, thispatch in zip(fracs, patches):
+		color = plt.cm.viridis(norm(thisfrac))
+		thispatch.set_facecolor(color)
+
+	# We can also normalize our inputs by the total number of counts.
+	axs[1].hist(x, bins=n_bins, density=True)
+
+	# Now we format the y-axis to display percentage.
+	axs[1].yaxis.set_major_formatter(PercentFormatter(xmax=1))
+
 def main():
 	#basic()
 	#control_line_properties()
@@ -179,6 +231,9 @@ def main():
 	#work_with_text()
 
 	#logarithmic_and_other_nonlinear_axes()
+
+	bar_chart_example()
+	histogram_example()
 
 #--------------------------------------------------------------------
 
