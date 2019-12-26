f134f1dd74ff4ddd21a4fedd348a73b11a64ef56
Sang-Wook Lee <sangwook236@gmail.com>
2019-12-26 23:39:57 +0900

    [Update, Python] Implemented an example of masked language modeling for BERT model.

--- sw_dev/python/rnd/test/language_processing/pytorch_transformers_test.py
+++ sw_dev/python/rnd/test/language_processing/pytorch_transformers_test.py
@@ -5,6 +5,7 @@
 
 import torch
 from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
+from pytorch_transformers import BertTokenizer, BertModel, BertForMaskedLM
 
 # REF [site] >> https://medium.com/analytics-vidhya/a-comprehensive-guide-to-build-your-own-language-model-in-python-5141b3917d6d
 def sentence_completion_model_using_gpt2_example():
@@ -47,9 +48,54 @@ def sentence_completion_model_using_gpt2_example():
 def conditional_text_teneration_using_gpt2_example():
 	raise NotImplementedError
 
+# REF [site] >> https://www.analyticsvidhya.com/blog/2019/07/pytorch-transformers-nlp-python/?utm_source=blog&utm_medium=openai-gpt2-text-generator-python
+def masked_language_modeling_for_BERT_example():
+	# Load pre-trained model tokenizer (vocabulary).
+	tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+	# Tokenize input.
+	text = '[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]'
+	tokenized_text = tokenizer.tokenize(text)
+
+	# Mask a token that we will try to predict back with 'BertForMaskedLM'.
+	masked_index = 8
+	tokenized_text[masked_index] = '[MASK]'
+	assert tokenized_text == ['[CLS]', 'who', 'was', 'jim', 'henson', '?', '[SEP]', 'jim', '[MASK]', 'was', 'a', 'puppet', '##eer', '[SEP]']
+
+	# Convert token to vocabulary indices.
+	indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+	# Define sentence A and B indices associated to 1st and 2nd sentences (see paper).
+	segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+
+	# Convert inputs to PyTorch tensors.
+	tokens_tensor = torch.tensor([indexed_tokens])
+	segments_tensors = torch.tensor([segments_ids])
+
+	# Load pre-trained model (weights).
+	model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+	model.eval()
+
+	# If you have a GPU, put everything on cuda.
+	tokens_tensor = tokens_tensor.to('cuda')
+	segments_tensors = segments_tensors.to('cuda')
+	model.to('cuda')
+
+	# Predict all tokens.
+	with torch.no_grad():
+		outputs = model(tokens_tensor, token_type_ids=segments_tensors)
+		predictions = outputs[0]
+
+	# Confirm we were able to predict 'henson'.
+	predicted_index = torch.argmax(predictions[0, masked_index]).item()
+	predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+	assert predicted_token == 'henson'
+	print('Predicted token is:', predicted_token)
+
 def main():
 	#sentence_completion_model_using_gpt2_example()
-	conditional_text_teneration_using_gpt2_example()
+	#conditional_text_teneration_using_gpt2_example()
+
+	masked_language_modeling_for_BERT_example()
 
 #--------------------------------------------------------------------
 
