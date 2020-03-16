5a3a119f9e50ed11b53927edc71371a91c5c686e
Sang-Wook Lee <sangwook236@gmail.com>
2020-03-16 13:30:36 +0900

    [Update, Python] Moved the examples and the usage guide for pdfplumber library from python/rnd to python/ext. [New, Python] Implemented a simple example for openpyxl library.

---
+++ sw_dev/python/ext/test/documentation/openpyxl_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# REF [site] >> https://bitbucket.org/openpyxl/openpyxl/src/default/
+
+import openpyxl
+
+# REF [site] >> https://bitbucket.org/openpyxl/openpyxl/src/default/
+def basic_example():
+	wb = Workbook()
+
+	# grab the active worksheet.
+	ws = wb.active
+
+	# Data can be assigned directly to cells.
+	ws['A1'] = 42
+
+	# Rows can also be appended.
+	ws.append([1, 2, 3])
+
+	# Python types will automatically be converted.
+	import datetime
+	ws['A2'] = datetime.datetime.now()
+
+	# Save the file.
+	wb.save('./sample.xlsx')
+
+def main():
+	basic_example()
+
+#--------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
