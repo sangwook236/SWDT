c7eeccba6fe5c07657758ea59037bd0ca70ac16b
Sang-Wook Lee <sangwook236@gmail.com>
2020-07-16 01:54:19 +0900

    [New, Python] Implemented a simple example for CTranslate2 library.

---
+++ sw_dev/python/rnd/test/language_processing/ctranslate2_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import time
+import ctranslate2
+
+def quickstart_example():
+	# Convert a model trained with OpenNMT-py or OpenNMT-tf.
+	#	REF [site] >> https://github.com/OpenNMT/CTranslate2
+
+	#--------------------
+	translator = ctranslate2.Translator('ende_ctranslate2/')
+
+	print('Start translating...')
+	start_time = time.time()
+	# Translate tokenized inputs.
+	translated = translator.translate_batch([['▁H', 'ello', '▁world', '!']])
+	print('End translating: {} secs.'.format(time.time() - start_time))
+
+	print('Tokens: {}.'.format(translated[0][0]['tokens']))
+	print('Score = {}.'.format(translated[0][0]['score']))
+
+def main():
+	quickstart_example()
+
+#--------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
