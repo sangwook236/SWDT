315103427689922d966e83583ceec8b8f7d467d1
Sang-Wook Lee <sangwook236@gmail.com>
2020-08-07 14:54:38 +0900

    [Update, Python] Strengthened the test for CSV/TSV interfacing.

--- sw_dev/python/basic/test/python_basic/file_test.py
+++ sw_dev/python/basic/test/python_basic/file_test.py
@@ -12,10 +12,10 @@ def basic():
 			data = fd.read()
 
 		words = data.split()
-	except FileNotFoundError as ex:
-		print('File not found: {}.'.format(filepath))
 	except UnicodeDecodeError as ex:
-		print('Unicode decode error: {}.'.format(filepath))
+		print('Unicode decode error in {}: {}.'.format(filepath, ex))
+	except FileNotFoundError as ex:
+		print('File not found, {}: {}.'.format(filepath, ex))
 
 	filepath = 'data_copyed.txt'
 	try:
@@ -23,32 +23,32 @@ def basic():
 		#with open(filepath, 'w+', encoding='UTF8') as fd:
 		#with open(filepath, 'wb', encoding='UTF8') as fd:
 			fd.write(data)
-	except FileNotFoundError as ex:
-		print('File not found: {}.'.format(filepath))
 	except UnicodeDecodeError as ex:
-		print('Unicode decode error: {}.'.format(filepath))
+		print('Unicode decode error in {}: {}.'.format(filepath, ex))
+	except FileNotFoundError as ex:
+		print('File not found, {}: {}.'.format(filepath, ex))
 
 	filepath = 'data_copyed.txt'
 	try:
 		with open(filepath, 'a', encoding='UTF8') as fd:
 			fd.write(data)
-	except FileNotFoundError as ex:
-		print('File not found: {}.'.format(filepath))
 	except UnicodeDecodeError as ex:
-		print('Unicode decode error: {}.'.format(filepath))
+		print('Unicode decode error in {}: {}.'.format(filepath, ex))
+	except FileNotFoundError as ex:
+		print('File not found, {}: {}.'.format(filepath, ex))
 
 	filepath = 'data.txt'
 	try:
-		with open(filepath, 'r') as fd:
+		with open(filepath, 'r', encoding='UTF8') as fd:
 			#lines = fd.read()  # A string.
 			#lines = fd.read().strip('\n')  # A string.
 			#lines = fd.read().replace(' ', '')  # A string.
 			#lines = fd.readlines()  # A list of strings.
 			lines = fd.read().splitlines()  # A list of strings.
-	except FileNotFoundError as ex:
-		print('File not found: {}.'.format(filepath))
 	except UnicodeDecodeError as ex:
-		print('Unicode decode error: {}.'.format(filepath))
+		print('Unicode decode error in {}: {}.'.format(filepath, ex))
+	except FileNotFoundError as ex:
+		print('File not found, {}: {}.'.format(filepath, ex))
 
 	lines2 = list()
 	for line in lines:
@@ -65,31 +65,52 @@ def glob_example():
 	filepaths = glob.glob('**/*.txt', recursive=True)
 	filepaths = glob.glob('./**/', recursive=True)
 
+# REF [site] >> https://docs.python.org/3/library/csv.html
 def csv_example():
-	csv_filepath = './test.csv'
+	if False:
+		# For a CSV file.
+		filepath = './test.csv'
+		delimiter = ','
+	else:
+		# For a TSV file.
+		filepath = './test.tsv'
+		delimiter = '\t'
+	quoting = csv.QUOTE_MINIMAL  # {csv.QUOTE_ALL, csv.QUOTE_NONNUMERIC, csv.QUOTE_MINIMAL}.
+	quotechar = '"'
+	escapechar = '\\'
 
 	try:
-		#with open(csv_filepath, 'w', encoding='UTF8') as csvfile:
-		with open(csv_filepath, 'w', newline='', encoding='UTF8') as csvfile:
-			writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		#with open(filepath, 'w', encoding='UTF8') as fd:
+		with open(filepath, 'w', newline='', encoding='UTF8') as fd:
+			writer = csv.writer(fd, delimiter=delimiter, quotechar=quotechar, quoting=quoting)
+			#writer = csv.writer(fd, delimiter=delimiter, quotechar=quotechar, doublequote=False, escapechar=escapechar, quoting=quoting)
+			#writer = csv.writer(fd, delimiter=delimiter, escapechar=escapechar, quoting=csv.QUOTE_NONE)
+			#writer = csv.writer(fd, delimiter=delimiter, quotechar=None, escapechar=escapechar, quoting=csv.QUOTE_NONE)
 
 			#writer.writerow(['id', 'lowercase1', 'uppercase1', 'lowercase2', 'uppercase2', 'class'])  # Writes a header.
 
-			writer.writerow([1, 'a', 'A', 'aa,bb', 'AA,BB', 0])
+			writer.writerow([1, 'a', 'A', 'aa,"bb', 'AA,BB', 0])
 			writer.writerow([2, 'b', 'B', 'bb,cc', 'BB,CC', 1])
 			writer.writerow([3, 'c', 'C', 'cc,dd', 'CC,DD', 0])
 			writer.writerow([4, 'd', 'D', 'dd,ee', 'DD,EE', 1])
 			writer.writerow([5, 'e', 'E', 'ee,ff', 'EE,FF', 0])
+	except csv.Error as ex:
+		print('csv.Error in {}: {}.'.format(filepath, ex))
+	except UnicodeDecodeError as ex:
+		print('Unicode decode error in {}: {}.'.format(filepath, ex))
 	except FileNotFoundError as ex:
-		print('File not found: {}.'.format(csv_filepath))
+		print('File not found, {}: {}.'.format(filepath, ex))
 
 	try:
-		#with open(csv_filepath, 'r', encoding='UTF8') as fd:
-		with open(csv_filepath, 'r', newline='', encoding='UTF8') as fd:
+		#with open(filepath, 'r', encoding='UTF8') as fd:
+		with open(filepath, 'r', newline='', encoding='UTF8') as fd:
 			has_header = csv.Sniffer().has_header(fd.read(1024))
 			fd.seek(0)  # Rewind.
 
-			reader = csv.reader(fd, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			reader = csv.reader(fd, delimiter=delimiter, quotechar=quotechar, quoting=quoting)
+			#reader = csv.reader(fd, delimiter=delimiter, quotechar=quotechar, doublequote=False, escapechar=escapechar, quoting=quoting)
+			#reader = csv.reader(fd, delimiter=delimiter, escapechar=escapechar, quoting=csv.QUOTE_NONE)
+			#reader = csv.reader(fd, delimiter=delimiter, quotechar=None, escapechar=escapechar, quoting=csv.QUOTE_NONE)
 			if has_header:
 				header = next(reader, None)  # Reads a header.
 				print('Header = {}.'.format(header))
@@ -97,8 +118,12 @@ def csv_example():
 				print('No header.')
 			for idx, row in enumerate(reader):
 				print('Row {}: {}.'.format(idx, row))
+	except csv.Error as ex:
+		print('csv.Error in {}: {}.'.format(filepath, ex))
+	except UnicodeDecodeError as ex:
+		print('Unicode decode error in {}: {}.'.format(filepath, ex))
 	except FileNotFoundError as ex:
-		print('File not found: {}.'.format(csv_filepath))
+		print('File not found, {}: {}.'.format(filepath, ex))
 
 def main():
 	#basic()
