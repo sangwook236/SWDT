a633153c87489cdd253830c1c820b030bd6d4f94
Sang-Wook Lee <sangwook236@gmail.com>
2020-08-09 13:51:23 +0900

    [Update, Python] Implemented redirection of print() function for logging.

--- sw_dev/python/ext/test/logging/logging_main.py
+++ sw_dev/python/ext/test/logging/logging_main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-import os, argparse, logging, logging.handlers
+import sys, os, argparse, logging, logging.handlers
 
-def get_logger(name, log_level, is_rotating=True):
-	if not os.path.isdir('log'):
-		os.mkdir('log')
+# Define a global variable, print.
+print = print
 
-	log_filepath = './log/' + (name if name else 'swdt') + '.log'
+def get_logger(log_filepath, log_level, is_rotating=True):
 	if is_rotating:
 		file_handler = logging.handlers.RotatingFileHandler(log_filepath, maxBytes=10000000, backupCount=10)
 	else:
@@ -27,12 +27,22 @@ def get_logger(name, log_level, is_rotating=True):
 
 def output_logs(logger):
 	#logger.log(logging.WARNING, '[Warning] Warning.')
-	logger.debug('[Debug] Debug.')
-	logger.info('[Info] Info.')
-	logger.warning('[Warning] Warning.')
-	logger.error('[Error] Error.')
-	logger.critical('[Critical] Critical.')
-	logger.exception('[Exception] Exception.')
+	logger.debug('[logger] Debug.')
+	logger.info('[logger] Info.')
+	logger.warning('[logger] Warning.')
+	logger.error('[logger] Error.')
+	logger.critical('[logger] Critical.')
+	try:
+		raise Exception('Test exception')
+	except Exception as ex:
+		logger.exception('[logger] Exception.')
+
+	#print = logger.info  # Locally applied.
+
+	print('[sys.stdout] Print')
+	print('[sys.stderr] Print', file=sys.stderr)  # TypeError: _log() got an unexpected keyword argument 'file'.
+	#print('[sys.stdout] Print', 'Print2')  # TypeError: not all arguments converted during string formatting.
+	#print('[sys.stderr] Print', 'Print2', file=sys.stderr)  # TypeError: not all arguments converted during string formatting.
 
 def simple_logging(log_level):
 	#format = '%(asctime)-15s %(clientip)s %(user)-8s: %(message)s'
@@ -91,17 +101,43 @@ def main():
 		if not isinstance(log_level, int):
 			raise ValueError('Invalid log level: {}'.format(log_level))
 	else:
-		log_level = logging.WARNING
-	print('Log level:', log_level)
+		#log_level = logging.WARNING
+		log_level = logging.DEBUG
 
-	logger = get_logger(os.path.basename(os.path.normpath(__file__)), log_level, is_rotating=True)
+	log_dir_path = './log'
+	if not os.path.isdir(log_dir_path):
+		os.mkdir(log_dir_path)
+
+	# Define a logger.
+	log_filename = os.path.basename(os.path.normpath(__file__))
+	log_filepath = os.path.join(log_dir_path, '{}.log'.format(log_filename if log_filename else 'swdt'))
+	logger = get_logger(log_filepath, log_level, is_rotating=True)
+
+	# Redirection of print().
+	if True:
+		#fd_stdout = open(os.path.join(log_dir_path, 'stdout.log'), 'a', encoding='utf-8')
+		#sys.stdout = fd_stdout
+		#fd_stderr = open(os.path.join(log_dir_path, 'stderr.log'), 'a', encoding='utf-8')
+		#sys.stderr = fd_stderr
+
+		# NOTE [error] >> If logging.StreamHandler is set, recursion error will occur.
+		#sys.stdout.write = logger.info
+		#sys.stderr.write = logger.error
+
+		global print
+		#print = logger.info
+		print = lambda *objects, sep=' ', end='\n', file=sys.stdout, flush=False: logger.error(*objects) if file == sys.stderr else logger.info(*objects)
+
+	#--------------------
+	print('Log level: {}.'.format(log_level))
+
 	output_logs(logger)
 
 	#simple_logging(log_level)
 	#simple_file_logging(log_level)
 	simple_logging_in_multiple_functions(log_level)
 
-#%%------------------------------------------------------------------
+#--------------------------------------------------------------------
 
 # Usage:
 #	python logging_main.py --loglevel=DEBUG
