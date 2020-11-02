dfed7783c7709d8760394b2be2f1701adec2b53e
Sang-Wook Lee <sangwook236@gmail.com>
2020-11-02 11:04:49 +0900

    [Update, Python] Generalized the example of OpenCV geometric transformation.

--- sw_dev/python/rnd/test/machine_vision/opencv/opencv_transformation.py
+++ sw_dev/python/rnd/test/machine_vision/opencv/opencv_transformation.py
@@ -73,115 +73,138 @@ def geometric_transformation():
 	plt.show()
 
 def ellipse_transformation_example():
-	rgb = np.zeros((500, 500, 3), np.uint8)
-	cv.ellipse(rgb, (300, 200), (100, 50), 30, 0, 360, (255, 0, 255), cv.FILLED, cv.LINE_8)
+	#for rangle in range(0, 360, 10):
+	for rangle in range(-90, 90, 10):
+		rgb = np.zeros((500, 500, 3), np.uint8)
+		cv.ellipse(rgb, (300, 200), (100, 50), rangle, 0, 360, (127, 127, 127), cv.FILLED, cv.LINE_8)
 
-	pts = np.stack(np.nonzero(rgb)).transpose()[:,[1, 0]]
+		pts = np.stack(np.nonzero(rgb)).transpose()[:,[1, 0]]
 
-	# Rotated rectangle (OBB).
-	obb = cv.minAreaRect(pts)  # Tuple: (center, size, angle).
-	obb_pts = cv.boxPoints(obb)  # 4 x 2. np.float32.
-	#obb_pts = np.int0(obb_pts)
+		# Rotated rectangle (OBB).
+		obb = cv.minAreaRect(pts)  # Tuple: (center, size, angle).
+		obb_center, obb_size, obb_angle = obb
+		obb_pts = cv.boxPoints(obb)  # 4 x 2. np.float32.
+		#obb_pts = np.int0(obb_pts)
 
-	obb_center, obb_size, obb_angle = obb
-	obb_size, obb_angle = obb_size[1::-1], obb_angle + 90
+		# Straight bounding rectangle (AABB).
+		aabb = cv.boundingRect(pts)  # x (left), y (top), width, height.
+		aabb_x1, aabb_y1, aabb_x2, aabb_y2 = aabb[0], aabb[1], aabb[0] + aabb[2], aabb[1] + aabb[3]
 
-	# Straight bounding rectangle (AABB).
-	aabb = cv.boundingRect(pts)  # x (left), y (top), width, height.
-	aabb_x1, aabb_y1, aabb_x2, aabb_y2 = aabb[0], aabb[1], aabb[0] + aabb[2], aabb[1] + aabb[3]
+		# The largest bbox.
+		(left, top), (right, bottom) = np.min(obb_pts, axis=0), np.max(obb_pts, axis=0)
 
-	# The largest bbox.
-	(left, top), (right, bottom) = np.min(obb_pts, axis=0), np.max(obb_pts, axis=0)
+		cv.rectangle(rgb, (left, top), (right, bottom), (255, 255, 0), 2, cv.LINE_8)
+		cv.rectangle(rgb, (aabb_x1, aabb_y1), (aabb_x2, aabb_y2), (0, 255, 255), 2, cv.LINE_8)
+		if False:
+			cv.drawContours(rgb, [np.int0(obb_pts)], 0, (0, 0, 255), 2, cv.LINE_8)
+		else:
+			cv.line(rgb, (math.floor(obb_pts[1,0]), math.floor(obb_pts[1,1])), (math.floor(obb_pts[2,0]), math.floor(obb_pts[2,1])), (0, 0, 255), 2, cv.LINE_8)
+			cv.line(rgb, (math.floor(obb_pts[2,0]), math.floor(obb_pts[2,1])), (math.floor(obb_pts[3,0]), math.floor(obb_pts[3,1])), (0, 255, 0), 2, cv.LINE_8)
+			cv.line(rgb, (math.floor(obb_pts[3,0]), math.floor(obb_pts[3,1])), (math.floor(obb_pts[0,0]), math.floor(obb_pts[0,1])), (255, 0, 0), 2, cv.LINE_8)
+			cv.line(rgb, (math.floor(obb_pts[0,0]), math.floor(obb_pts[0,1])), (math.floor(obb_pts[1,0]), math.floor(obb_pts[1,1])), (255, 0, 255), 2, cv.LINE_8)
 
-	cv.rectangle(rgb, (left, top), (right, bottom), (255, 0, 0), 2, cv.LINE_8)
-	cv.rectangle(rgb, (aabb_x1, aabb_y1), (aabb_x2, aabb_y2), (0, 255, 0), 2, cv.LINE_8)
-	cv.drawContours(rgb, [np.int0(obb_pts)], 0, (0, 0, 255), 2, cv.LINE_8)
+		cv.imshow('Input Image', rgb)
 
-	cv.imshow('Input Image', rgb)
+		#--------------------
+		# Rotation.
 
-	#--------------------
-	# Rotation.
+		rot_size, rot_angle = obb_size, obb_angle
+		# TODO [check] >>
+		if obb_size[0] >= obb_size[1]:
+		#if obb_angle < -10 or obb_angle > 10:
+			rot_size, rot_angle = obb_size[1::-1], obb_angle + 90
 
-	if False:
-		# Too big.
-		dia = math.ceil(math.sqrt(rgb.shape[0]**2 + rgb.shape[1]**2))
-		R = cv.getRotationMatrix2D(obb_center, angle=obb_angle, scale=1)
-		rotated = cv.warpAffine(rgb, R, (dia, dia), flags=cv.INTER_LINEAR)
+		if False:
+			# Too big.
+			dia = math.ceil(math.sqrt(rgb.shape[0]**2 + rgb.shape[1]**2))
+			R = cv.getRotationMatrix2D(obb_center, angle=rot_angle, scale=1)
+			rotated = cv.warpAffine(rgb, R, (dia, dia), flags=cv.INTER_LINEAR)
 
-		rotated_patch = rotated[math.floor(obb_center[1] - obb_size[1] / 2):math.ceil(obb_center[1] + obb_size[1] / 2), math.floor(obb_center[0] - obb_size[0] / 2):math.ceil(obb_center[0] + obb_size[0] / 2)]
-	elif False:
-		# Trimmed.
+			rotated_patch = rotated[math.floor(obb_center[1] - rot_size[1] / 2):math.ceil(obb_center[1] + rot_size[1] / 2), math.floor(obb_center[0] - rot_size[0] / 2):math.ceil(obb_center[0] + rot_size[0] / 2)]
+		elif False:
+			# Trimmed.
+			patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1) - 1), max(0, math.floor(aabb_y1) - 1), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
+			#patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1)), max(0, math.floor(aabb_y1)), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
+			patch = rgb[patch_y1:patch_y2, patch_x1:patch_x2]
+
+			ctr = patch.shape[1] / 2, patch.shape[0] / 2
+			dia = math.ceil(math.sqrt(patch.shape[0]**2 + patch.shape[1]**2))
+			R = cv.getRotationMatrix2D(ctr, angle=rot_angle, scale=1)
+			rotated = cv.warpAffine(patch, R, (dia, dia), flags=cv.INTER_LINEAR)
+
+			rotated_patch = rotated[:patch.shape[0],:patch.shape[1]]
+		else:
+			radius = math.sqrt(rot_size[0]**2 + rot_size[1]**2) / 2
+			dia = math.ceil(radius * 2)
+
+			patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(obb_center[0] - radius) - 1), max(0, math.floor(obb_center[1] - radius) - 1), min(rgb.shape[1], math.ceil(obb_center[0] + radius) + 1), min(rgb.shape[0], math.ceil(obb_center[1] + radius) + 1)
+			#patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(obb_center[0] - radius)), max(0, math.floor(obb_center[1] - radius)), min(rgb.shape[1], math.ceil(obb_center[0] + radius) + 1), min(rgb.shape[0], math.ceil(obb_center[1] + radius) + 1)
+			patch = rgb[patch_y1:patch_y2, patch_x1:patch_x2]
+
+			ctr = patch.shape[1] / 2, patch.shape[0] / 2
+			R = cv.getRotationMatrix2D(ctr, angle=rot_angle, scale=1)
+			rotated = cv.warpAffine(patch, R, (dia, dia), flags=cv.INTER_LINEAR)
+
+			rotated_patch = rotated[math.floor(ctr[1] - rot_size[1] / 2):math.ceil(ctr[1] + rot_size[1] / 2), math.floor(ctr[0] - rot_size[0] / 2):math.ceil(ctr[0] + rot_size[0] / 2)]
+
+		cv.imshow('Rotated Image (OBB)', rotated)
+		cv.imshow('Rotated Image (OBB, Patched)', rotated_patch)
+
 		patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1) - 1), max(0, math.floor(aabb_y1) - 1), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
 		#patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1)), max(0, math.floor(aabb_y1)), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
 		patch = rgb[patch_y1:patch_y2, patch_x1:patch_x2]
 
 		ctr = patch.shape[1] / 2, patch.shape[0] / 2
-		dia = math.ceil(math.sqrt(patch.shape[0]**2 + patch.shape[1]**2))
-		R = cv.getRotationMatrix2D(ctr, angle=obb_angle, scale=1)
-		rotated = cv.warpAffine(patch, R, (dia, dia), flags=cv.INTER_LINEAR)
+		#ctr = tuple(np.array(patch.shape[1::-1]) / 2)
+		R = cv.getRotationMatrix2D(ctr, angle=rot_angle, scale=1)
+		rotated = cv.warpAffine(patch, R, (300, 150), flags=cv.INTER_LINEAR)
 
-		rotated_patch = rotated[:patch.shape[0],:patch.shape[1]]
-	else:
-		radius = math.sqrt(obb_size[0]**2 + obb_size[1]**2) / 2
-		dia = math.ceil(radius * 2)
+		cv.imshow('Rotated Image (AABB)', rotated)
 
-		patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(obb_center[0] - radius) - 1), max(0, math.floor(obb_center[1] - radius) - 1), min(rgb.shape[1], math.ceil(obb_center[0] + radius) + 1), min(rgb.shape[0], math.ceil(obb_center[1] + radius) + 1)
-		#patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(obb_center[0] - radius)), max(0, math.floor(obb_center[1] - radius)), min(rgb.shape[1], math.ceil(obb_center[0] + radius) + 1), min(rgb.shape[0], math.ceil(obb_center[1] + radius) + 1)
-		patch = rgb[patch_y1:patch_y2, patch_x1:patch_x2]
+		import scipy.ndimage
+		rotated_scipy = scipy.ndimage.rotate(patch, rot_angle, reshape=True)
+		ctr = rotated_scipy.shape[1] / 2, rotated_scipy.shape[0] / 2
+		rotated_scipy_patch = rotated_scipy[math.floor(ctr[1] - rot_size[1] / 2):math.ceil(ctr[1] + rot_size[1] / 2), math.floor(ctr[0] - rot_size[0] / 2):math.ceil(ctr[0] + rot_size[0] / 2)]
 
-		ctr = patch.shape[1] / 2, patch.shape[0] / 2
-		R = cv.getRotationMatrix2D(ctr, angle=obb_angle, scale=1)
-		rotated = cv.warpAffine(patch, R, (dia, dia), flags=cv.INTER_LINEAR)
+		cv.imshow('Rotated Image (scipy)', rotated_scipy)
+		cv.imshow('Rotated Image (scipy, Patch)', rotated_scipy_patch)
 
-		rotated_patch = rotated[math.floor(ctr[1] - obb_size[1] / 2):math.ceil(ctr[1] + obb_size[1] / 2), math.floor(ctr[0] - obb_size[0] / 2):math.ceil(ctr[0] + obb_size[0] / 2)]
+		#--------------------
+		# Affine transformation.
 
-	cv.imshow('Rotated Image (OBB)', rotated)
-	cv.imshow('Rotated Image (OBB, patched)', rotated_patch)
+		if False:
+			if obb_size[0] >= obb_size[1]:
+				target_pts = np.float32([[0, 100], [0, 0], [100, 0], [100, 100]])
+			else:
+				target_pts = np.float32([[100, 100], [0, 100], [0, 0], [100, 0]])
+			canvas_size = 300, 150
+		else:
+			if obb_size[0] >= obb_size[1]:
+				target_pts = np.float32([[0, obb_size[1]], [0, 0], [obb_size[0], 0], [obb_size[0], obb_size[1]]])
+				canvas_size = round(obb_size[0]), round(obb_size[1])
+			else:
+				target_pts = np.float32([[obb_size[1], obb_size[0]], [0, obb_size[0]], [0, 0], [obb_size[1], 0]])
+				canvas_size = round(obb_size[1]), round(obb_size[0])
 
-	patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1) - 1), max(0, math.floor(aabb_y1) - 1), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
-	#patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1)), max(0, math.floor(aabb_y1)), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
-	patch = rgb[patch_y1:patch_y2, patch_x1:patch_x2]
+		A = cv.getAffineTransform(obb_pts[:3], target_pts[:3])  # Three points.
+		warped = cv.warpAffine(rgb, A, canvas_size, flags=cv.INTER_LINEAR)
 
-	ctr = tuple(np.array(patch.shape[1::-1]) / 2)
-	R = cv.getRotationMatrix2D(ctr, angle=obb_angle, scale=1)
-	rotated = cv.warpAffine(patch, R, (300, 150), flags=cv.INTER_LINEAR)
+		cv.imshow('Affinely Warped Image', warped)
 
-	cv.imshow('Rotated Image (AABB)', rotated)
+		#--------------------
+		# Perspective transformation.
 
-	import scipy.ndimage
-	cv.imshow('Rotated Image (scipy)', scipy.ndimage.rotate(patch, obb_angle, reshape=True))
+		T = cv.getPerspectiveTransform(obb_pts, target_pts, solveMethod=cv.DECOMP_LU)  # Four points.
+		warped = cv.warpPerspective(rgb, T, canvas_size, flags=cv.INTER_LINEAR)
 
-	#--------------------
-	# Affine transformation.
+		cv.imshow('Perspectively Warped Image', warped)
 
-	if False:
-		target_pts = np.float32([[100, 100], [0, 100], [0, 0]])  # Three points.
-		A = cv.getAffineTransform(obb_pts[:3], target_pts)
-		warped = cv.warpAffine(rgb, A, (300, 150), flags=cv.INTER_LINEAR)
-	else:
-		target_pts = np.float32([[obb_size[0], obb_size[1]], [0, obb_size[1]], [0, 0]])  # Three points.
-		A = cv.getAffineTransform(obb_pts[:3], target_pts)
-		warped = cv.warpAffine(rgb, A, (round(obb_size[0]), round(obb_size[1])), flags=cv.INTER_LINEAR)
-
-	cv.imshow('Affinely Warped Image', warped)
-
-	#--------------------
-	# Perspective transformation.
-
-	if False:
-		target_pts = np.float32([[100, 100], [0, 100], [0, 0], [100, 0]])  # Four points.
-		T = cv.getPerspectiveTransform(obb_pts, target_pts, solveMethod=cv.DECOMP_LU)
-		warped = cv.warpPerspective(rgb, T, (300, 150), flags=cv.INTER_LINEAR)
-	else:
-		target_pts = np.float32([[obb_size[0], obb_size[1]], [0, obb_size[1]], [0, 0], [obb_size[0], 0]])  # Four points.
-		T = cv.getPerspectiveTransform(obb_pts, target_pts, solveMethod=cv.DECOMP_LU)
-		warped = cv.warpPerspective(rgb, T, (round(obb_size[0]), round(obb_size[1])), flags=cv.INTER_LINEAR)
-
-	cv.imshow('Perspectively Warped Image', warped)
-	cv.waitKey(0)
+		cv.waitKey(0)
 	cv.destroyAllWindows()
 
 def measure_time():
+	import scipy.ndimage
+
 	rgb = np.zeros((500, 500, 3), np.uint8)
 	cv.ellipse(rgb, (300, 200), (100, 50), 30, 0, 360, (255, 0, 255), cv.FILLED, cv.LINE_8)
 
@@ -193,11 +216,11 @@ def measure_time():
 	#obb_pts = np.int0(obb_pts)
 
 	obb_center, obb_size, obb_angle = obb
-	obb_size, obb_angle = obb_size[1::-1], obb_angle + 90
+	#obb_size, obb_angle = obb_size[1::-1], obb_angle + 90
 
 	import time
-	num_iterations = 10000
-	print('Start measuring rotation...')
+	num_iterations = 1000
+	print('Start measuring rotation (opencv)...')
 	start_time = time.time()
 	for _ in range(num_iterations):
 		radius = math.sqrt(obb_size[0]**2 + obb_size[1]**2) / 2
@@ -212,21 +235,36 @@ def measure_time():
 		rotated = cv.warpAffine(patch, R, (dia, dia), flags=cv.INTER_LINEAR)
 
 		rotated_patch = rotated[math.floor(ctr[1] - obb_size[1] / 2):math.ceil(ctr[1] + obb_size[1] / 2), math.floor(ctr[0] - obb_size[0] / 2):math.ceil(ctr[0] + obb_size[0] / 2)]
-	print('End measuring rotation: {} secs.'.format(time.time() - start_time))
+	print('End measuring rotation (opencv): {} secs.'.format(time.time() - start_time))
+
+	print('Start measuring rotation (scipy)...')
+	start_time = time.time()
+	for _ in range(num_iterations):
+		aabb = cv.boundingRect(pts)  # x (left), y (top), width, height.
+		aabb_x1, aabb_y1, aabb_x2, aabb_y2 = aabb[0], aabb[1], aabb[0] + aabb[2], aabb[1] + aabb[3]
+
+		patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1) - 1), max(0, math.floor(aabb_y1) - 1), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
+		#patch_x1, patch_y1, patch_x2, patch_y2 = max(0, math.floor(aabb_x1)), max(0, math.floor(aabb_y1)), min(rgb.shape[1], math.ceil(aabb_x2) + 1), min(rgb.shape[0], math.ceil(aabb_y2) + 1)
+		patch = rgb[patch_y1:patch_y2, patch_x1:patch_x2]
+
+		rotated_scipy = scipy.ndimage.rotate(patch, obb_angle, reshape=True)
+		ctr = rotated_scipy.shape[1] / 2, rotated_scipy.shape[0] / 2
+		rotated_scipy_patch = rotated_scipy[math.floor(ctr[1] - obb_size[1] / 2):math.ceil(ctr[1] + obb_size[1] / 2), math.floor(ctr[0] - obb_size[0] / 2):math.ceil(ctr[0] + obb_size[0] / 2)]
+	print('End measuring rotation (scipy): {} secs.'.format(time.time() - start_time))
 
 	print('Start measuring affine transformation...')
 	start_time = time.time()
 	for _ in range(num_iterations):
-		target_pts = np.float32([[obb_size[0], obb_size[1]], [0, obb_size[1]], [0, 0]])  # Three points.
-		A = cv.getAffineTransform(obb_pts[:3], target_pts)
+		target_pts = np.float32([[0, obb_size[1]], [0, 0], [obb_size[0], 0]])
+		A = cv.getAffineTransform(obb_pts[:3], target_pts)  # Three points.
 		warped = cv.warpAffine(rgb, A, (round(obb_size[0]), round(obb_size[1])), flags=cv.INTER_LINEAR)
 	print('End measuring affine transformation: {} secs.'.format(time.time() - start_time))
 
 	print('Start measuring perspective transformation...')
 	start_time = time.time()
 	for _ in range(num_iterations):
-		target_pts = np.float32([[obb_size[0], obb_size[1]], [0, obb_size[1]], [0, 0], [obb_size[0], 0]])  # Four points.
-		T = cv.getPerspectiveTransform(obb_pts, target_pts, solveMethod=cv.DECOMP_LU)
+		target_pts = np.float32([[0, obb_size[1]], [0, 0], [obb_size[0], 0], [obb_size[0], obb_size[1]]])
+		T = cv.getPerspectiveTransform(obb_pts, target_pts, solveMethod=cv.DECOMP_LU)  # Four points.
 		warped = cv.warpPerspective(rgb, T, (round(obb_size[0]), round(obb_size[1])), flags=cv.INTER_LINEAR)
 	print('End measuring perspective transformation: {} secs.'.format(time.time() - start_time))
 
