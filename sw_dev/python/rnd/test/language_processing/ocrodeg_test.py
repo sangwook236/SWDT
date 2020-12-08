e5acd64e4a27dbdea2a4257e4dc88075b8e0a12f
Sang-Wook Lee <sangwook236@gmail.com>
2020-12-08 19:58:08 +0900

    [Update, Python] Distorted a pair of image and mask using ocrodeg library.

--- sw_dev/python/rnd/test/language_processing/ocrodeg_test.py
+++ sw_dev/python/rnd/test/language_processing/ocrodeg_test.py
@@ -13,7 +13,7 @@ import skimage.color
 import matplotlib
 import matplotlib.pyplot as plt
 import pylab
-#import cv2
+import cv2
 
 # REF [function] >> noise_distort1d() in ${ocrodeg_HOME}/ocrodeg/degrade.py.
 def my_noise_distort1d(shape, in_vec, sigma=100.0, magnitude=100.0):
@@ -31,21 +31,21 @@ def simple_example():
 	if False:
 		image_filepath = './poem_01.jpg'  # A color page.
 		patch_hmin, patch_hmax, patch_wmin, patch_wmax = 160, 460, 40, 370
-		is_grayscale, is_normalized = False, True
+		is_grayscale, is_normalized = False, False
 	elif False:
 		image_filepath = './W1P0_title.png'  # A grayscale text line.
 		patch_hmin, patch_hmax, patch_wmin, patch_wmax = 0, 89, 0, 1323
-		is_grayscale, is_normalized = True, True
+		is_grayscale, is_normalized = True, False
 	else:
 		image_filepath = './W1P0.png'  # A grayscale page. np.float32. [0, 1].
 		patch_hmin, patch_hmax, patch_wmin, patch_wmax = 1900, 2156, 1000, 1256
-		is_grayscale, is_normalized = True, False
+		is_grayscale, is_normalized = True, True
 
 	if is_grayscale:
-		import ocrodeg
+		import ocrodeg  # For grayscale images.
 		cmap = 'gray'
 	else:
-		import my_ocrodeg.degrade as ocrodeg
+		import my_ocrodeg.degrade as ocrodeg  # For color images.
 		cmap = None
 
 	image = plt.imread(image_filepath)
@@ -54,7 +54,7 @@ def simple_example():
 	if image is None:
 		print('Failed to load an image: {}.'.format(image_filepath))
 		return
-	if is_normalized: image = image / 255
+	if not is_normalized: image = image / 255
 	print('Image: shape = {}, dtype = {}, (min, max) = ({}, {}).'.format(image.shape, image.dtype, np.min(image), np.max(image)))
 
 	patch = image[patch_hmin:patch_hmax, patch_wmin:patch_wmax]
@@ -244,8 +244,121 @@ def simple_example():
 
 	plt.show()
 
+def paired_image_mask_example():
+	import ocrodeg  # For grayscale images.
+	import my_ocrodeg.degrade as ocrodeg_rgb  # For color images.
+
+	image_height, image_width = 500, 500
+	image = np.zeros((image_height, image_width, 3), dtype=np.float32)
+	mask = np.zeros((image_height, image_width), dtype=np.float32)
+
+	cv2.rectangle(image, (100, 100), (200, 200), (0, 0, 1), cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(image, (300, 100), (400, 200), (0, 1, 0), cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(image, (100, 300), (200, 400), (1, 0, 0), cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(image, (300, 300), (400, 400), (1, 0, 1), cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(image, (200, 200), (300, 300), (1, 1, 1), cv2.FILLED, cv2.LINE_8)
+
+	cv2.rectangle(mask, (100, 100), (200, 200), 1, cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(mask, (300, 100), (400, 200), 1, cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(mask, (100, 300), (200, 400), 1, cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(mask, (300, 300), (400, 400), 1, cv2.FILLED, cv2.LINE_8)
+	cv2.rectangle(mask, (200, 200), (300, 300), 1, cv2.FILLED, cv2.LINE_8)
+
+	print('Image: shape = {}, dtype = {}, (min, max) = ({}, {}).'.format(image.shape, image.dtype, np.min(image), np.max(image)))
+	print('Mask: shape = {}, dtype = {}, (min, max) = ({}, {}).'.format(mask.shape, mask.dtype, np.min(mask), np.max(mask)))
+
+	cv2.imshow('Image', image)
+	cv2.imshow('Mask', mask)
+
+	if False:
+		# Page rotation.
+		angle = random.uniform(-180, 180)
+
+		image = ndi.rotate(image, angle, reshape=True, mode='nearest')
+		# TODO [check] >> Which one is better?
+		image = np.clip(image, 0, 1)
+		#minv, maxv = np.min(image), np.max(image)
+		#image = (image - minv) / (maxv - minv)
+
+		mask = ndi.rotate(mask, angle, reshape=True, mode='nearest')
+		# TODO [check] >> Which one is better?
+		mask = np.clip(mask, 0, 1)
+		#minv, maxv = np.min(mask), np.max(mask)
+		#mask = (mask - minv) / (maxv - minv)
+	elif True:
+		# Random geometric transformation.
+		transform_kwargs = ocrodeg.random_transform()
+		image = ocrodeg_rgb.transform_image(image, **transform_kwargs)
+		mask = ocrodeg.transform_image(mask, **transform_kwargs)
+	elif False:
+		# Random distortion.
+		sigma = random.uniform(1, 20)
+		noise = ocrodeg.bounded_gaussian_noise(image.shape[:2], sigma, maxdelta=5.0)  # Shape = (2, image height, image width).
+		image = ocrodeg_rgb.distort_with_noise(image, noise.copy())
+		mask = ocrodeg.distort_with_noise(mask, noise.copy())
+	elif False:
+		# Ruled surface distortion.
+		mag = random.uniform(5, 200)
+		#noise = ocrodeg.noise_distort1d(image.shape[:2], magnitude=mag)
+		in_vec = pylab.randn(image.shape[1])
+		noise = my_noise_distort1d(image.shape, in_vec, magnitude=mag)  # My modification. Shape = (2, image height, image width).
+		image = ocrodeg_rgb.distort_with_noise(image, noise.copy())
+		mask = ocrodeg.distort_with_noise(mask, noise.copy())
+	elif False:
+		# Blur, thresholding, noise.
+		sigma = random.uniform(0.5, 1)
+		image = ndi.gaussian_filter(image, sigma)
+		#mask = ndi.gaussian_filter(mask, sigma)
+
+		"""
+		sigma = random.uniform(0, 4)
+		image = skimage.color.rgb2gray(image)
+		image = ndi.gaussian_filter(image, sigma)
+		image = 1.0 * (image > 0.5)
+		##mask = skimage.color.rgb2gray(mask)
+		#mask = ndi.gaussian_filter(mask, sigma)
+		#mask = 1.0 * (mask > 0.5)
+		"""
+
+		"""
+		sigma = random.uniform(0, 4)
+		image = skimage.color.rgb2gray(image)
+		image = ocrodeg.binary_blur(image, sigma, noise=0.0)
+		##mask = skimage.color.rgb2gray(mask)
+		#mask = ocrodeg.binary_blur(mask, sigma, noise=0.0)
+		"""
+
+		"""
+		noise = random.uniform(0, 0.3)
+		image = skimage.color.rgb2gray(image)
+		image = ocrodeg.binary_blur(image, sigma=2.0, noise=noise)
+		##mask = skimage.color.rgb2gray(mask)
+		#mask = ocrodeg.binary_blur(mask, sigma=2.0, noise=noise)
+		"""
+	elif False:
+		# Random blob.
+		image = ocrodeg_rgb.random_blotches(image, fgblobs=3e-4, bgblobs=1e-4, fgscale=10, bgscale=10)
+		#image = np.minimum(np.maximum(image, ocrodeg.random_blobs(image.shape[:2], 30, 10)), 1 - ocrodeg.random_blobs(image.shape[:2], 15, 8))
+		#mask = ocrodeg.random_blotches(mask, fgblobs=3e-4, bgblobs=1e-4, fgscale=10, bgscale=10)
+		#mask = np.minimum(np.maximum(mask, ocrodeg.random_blobs(mask.shape, 30, 10)), 1 - ocrodeg.random_blobs(mask.shape, 15, 8))
+	elif False:
+		# Multiscale noise.
+		image = ocrodeg_rgb.printlike_multiscale(image, blur=1.0, blotches=5e-05)
+		#mask = ocrodeg.printlike_multiscale(mask, blur=1.0, blotches=5e-05)
+	elif False:
+		# Fibrous noise.
+		image = ocrodeg_rgb.printlike_fibrous(image, blur=1.0, blotches=5e-05)
+		#mask = ocrodeg.printlike_fibrous(mask, blur=1.0, blotches=5e-05)
+
+	cv2.imshow('Distorted Image', image)
+	cv2.imshow('Distorted Mask', mask)
+
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
 def main():
-	simple_example()
+	#simple_example()
+	paired_image_mask_example()
 
 #--------------------------------------------------------------------
 
