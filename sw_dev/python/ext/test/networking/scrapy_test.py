76bf5517aed5d0e69f4cd7cafc099ef8b67ba4c8
Sang-Wook Lee <sangwook236@gmail.com>
2021-01-21 01:26:10 +0900

    [New, Python] Implemented a simple example for Scrapy library.

---
+++ sw_dev/python/ext/test/networking/scrapy_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# REF [site] >> https://scrapy.org/
+
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+	name = 'blogspider'
+	start_urls = ['https://blog.scrapinghub.com']
+
+	def parse(self, response):
+		for title in response.css('.post-header>h2'):
+			yield {'title': title.css('a ::text').get()}
+
+		for next_page in response.css('a.next-posts-link'):
+			yield response.follow(next_page, self.parse)
+
+#--------------------------------------------------------------------
+
+# Usage:
+#	scrapy runspider scrapy_test.py
+
+#if '__main__' == __name__:
+#	main()
