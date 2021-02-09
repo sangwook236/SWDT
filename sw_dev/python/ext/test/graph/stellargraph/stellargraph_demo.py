d83c9648c32d3814836a5bee1cb3176ffc21fc81
Sang-Wook Lee <sangwook236@gmail.com>
2021-02-09 14:19:39 +0900

    [Update, Python] Implemented two examples to classify graphs with GCN and classify nodes with GAT using StellarGraph library.

--- sw_dev/python/ext/test/graph/stellargraph/stellargraph_demo.py
+++ sw_dev/python/ext/test/graph/stellargraph/stellargraph_demo.py
@@ -3,17 +3,105 @@
 
 # REF [site] >> https://stellargraph.readthedocs.io/en/stable/demos/index.html
 
-#import os
+import os
+import numpy as np
 import pandas as pd
 import stellargraph as sg
 import tensorflow as tf
-import sklearn
+import sklearn, sklearn.manifold
 import matplotlib.pyplot as plt
 #from IPython.display import display, HTML
 #%matplotlib inline
 
+# REF [site] >> https://stellargraph.readthedocs.io/en/stable/demos/graph-classification/gcn-supervised-graph-classification.html
+def supervised_graph_classification_with_GCN():
+	# Import the data.
+	dataset = sg.datasets.MUTAG()
+	#display(HTML(dataset.description))
+	print(dataset.description)
+
+	graphs, graph_labels = dataset.load()
+	print(graphs[0].info())
+	print(graphs[1].info())
+
+	summary = pd.DataFrame(
+		[(g.number_of_nodes(), g.number_of_edges()) for g in graphs],
+		columns=['nodes', 'edges'],
+	)
+	print(summary.describe().round(1))
+	print(graph_labels.value_counts().to_frame())
+
+	graph_labels = pd.get_dummies(graph_labels, drop_first=True)
+
+	# Prepare graph generator.
+	generator = sg.mapper.PaddedGraphGenerator(graphs=graphs)
+
+	#--------------------
+	# Create the Keras graph classification model.
+	def create_graph_classification_model(generator):
+		gc_model = sg.layer.GCNSupervisedGraphClassification(
+			layer_sizes=[64, 64],
+			activations=['relu', 'relu'],
+			generator=generator,
+			dropout=0.5,
+		)
+		x_inp, x_out = gc_model.in_out_tensors()
+		predictions = tf.keras.layers.Dense(units=32, activation='relu')(x_out)
+		predictions = tf.keras.layers.Dense(units=16, activation='relu')(predictions)
+		predictions = tf.keras.layers.Dense(units=1, activation='sigmoid')(predictions)
+
+		# Create the Keras model and prepare it for training.
+		model = tf.keras.Model(inputs=x_inp, outputs=predictions)
+		model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss=tf.keras.losses.binary_crossentropy, metrics=['acc'])
+
+		return model
+
+	#--------------------
+	# Train the model.
+	epochs = 200  # Maximum number of training epochs.
+	folds = 10  # The number of folds for k-fold cross validation.
+	n_repeats = 5  # The number of repeats for repeated k-fold cross validation.
+
+	es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=25, restore_best_weights=True)
+
+	def train_fold(model, train_gen, test_gen, es, epochs):
+		history = model.fit(train_gen, epochs=epochs, validation_data=test_gen, verbose=0, callbacks=[es])
+
+		# Calculate performance on the test data and return along with history.
+		test_metrics = model.evaluate(test_gen, verbose=0)
+		test_acc = test_metrics[model.metrics_names.index('acc')]
+
+		return history, test_acc
+
+	def get_generators(train_index, test_index, graph_labels, batch_size):
+		train_gen = generator.flow(train_index, targets=graph_labels.iloc[train_index].values, batch_size=batch_size)
+		test_gen = generator.flow(test_index, targets=graph_labels.iloc[test_index].values, batch_size=batch_size)
+
+		return train_gen, test_gen
+
+	stratified_folds = sklearn.model_selection.RepeatedStratifiedKFold(n_splits=folds, n_repeats=n_repeats).split(graph_labels, graph_labels)
+
+	test_accs = list()
+	for i, (train_index, test_index) in enumerate(stratified_folds):
+		print(f'Training and evaluating on fold {i+1} out of {folds * n_repeats}...')
+		train_gen, test_gen = get_generators(train_index, test_index, graph_labels, batch_size=30)
+
+		model = create_graph_classification_model(generator)
+
+		history, acc = train_fold(model, train_gen, test_gen, es, epochs)
+
+		test_accs.append(acc)
+
+	print(f'Accuracy over all folds mean: {np.mean(test_accs)*100:.3}% and std: {np.std(test_accs)*100:.2}%')
+
+	plt.figure(figsize=(8, 6))
+	plt.hist(test_accs)
+	plt.xlabel('Accuracy')
+	plt.ylabel('Count')
+	plt.show()
+
 # REF [site] >> https://stellargraph.readthedocs.io/en/stable/demos/link-prediction/gcn-link-prediction.html
-def predict_link_with_GCN():
+def link_prediction_with_GCN():
 	# Load the CORA network data.
 	dataset = sg.datasets.Cora()
 	#display(HTML(dataset.description))
@@ -97,7 +185,7 @@ def predict_link_with_GCN():
 	plt.show()
 
 # REF [site] >> https://stellargraph.readthedocs.io/en/stable/demos/node-classification/gcn-node-classification.html
-def classify_node_with_GCN():
+def node_classification_with_GCN():
 	# Load the CORA network.
 	dataset = sg.datasets.Cora()
 	#display(HTML(dataset.description))
@@ -128,26 +216,26 @@ def classify_node_with_GCN():
 	generator = sg.mapper.FullBatchNodeGenerator(G, method='gcn')
 	gcn = sg.layer.GCN(layer_sizes=[16, 16], activations=['relu', 'relu'], generator=generator, dropout=0.5)
 	x_inp, x_out = gcn.in_out_tensors()
-
 	predictions = tf.keras.layers.Dense(units=train_targets.shape[1], activation='softmax')(x_out)
 
 	model = tf.keras.Model(inputs=x_inp, outputs=predictions)
 
-	#--------------------
-	# Train the model.
 	model.compile(
 		optimizer=tf.keras.optimizers.Adam(lr=0.01),
 		loss=tf.keras.losses.categorical_crossentropy,
 		metrics=['acc'],
 	)
 
+	#--------------------
+	# Train the model.
 	train_gen = generator.flow(train_subjects.index, train_targets)
 	val_gen = generator.flow(val_subjects.index, val_targets)
 	es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=50, restore_best_weights=True)
 
+	epochs = 200
 	history = model.fit(
 		train_gen,
-		epochs=200,
+		epochs=epochs,
 		validation_data=val_gen,
 		verbose=2,
 		shuffle=False,  # This should be False, since shuffling data means shuffling the whole graph.
@@ -180,10 +268,8 @@ def classify_node_with_GCN():
 	emb = embedding_model.predict(all_gen)
 	print(emb.shape)
 
-	from sklearn.decomposition import PCA
-	from sklearn.manifold import TSNE
-
-	transform = TSNE  # or PCA.
+	#transform = sklearn.decomposition.PCA
+	transform = sklearn.manifold.TSNE
 
 	X = emb.squeeze(0)
 	print(X.shape)
@@ -208,12 +294,147 @@ def classify_node_with_GCN():
 	)
 	plt.show()
 
+# REF [site] >> https://stellargraph.readthedocs.io/en/stable/demos/node-classification/gat-node-classification.html
+def node_classification_with_GAT():
+	# Load the CORA network.
+	dataset = sg.datasets.Cora()
+	#display(HTML(dataset.description))
+	print(dataset.description)
+
+	G, node_subjects = dataset.load()
+	print(G.info())
+	print(set(node_subjects))
+
+	# Split the data.
+	train_subjects, test_subjects = sklearn.model_selection.train_test_split(
+		node_subjects, train_size=140, test_size=None, stratify=node_subjects
+	)
+	val_subjects, test_subjects = sklearn.model_selection.train_test_split(
+		test_subjects, train_size=500, test_size=None, stratify=test_subjects
+	)
+	from collections import Counter
+	print(Counter(train_subjects))
+
+	# Convert to numeric arrays.
+	target_encoding = sklearn.preprocessing.LabelBinarizer()
+
+	train_targets = target_encoding.fit_transform(train_subjects)
+	val_targets = target_encoding.transform(val_subjects)
+	test_targets = target_encoding.transform(test_subjects)
+
+	#--------------------
+	# Create the GAT model.
+	generator = sg.mapper.FullBatchNodeGenerator(G, method='gat')
+	gat = sg.layer.GAT(
+		layer_sizes=[8, train_targets.shape[1]],
+		activations=['elu', 'softmax'],
+		attn_heads=8,
+		generator=generator,
+		in_dropout=0.5,
+		attn_dropout=0.5,
+		normalize=None,
+	)
+	x_inp, predictions = gat.in_out_tensors()
+
+	model = tf.keras.Model(inputs=x_inp, outputs=predictions)
+
+	model.compile(
+		optimizer=tf.keras.optimizers.Adam(lr=0.005),
+		loss=tf.keras.losses.categorical_crossentropy,
+		metrics=['acc'],
+	)
+
+	#--------------------
+	# Train the model.
+	train_gen = generator.flow(train_subjects.index, train_targets)
+	val_gen = generator.flow(val_subjects.index, val_targets)
+
+	if not os.path.isdir('logs'):
+		os.makedirs('logs')
+	es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=20)  # patience is the number of epochs to wait before early stopping in case of no further improvement.
+	mc_callback = tf.keras.callbacks.ModelCheckpoint('logs/best_model.h5', monitor='val_acc', save_best_only=True, save_weights_only=True)
+
+	epochs = 50
+	history = model.fit(
+		train_gen,
+		epochs=epochs,
+		validation_data=val_gen,
+		verbose=2,
+		shuffle=False,  # This should be False, since shuffling data means shuffling the whole graph.
+		callbacks=[es_callback, mc_callback],
+	)
+	sg.utils.plot_history(history)
+
+	#--------------------
+	# Evaluate the model.
+	# Reload the saved weights of the best model found during the training.
+	model.load_weights('logs/best_model.h5')
+
+	test_gen = generator.flow(test_subjects.index, test_targets)
+	test_metrics = model.evaluate(test_gen)
+	print('\nTest Set Metrics:')
+	for name, val in zip(model.metrics_names, test_metrics):
+		print('\t{}: {:0.4f}'.format(name, val))
+
+	#--------------------
+	# Make predictions with the model.
+	all_nodes = node_subjects.index
+	all_gen = generator.flow(all_nodes)
+	all_predictions = model.predict(all_gen)
+
+	node_predictions = target_encoding.inverse_transform(all_predictions.squeeze())
+
+	df = pd.DataFrame({'Predicted': node_predictions, 'True': node_subjects})
+	print(df.head(20))
+
+	#--------------------
+	# Node embedding.
+	emb_layer = next(l for l in model.layers if l.name.startswith('graph_attention'))
+	print('Embedding layer: {}, output shape {}'.format(emb_layer.name, emb_layer.output_shape))
+
+	embedding_model = tf.keras.Model(inputs=x_inp, outputs=emb_layer.output)
+	emb = embedding_model.predict(all_gen)
+	print(emb.shape)
+
+	X = emb.squeeze(0)
+	y = np.argmax(target_encoding.transform(node_subjects), axis=1)
+
+	if X.shape[1] > 2:
+		#transform = sklearn.decomposition.PCA
+		transform = sklearn.manifold.TSNE
+
+		trans = transform(n_components=2)
+		emb_transformed = pd.DataFrame(trans.fit_transform(X), index=list(G.nodes()))
+		emb_transformed['label'] = y
+	else:
+		emb_transformed = pd.DataFrame(X, index=list(G.nodes()))
+		emb_transformed = emb_transformed.rename(columns={'0': 0, '1': 1})
+		emb_transformed['label'] = y
+
+	alpha = 0.7
+
+	fig, ax = plt.subplots(figsize=(7, 7))
+	ax.scatter(
+		emb_transformed[0],
+		emb_transformed[1],
+		c=emb_transformed['label'].astype('category'),
+		cmap='jet',
+		alpha=alpha,
+	)
+	ax.set(aspect='equal', xlabel='$X_1$', ylabel='$X_2$')
+	plt.title('{} visualization of GAT embeddings for cora dataset'.format(transform.__name__))
+	plt.show()
+
 def main():
+	# Graph classification.
+	#supervised_graph_classification_with_GCN()
+
 	# Link prediction.
-	predict_link_with_GCN()
+	#link_prediction_with_GCN()
 
 	# Node classification.
-	#classify_node_with_GCN()
+	#node_classification_with_GCN()
+	node_classification_with_GAT()
 
 #--------------------------------------------------------------------
 
