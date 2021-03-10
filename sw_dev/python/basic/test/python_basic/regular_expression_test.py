5f8411177c1e8796118422c20de4b652eaf9d0ad
Sang-Wook Lee <sangwook236@gmail.com>
2021-03-10 16:48:16 +0900

    [Update, Python] Implemented an example if regular expression for roman numerals and HTML tags.

--- sw_dev/python/basic/test/python_basic/regular_expression_test.py
+++ sw_dev/python/basic/test/python_basic/regular_expression_test.py
@@ -8,16 +8,6 @@
 import re
 
 def basic_operation():
-	# REF [site] >> Commonly used regular expressions.
-	#	https://digitalfortress.tech/tricks/top-15-commonly-used-regex/
-
-	pattern = r'([0-9]+\.)*[0-9]+\.?'  # 1, 2., 1.2, 2.3., ...
-	pattern = r'[A-Za-z][0-9]*\.'  # A. B1., c2., ...
-	# NOTE [info] >> As the target string is scanned, REs separated by '|' are tried from left to right.
-	pattern = r'([A-Za-z][0-9]*\.)?([0-9]+\.)*[0-9]+\.?|[A-Za-z][0-9]*\.'
-	pattern = r'(([A-Za-z][0-9]*\.)?([0-9]+\.)*[0-9]+\.?|[A-Za-z][0-9]*\.)\s+.+'
-
-	#--------------------
 	r"""
 	# Special sequence.
 	\number \A \b \B \d \D \s \S \w \W \Z
@@ -96,6 +86,7 @@ def basic_operation():
 
 	pattern = re.compile('[a-z]+')
 	#pattern = re.compile('[a-z]+', re.IGNORECASE)
+	#pattern = re.compile(r'(\w+) (\w+)')
 	#pattern = re.compile(r'\d+\.\d*', re.X)
 
 	pattern = re.compile('d')
@@ -178,56 +169,116 @@ def basic_operation():
 	match = re.match(r'(?P<first_name>\w+) (?P<last_name>\w+)', 'Malcolm Reynolds')
 	match.groupdict()
 
+# REF [site] >> Commonly used regular expressions.
+#	https://digitalfortress.tech/tricks/top-15-commonly-used-regex/
+def commonly_used_expressions():
+	# Roman numerals.
+	#	I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000.
+
+	# REF [site] >> https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch06s09.html
+	#roman_numeral_pattern = re.compile(r'^[MDCLXVI]+$', re.I)  # No validation.
+	roman_numeral_pattern = re.compile(r'^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$', re.I)  # Strict.
+	#roman_numeral_pattern = re.compile(r'^(?=[MDCLXVI])M*(C[MD]|D?C*)(X[CL]|L?X*)(I[XV]|V?I*)$', re.I)  # Flexible.
+	#roman_numeral_pattern = re.compile(r'^(?=[MDCLXVI])M*D?C{0,4}L?X{0,4}V?I{0,4}$', re.I)  # Simple.
+	# REF [site] >> https://stackoverflow.com/questions/267399/how-do-you-match-only-valid-roman-numerals-with-a-regular-expression
+	#roman_numeral_pattern = re.compile(r'^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$', re.I)
+
+	roman_numerals_correct = [
+		'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
+		'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX',
+		'i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x',
+		'xi', 'xii', 'xiii', 'xiv', 'xv', 'xvi', 'xvii', 'xviii', 'xix', 'xx',
+	]
+	roman_numerals_incorrect = [
+		'I.',
+		'IIII',
+		'IIV', 'VIIII', 'VV',
+		'IIX','XIIII',
+	]
+
+	for ss in roman_numerals_correct:
+		match = roman_numeral_pattern.match(ss)
+		if match is None:
+			print('Roman numeral correct (unmatched): {}.'.format(ss))
+		elif ss != match[0]:
+			print('Roman numeral correct (partially matched): {} != {}.'.format(match[0], ss))
+
+	for ss in roman_numerals_incorrect:
+		#match = roman_numeral_pattern.match(ss)
+		match = roman_numeral_pattern.fullmatch(ss)
+		if match is not None:
+			if ss == match[0]:
+				print('Roman numeral incorrect (matched): {}.'.format(ss))
+			else:
+				print('Roman numeral incorrect (partially matched): {} != {}.'.format(match[0], ss))
+
+	#--------------------
+	# HTML tag.
+	html_tag_pattern = re.compile(r'<\/?[\w\s]*>|<.+[\W]>')
 
 def page_object_example():
 	# Basic pattern.
-	re_pattern = re.compile(r'([0-9]+\.)*[0-9]+\.?')  # 1, 2., 1.2, 2.3., ...
-	re_pattern = re.compile(r'[A-Za-z][0-9]*\.')  # A. B1., c2., ...
+	#re_pattern = re.compile(r'([0-9]+\.)*[0-9]+\.?')  # 1, 2., 1.2, 2.3., ...
+	re_pattern = re.compile(r'(\d+\.)*\d+\.?')  # 1, 2., 1.2, 2.3., ...
+	re_pattern = re.compile(r'[A-Za-z]\d*\.')  # A. B1., c2., ...
+	# NOTE [info] >> As the target string is scanned, REs separated by '|' are tried from left to right.
+	re_pattern = re.compile(r'([A-Za-z]\d*\.)?(\d+\.)*\d+\.?|[A-Za-z]\d*\.')
+	re_pattern = re.compile(r'(([A-Za-z]\d*\.)?(\d+\.)*\d+\.?|[A-Za-z]\d*\.)\s+.+')
 
 	#--------------------
 	numberings_correct = [
-		'3', '3.', '3)', '(3)',
-		'2.3', '2.3.', '2.3)', '(2.3)',
-		'2.2.3', '2.2.3.', '2.2.3)', '(2.2.3)',
+		'3.', '3)', '(3)',
+		'12345.', '12345)', '(12345)',
+		'1.2', '1.2.', '1.2)', '(1.2)',
+		'1.2.3.4.5', '1.2.3.4.5.', '1.2.3.4.5)', '(1.2.3.4.5)',
 		'b.', 'b)', '(b)',
 		'b.2', 'b.2.', 'b.2)', '(b.2)',
-		'b3.', 'b3)', '(b3)',
-		'b.2.2.3', 'b.2.2.3.', 'b.2.2.3)', '(b.2.2.3)',
-		'b2.2.3', 'b2.2.3.', 'b2.2.3)', '(b2.2.3)',
+		'b137.', 'b137)', '(b137)',
+		'b.1.2', 'b.1.2.', 'b.1.2)', '(b.1.2)',
+		'b1.2', 'b1.2.', 'b1.2)', '(b1.2)',
+		'b.1.2.3.4.5', 'b.1.2.3.4.5.', 'b.1.2.3.4.5)', '(b.1.2.3.4.5)',
+		'b1.2.3.4.5', 'b1.2.3.4.5.', 'b1.2.3.4.5)', '(b1.2.3.4.5)',
 	]
-
 	numberings_incorrect = [
+		'3', '12', '12345',
+		'.3', '.12', '.12345',
 		'b', 'b2', '2b', 'ab',
-		'b.a2.2.3', 'b.a2.2.3.',
-		'ab.2.2.3', 'ab.2.2.3.',
-		'2b.2.2.3', '2b.2.2.3.',
-		'.b.2.2.3', '.b.2.2.3.',
+		'b.a1.2', 'b.a1.2.',
+		'b.a.1.2', 'b.a.1.2.',
+		'ab.1.2.3.4.5', 'ab.1.2.3.4.5.',
+		'2b.1.2', '2b.1.2.',
+		'2b.1.2.3.4.5', '2b.1.2.3.4.5.',
+		'.b.1.2', '.b.1.2.',
+		'.b.1.2.3.4.5', '.b.1.2.3.4.5.',
 	]
 
+	#--------------------
 	# For numbering.
-	re_pattern = re.compile(r'\(?([A-Za-z][0-9]*\.)?([0-9]+\.)*[0-9]+\)|([A-Za-z][0-9]*\.)?([0-9]+\.)*[0-9]+\.?|\(?[A-Za-z][0-9]*\)|[A-Za-z][0-9]*\.')
+	if False:
+		#re_pattern = re.compile(r'\(?([A-Za-z]\d*\.)?(\d+\.)*\d+\)|([A-Za-z]\d*\.)?(\d+\.)*\d+\.?|\(?[A-Za-z]\d*\)|[A-Za-z]\d*\.')
+		re_pattern = re.compile(r'\(?([A-Za-z]\d*\.)?(\d+\.)*\d+\)|[A-Za-z]\d*\.(\d+\.)*\d+\.?|(\d+\.)+\d+\.?|\d+\.|\(?[A-Za-z]\d*\)|[A-Za-z]\d*\.')
 
-	for ss in numberings_correct:
-		match = re_pattern.match(ss)
-		if match is None:
-			print('Numbering (correct, unmatched): {}.'.format(ss))
-		elif ss != match[0]:
-			print('Numbering (correct, partially matched): {} != {}.'.format(ss, match[0]))
-		#else:
-		#z	print('Numbering (correct, matched): {}.'.format(ss))
+		for ss in numberings_correct:
+			match = re_pattern.match(ss)
+			if match is None:
+				print('Numbering correct (unmatched): {}.'.format(ss))
+			elif ss != match[0]:
+				print('Numbering correct (partially matched): {} != {}.'.format(match[0], ss))
+			#else:
+			#z	print('Numbering correct (matched): {}.'.format(ss))
 
-	for ss in numberings_incorrect:
-		#match = re_pattern.match(ss)
-		match = re_pattern.fullmatch(ss)
-		if match is not None:
-			if ss == match[0]:
-				print('Numbering (incorrect, matched): {}.'.format(ss))
-			else:
-				print('Numbering (incorrect, partially matched): {} != {}.'.format(ss, match[0]))
+		for ss in numberings_incorrect:
+			#match = re_pattern.match(ss)
+			match = re_pattern.fullmatch(ss)
+			if match is not None:
+				if ss == match[0]:
+					print('Numbering incorrect (matched): {}.'.format(ss))
+				else:
+					print('Numbering incorrect (partially matched): {} != {}.'.format(match[0], ss))
 
 	#--------------------
 	# For heading.
-	if True:
+	if False:
 		heading_formats = [
 			'{} Introduction',
 			'{} Introduction.',
@@ -238,7 +289,8 @@ def page_object_example():
 			'{} Introduction. Experimental results.',
 		]
 
-		re_pattern = re.compile(r'(\(?([A-Za-z][0-9]*\.)?([0-9]+\.)*[0-9]+\)|([A-Za-z][0-9]*\.)?([0-9]+\.)*[0-9]+\.?|\(?[A-Za-z][0-9]*\)|[A-Za-z][0-9]*\.)\s+.+')
+		#re_pattern = re.compile(r'(\(?([A-Za-z]\d*\.)?(\d+\.)*\d+\)|([A-Za-z]\d*\.)?(\d+\.)*\d+\.?|\(?[A-Za-z]\d*\)|[A-Za-z]\d*\.)\s+.+')
+		re_pattern = re.compile(r'(\(?([A-Za-z]\d*\.)?(\d+\.)*\d+\)|[A-Za-z]\d*\.(\d+\.)*\d+\.?|(\d+\.)+\d+\.?|\d+\.|\(?[A-Za-z]\d*\)|[A-Za-z]\d*\.)\s+.+')
 
 		for head_fmt in heading_formats:
 			for numbering in numberings_correct:
@@ -246,9 +298,9 @@ def page_object_example():
 
 				match = re_pattern.match(ss)
 				if match is None:
-					print('Heading (correct, unmatched): {}.'.format(ss))
+					print('Heading correct (unmatched): {}.'.format(ss))
 				elif ss != match[0]:
-					print('Heading (correct, partially matched): {} != {}.'.format(ss, match[0]))
+					print('Heading correct (partially matched): {} != {}.'.format(match[0], ss))
 
 			for numbering in numberings_incorrect:
 				ss = head_fmt.format(numbering)
@@ -257,34 +309,44 @@ def page_object_example():
 				match = re_pattern.fullmatch(ss)
 				if match is not None:
 					if ss == match[0]:
-						print('Heading (incorrect, matched): {}.'.format(ss))
+						print('Heading incorrect (matched): {}.'.format(ss))
 					else:
-						print('Heading (incorrect, partially matched): {} != {}.'.format(ss, match[0]))
+						print('Heading incorrect (partially matched): {} != {}.'.format(match[0], ss))
 
 		#--------------------
 
-		simple_headings = [
+		simple_headings_correct = [
 			'Table 0', 'TABLE 0.',
 			'figure 13', 'FIGURE 13.',
 			'APPendix 297', 'AppEndiX 297.',
-			# Error cases.
+		]
+		simple_headings_incorrect = [
 			'Tablea 0', 'TABLE 0)',
 			'figure (13', 'PIGURE 13.',
 			'APPendix 297]', '0AppEndiX 297.',
 		]
 
-		re_pattern = re.compile(r'([Tt][Aa][Bb][Ll][Ee]|[Ff][Ii][Gg][Uu][Rr][Ee]|[Aa][Pp]{2}[Ee][Nn][Dd][Ii][Xx])\s+[0-9]+\.?')
+		re_pattern = re.compile(r'([Tt][Aa][Bb][Ll][Ee]|[Ff][Ii][Gg][Uu][Rr][Ee]|[Aa][Pp]{2}[Ee][Nn][Dd][Ii][Xx])\s+\d+\.?')
 
-		for ss in simple_headings:
+		for ss in simple_headings_correct:
 			match = re_pattern.match(ss)
 			if match is None:
-				print('Simlple heading (unmatched): {}.'.format(ss))
+				print('Simlple heading correct (unmatched): {}.'.format(ss))
 			elif ss != match[0]:
-				print('Simlple heading (partially matched): {} != {}.'.format(ss, match[0]))
+				print('Simlple heading correct (partially matched): {} != {}.'.format(match[0], ss))
+
+		for ss in simple_headings_incorrect:
+			#match = re_pattern.match(ss)
+			match = re_pattern.fullmatch(ss)
+			if match is not None:
+				if ss == match[0]:
+					print('Simlple heading incorrect (matched): {}.'.format(ss))
+				elif ss != match[0]:
+					print('Simlple heading incorrect (partially matched): {} != {}.'.format(match[0], ss))
 
 	#--------------------
 	# For list.
-	if False:
+	if True:
 		list_paragraph_formats = [
 			'{1} studies not conducted in SSA{0} {2} studies that were reviews, case reports, or case series{0} {3} studies that did not relate epidemiologic data{0} {4} studies that did not discuss dementia or its subtypes in individuals aged 60 years and above.',
 			#'{1} studies not conducted in SSA{0} {2} studies that were reviews, case reports, or case series{0} {3} studies that did not relate epidemiologic data{0} {4} studies that did not discuss dementia or its subtypes in individuals aged 60 years and above. VII.',
@@ -292,8 +354,8 @@ def page_object_example():
 			#'{1} {2}',
 		]
 		list_separators = [';', ':', ',', '.']
-		list_numbering_formats = ['({})', '{})', '{}.']
 
+		list_numbering_formats = ['({})', '{})', '{}.']
 		list_indices_lst = [
 			[1, 2, 3, 4], [3, 4, 5, 6], [9, 10, 11, 12], [1279, 1280, 1281, 1282],
 			['a', 'b', 'c', 'd'], ['c', 'd', 'e', 'f'],
@@ -303,14 +365,16 @@ def page_object_example():
 			# Error cases.
 			#['XIi', 'iXII', '0XII', 'XI0'],
 		]
+		list_bullets = ['•', '‣']
 
-		#re_pattern = re.compile(r'(\(?[0-9]+\)|[0-9]+\.|\(?[a-z]\)|[a-z]\.|\(?[A-Z]\)|[A-Z]\.)')  # Not correct.
-		#re_pattern = re.compile(r'\(?\b[0-9]+\b\)|\b[0-9]+\b\.|\(?\b[a-z]\b\)|\b[a-z]\b\.|\(?\b[A-Z]\b\)|\b[A-Z]\b\.')  # Digit and alphabet.
-		#re_pattern = re.compile(r'(\(?\b[0-9]+\b\)|\b[0-9]+\b\.|\(?\b[a-z]\b\)|\b[a-z]\b\.|\(?\b[A-Z]\b\)|\b[A-Z]\b\.)')  # Digit and alphabet.
+		#re_pattern = re.compile(r'(\(?\d+\)|\d+\.|\(?[a-zA-Z]\)|[a-zA-Z]\.)')  # Not correct.
+		#re_pattern = re.compile(r'\(?\b\d+\b\)|\b\d+\b\.|\(?\b[a-zA-Z]\b\)|\b[a-zA-Z]\b\.')  # Digit and alphabet.
+		#re_pattern = re.compile(r'(\(?\b\d+\b\)|\b\d+\b\.|\(?\b[a-zA-Z]\b\)|\b[a-zA-Z]\b\.)')  # Digit and alphabet.
 		#re_pattern = re.compile(r'\(?\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\)|\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\.|\(?\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\)|\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\.')  # Roman numerals (1 ~ 13).
 		#re_pattern = re.compile(r'(\(?\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\)|\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\.|\(?\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\)|\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\.)')  # Roman numerals (1 ~ 13).
-		#re_pattern = re.compile(r'\(?\b[0-9]+\b\)|\b[0-9]+\b\.|\(?\b[a-z]\b\)|\b[a-z]\b\.|\(?\b[A-Z]\b\)|\b[A-Z]\b\.|\(?\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\)|\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\.|\(?\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\)|\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\.')
-		re_pattern = re.compile(r'(\(?\b[0-9]+\b\)|\b[0-9]+\b\.|\(?\b[a-z]\b\)|\b[a-z]\b\.|\(?\b[A-Z]\b\)|\b[A-Z]\b\.|\(?\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\)|\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\.|\(?\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\)|\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\.)')
+		#re_pattern = re.compile(r'\(?\b\d+\b\)|\b\d+\b\.|\(?\b[a-zA-Z]\b\)|\b[a-zA-Z]\b\.|\(?\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\)|\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\.|\(?\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\)|\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\.|•')
+		re_pattern = re.compile(r'(\(?\b\d+\b\)|\b\d+\b\.|\(?\b[a-zA-Z]\b\)|\b[a-zA-Z]\b\.|\(?\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\)|\b(?:i{1,3}|i[vx]|[vx]i{0,3})\b\.|\(?\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\)|\b(?:I{1,3}|I[VX]|[VX]I{0,3})\b\.|•)')
+
 		for para_fmt in list_paragraph_formats:
 			for lst_sep in list_separators:
 				for num_fmt in list_numbering_formats:
@@ -322,11 +386,20 @@ def page_object_example():
 						matches_found = re_pattern.findall(ss)
 						print('Matches found (len = {}): {}.'.format(len(matches_found), matches_found))
 
+				for bullet in list_bullets:
+					ss = para_fmt.format(lst_sep, *([bullet] * 4))
+
+					occurrences = re_pattern.split(ss)
+					print('Occurrences split (len = {}): {}.'.format(len(occurrences), occurrences))
+					matches_found = re_pattern.findall(ss)
+					print('Matches found (len = {}): {}.'.format(len(matches_found), matches_found))
+
 def main():
 	#basic_operation()
-
+	commonly_used_expressions()
+	
 	#--------------------
-	page_object_example()
+	#page_object_example()
 
 #--------------------------------------------------------------------
 
