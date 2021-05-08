f52e91c896412e5c9ad442ce0d86fb84330ab2e8
Sang-Wook Lee <sangwook236@gmail.com>
2021-05-08 10:22:10 +0900

    [New, Python] Simple examples were newly implemented for teh jpype library.

---
+++ sw_dev/python/ext/test/interface/jpype_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import jpype
+import jpype.imports
+
+def basic_example():
+	class_path = jpype.getClassPath()
+	print("Class path: {}.".format(class_path))
+	jvm_path = jpype.getDefaultJVMPath()
+	print("JVM path: {}.".format(jvm_path))
+
+	#jpype.addClassPath("/path/to/sample.jar")
+	if not jpype.isJVMStarted():
+		try:
+			jpype.startJVM()
+		except TypeError as ex:
+			print("TypeError raised: {}.".format(ex))
+		except OSError as ex:
+			print("OSError raised: {}.".format(ex))
+
+	#--------------------
+	# Do something.
+
+	#--------------------
+	if jpype.isJVMStarted():
+		jpype.shutdownJVM()
+
+# REF [site] >> https://github.com/jpype-project/jpype/blob/master/examples/rmi.py
+def rmi_example():
+	if not jpype.isJVMStarted():
+		try:
+			jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=/path/to/classes")
+		except TypeError as ex:
+			print("TypeError raised: {}.".format(ex))
+		except OSError as ex:
+			print("OSError raised: {}.".format(ex))
+
+	#--------------------
+	import java.rmi
+
+	try:
+		p = java.rmi.Naming.lookup("rmi://localhost:2004/server")
+	except java.rmi.ConnectException as ex:
+		print("java.rmi.ConnectException raised: {}.".format(ex))
+		return
+	print(p, p.__class__)
+
+	p.callRemote()
+
+	#--------------------
+	if jpype.isJVMStarted():
+		jpype.shutdownJVM()
+
+def main():
+	# REF [file] >> ${SWDT_PYTHON_HOME}/ext/test/documentation/pdfbox_test.py
+
+	basic_example()
+	#rmi_example()
+
+#--------------------------------------------------------------------
+
+if "__main__" == __name__:
+	main()
