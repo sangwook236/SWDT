7b406a17728646a933d68aab0159b7787146965d
Sang-Wook Lee <sangwook236@gmail.com>
2021-05-22 01:32:15 +0900

    [Update, Python] The example of cProfile library was reinforced.

--- sw_dev/python/basic/test/python_basic/profiling_test.py
+++ sw_dev/python/basic/test/python_basic/profiling_test.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import random, io, re
+import cProfile, pstats
+
+def adder(iterations):
+	sum = 0
+	for _ in range(iterations):
+		sum += random.randrange(100)
+	return sum
+
 # REF [site] >> https://docs.python.org/3/library/profile.html
+def simple_example():
+	stats_filepath = 'restats'
 
-import cProfile, pstats, io
-import re
-
-def main():
-	use_stats_file = False
-
-	if use_stats_file:
-		stats_filepath = 'restats'
-		cProfile.run('re.compile("foo|bar")', stats_filepath)
+	if stats_filepath:
+		#cProfile.run('re.compile("foo|bar")', stats_filepath)
+		#cProfile.run('re.compile("foo|bar")', stats_filepath, sort='time')
+		cProfile.run('adder(iterations=1000000)', stats_filepath, sort='time')
 	else:
+		# Directly using the Profile class allows formatting profile results without writing the profile data to a file:
 		pr = cProfile.Profile()
 		pr.enable()
 
 		# Do something.
-		re.compile('foo|bar')
+		#re.compile('foo|bar')
+		adder(iterations=1000000)
 
 		pr.disable()
-	
-	#--------------------
 
-	if use_stats_file:
+	#--------------------
+	if stats_filepath:
 		ps = pstats.Stats(stats_filepath)
 	else:
 		s = io.StringIO()
@@ -33,19 +40,27 @@ def main():
 	#sortby = pstats.SortKey.CUMULATIVE
 	#ps.sort_stats(sortby)
 
+	print('-------------------------------------------------- ps.strip_dirs()')
 	ps.strip_dirs()
+	print('-------------------------------------------------- ps.print_stats()')
 	ps.print_stats()
+	print('-------------------------------------------------- ps.print_callers()')
 	ps.print_callers()
+	print('-------------------------------------------------- ps.print_callees()')
 	ps.print_callees()
 
-	if not use_stats_file:
+	if not stats_filepath:
+		print('-------------------------------------------------- s.getvalue()')
 		print(s.getvalue())
+
+def main():
+	simple_example()
 
 #--------------------------------------------------------------------
 
 # Usage:
-#	1) python profiling_test.py
-#	2) python -m cProfile profiling_on_existing_application.py
+#	python profiling_test.py
+#	python -m cProfile profiling_on_existing_application.py
 
 if '__main__' == __name__:
 	main()
