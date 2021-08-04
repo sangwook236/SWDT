0091cbadf288f5bec4e79b47fddfc428c3d671b2
Sang-Wook Lee <sangwook236@gmail.com>
2021-08-05 02:24:44 +0900

    [Update, Text] Settings for CMake and Make were enhanced.

--- sw_dev/cpp/rnd/test/machine_learning/torch/torch_torch_script_example.cpp
+++ sw_dev/cpp/rnd/test/machine_learning/torch/torch_torch_script_example.cpp
@@ -17,14 +17,15 @@ namespace my_torch {
 // REF [site] >> https://pytorch.org/tutorials/advanced/cpp_export.html.
 void torch_script_example()
 {
-	std::cout << (torch::hasCUDA() ? "CUDA available." : "No CUDA available.") << std::endl;
+	const bool is_cuda_available = torch::hasCUDA();
+	std::cout << (is_cuda_available ? "Device: CUDA." : "Device: CPU.") << std::endl;
 
 	//--------------------
 	// Load a script module in C++.
 
 	// REF [file] >> ${SWDT_PYTHON_HOME}/rnd/test/machine_learning/pytorch/pytorch_torch_script.py
 	const std::string script_module_filepath("./resnet_ts_model.pth");
-	const at::ArrayRef<int64_t> input_shape = {1, 3, 224, 224};
+	const std::vector<int64_t> input_shape = {1, 3, 224, 224};
 
 	torch::jit::script::Module script_module;
 	try
@@ -34,25 +35,28 @@ void torch_script_example()
 	}
 	catch (const c10::Error &ex)
 	{
-		std::cerr << "[Error] Failed to load a script module, " << script_module_filepath << ": " << ex.what() << std::endl;
+		std::cerr << "Error: Script module not loaded, " << script_module_filepath << ": " << ex.what() << std::endl;
 		return;
 	}
+	std::cout << "Info: A script module loaded from " << script_module_filepath << ": " << std::endl;
+
+	if (is_cuda_available)
+		script_module.to(at::kCUDA);
 
 	//--------------------
 	// Execute the script module in C++.
 
-	if (torch::hasCUDA())
-		script_module.to(at::kCUDA);
-
 	// Create a vector of inputs.
 	std::vector<torch::jit::IValue> inputs;
-	if (torch::hasCUDA())
-		inputs.push_back(torch::ones(input_shape).to(at::kCUDA));
+	if (is_cuda_available)
+		inputs.push_back(torch::ones(c10::IntArrayRef(input_shape)).to(at::kCUDA));
 	else
-		inputs.push_back(torch::ones(input_shape));
+		inputs.push_back(torch::ones(c10::IntArrayRef(input_shape)));
 
 	// Execute the model and turn its output into a tensor.
 	at::Tensor output = script_module.forward(inputs).toTensor();
+	if (is_cuda_available)
+		output = output.to(at::kCPU);
 	std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << std::endl;
 }
 
