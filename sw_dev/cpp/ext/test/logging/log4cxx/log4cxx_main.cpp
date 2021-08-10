d4579661a114b6764579a84ad87f7f20439ef274
Sang-Wook Lee <sangwook236@gmail.com>
2021-08-10 22:41:06 +0900

    [Update, C++] log4cxx::BasicConfigurator was set to use an appender. [Update, C++] The exception handling was added for the log4cxx library.

--- sw_dev/cpp/ext/test/logging/log4cxx/log4cxx_main.cpp
+++ sw_dev/cpp/ext/test/logging/log4cxx/log4cxx_main.cpp
@@ -1,12 +1,17 @@
+#include <limits>
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/xtime.hpp>
+#include <boost/test/test_tools.hpp>
 #include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
 #include <log4cxx/propertyconfigurator.h>
 #include <log4cxx/xml/domconfigurator.h>
-#include <boost/thread/thread.hpp>
-#include <boost/thread/xtime.hpp>
-#include <boost/test/test_tools.hpp>
-#include <iostream>
-#include <limits>
+#include <log4cxx/consoleappender.h>
+#include <log4cxx/rollingfileappender.h>
+#include <log4cxx/patternlayout.h>
+#include <log4cxx/helpers/exception.h>
+#include <log4cxx/helpers/pool.h>
 
 
 #if defined(max)
@@ -22,6 +27,41 @@ log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger(L"swlLogger.logger"));
 log4cxx::LoggerPtr tracer(log4cxx::Logger::getLogger(L"swlLogger.tracer"));
 boost::mutex mutex;
 
+log4cxx::AppenderPtr create_appender(const bool to_file = true)
+{
+	log4cxx::PatternLayoutPtr layout(new log4cxx::PatternLayout());
+	//layout->setConversionPattern("[%5p][%F:%L][%d{ABSOLUTE}] [DIA] %m%n");
+	layout->setConversionPattern("[%5p][%F:%L][%d{yyyy-MM-ddTHH:mm:ss}] [DIA] %m%n");
+
+	log4cxx::AppenderPtr appender = nullptr;
+	if (to_file)
+	{
+		log4cxx::RollingFileAppenderPtr fileAppender(new log4cxx::RollingFileAppender());
+		fileAppender->setName("fileAppender");
+		fileAppender->setFile("./log/dia.log");
+		fileAppender->setAppend(true);
+		fileAppender->setMaxFileSize("10MB");
+		fileAppender->setMaxBackupIndex(10);
+		fileAppender->setLayout(layout);
+
+		appender = fileAppender;
+	}
+	else
+	{
+		log4cxx::ConsoleAppenderPtr consoleAppender(new log4cxx::ConsoleAppender());
+		consoleAppender->setName("consoleAppender");
+		consoleAppender->setTarget("System.out");
+		consoleAppender->setImmediateFlush(true);
+		consoleAppender->setLayout(layout);
+
+		appender = consoleAppender;
+	}
+
+	log4cxx::helpers::Pool p;
+	appender->activateOptions(p);
+
+	return appender;
+}
 
 inline boost::xtime delay(int secs, int msecs = 0, int nsecs = 0)
 {
@@ -66,50 +106,63 @@ namespace my_log4cxx {
 
 int log4cxx_main(int argc, char *argv[])
 {
-	//
-	std::wcout << L"Level ALL: " << log4cxx::Level::getAll()->toInt() << std::endl;
-	std::wcout << L"Level TRACE: " << log4cxx::Level::getTrace()->toInt() << std::endl;
-	std::wcout << L"Level DEBUG: " << log4cxx::Level::getDebug()->toInt() << std::endl;
-	std::wcout << L"Level INFO: " << log4cxx::Level::getInfo()->toInt() << std::endl;
-	std::wcout << L"Level WARN: " << log4cxx::Level::getWarn()->toInt() << std::endl;
-	std::wcout << L"Level ERROR: " << log4cxx::Level::getError()->toInt() << std::endl;
-	std::wcout << L"Level FATAL: " << log4cxx::Level::getFatal()->toInt() << std::endl;
-	std::wcout << L"Level OFF: " << log4cxx::Level::getOff()->toInt() << std::endl;
-	std::wcout << std::endl;
-
-	//
-	std::wcout << L"***** Start configuring." << std::endl;
-
-	const int config = 1;
-	switch (config)
+	try
 	{
-	case 1:
-		log4cxx::PropertyConfigurator::configure(L"data/logging/log4cxx/swl_logger_conf.properties");
-		break;
-	case 2:
-		log4cxx::xml::DOMConfigurator::configure(L"data/logging/log4cxx/swl_logger_conf.xml");  // Run-time error.
-		break;
-	case 0:
-	default:
-		log4cxx::BasicConfigurator::configure();
-		break;
+		std::wcout << L"Level ALL: " << log4cxx::Level::getAll()->toInt() << std::endl;
+		std::wcout << L"Level TRACE: " << log4cxx::Level::getTrace()->toInt() << std::endl;
+		std::wcout << L"Level DEBUG: " << log4cxx::Level::getDebug()->toInt() << std::endl;
+		std::wcout << L"Level INFO: " << log4cxx::Level::getInfo()->toInt() << std::endl;
+		std::wcout << L"Level WARN: " << log4cxx::Level::getWarn()->toInt() << std::endl;
+		std::wcout << L"Level ERROR: " << log4cxx::Level::getError()->toInt() << std::endl;
+		std::wcout << L"Level FATAL: " << log4cxx::Level::getFatal()->toInt() << std::endl;
+		std::wcout << L"Level OFF: " << log4cxx::Level::getOff()->toInt() << std::endl;
+		std::wcout << std::endl;
+
+		//
+		std::wcout << L"***** Start configuring." << std::endl;
+
+		const int config = 1;
+		switch (config)
+		{
+		case 1:
+			log4cxx::PropertyConfigurator::configure(L"data/logging/log4cxx/swl_logger_conf.properties");
+			break;
+		case 2:
+			log4cxx::xml::DOMConfigurator::configure(L"data/logging/log4cxx/swl_logger_conf.xml");  // Run-time error.
+			break;
+		case 0:
+		default:
+			//log4cxx::BasicConfigurator::configure();
+			log4cxx::BasicConfigurator::configure(local::create_appender(true));
+			log4cxx::BasicConfigurator::configure(local::create_appender(false));
+			break;
+		}
+
+		// If this variable 'additivity' is set to false then the appenders found in the ancestors of this logger are not used.
+		// However, the children of this logger will inherit its appenders, unless the children have their additivity flag set to false too. 
+		//logger->setAdditivity(false);
+
+		//logger->setLevel(log4cxx::Level::getInfo());
+		std::wcout << L"***** End configuring." << std::endl;
+
+		//
+		std::wcout << L"***** Start processinng." << std::endl;
+
+		const int num_threads = 4;
+
+		boost::thread_group threads;
+		for (int i = 0; i < num_threads; ++i)
+			threads.create_thread(&local::thread_func);
+
+		threads.join_all();
+
+		std::wcout << L"***** End processinng." << std::endl;
 	}
-
-	//logger->setLevel(log4cxx::Level::getInfo());
-	std::wcout << L"***** End configuring." << std::endl;
-
-	//
-	std::wcout << L"***** Start processinng." << std::endl;
-
-	const int num_threads = 4;
-
-	boost::thread_group threads;
-	for (int i = 0; i < num_threads; ++i)
-		threads.create_thread(&local::thread_func);
-
-	threads.join_all();
-
-	std::wcout << L"***** End processinng." << std::endl;
+	catch (const log4cxx::helpers::Exception &ex)
+	{
+		std::cerr << "log4cxx::helpers::Exception caught: " << ex.what() << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
