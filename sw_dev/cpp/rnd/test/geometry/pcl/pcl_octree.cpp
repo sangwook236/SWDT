79d3c8d94ebda1bf74fb37d6b1a030395ccadb93
Sang-Wook Lee <sangwook236@gmail.com>
2021-12-13 20:37:19 +0900

    [Update, C++] Spatial change was detected based on the octree structure in the PCL library.

--- sw_dev/cpp/rnd/test/geometry/pcl/pcl_octree.cpp
+++ sw_dev/cpp/rnd/test/geometry/pcl/pcl_octree.cpp
@@ -3,21 +3,25 @@
 #include <iostream>
 #include <pcl/point_cloud.h>
 #include <pcl/octree/octree_search.h>
+#include <pcl/octree/octree_pointcloud_changedetector.h>
 
 
 namespace {
 namespace local {
 
-// REF [site] >> https://pcl.readthedocs.io/projects/tutorials/en/latest/octree.html
+// REF [site] >>
+//	https://pcl.readthedocs.io/projects/tutorials/en/latest/octree.html
+//	https://pointclouds.org/documentation/tutorials/octree.html
 void spatial_partitioning_and_search_operations_with_octrees_tutorial()
 {
-	srand ((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));
 
 	// Generate pointcloud data.
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	cloud->width = 1000;
 	cloud->height = 1;
 	cloud->points.resize(cloud->width * cloud->height);
+
 	for (std::size_t i = 0; i < cloud->size(); ++i)
 	{
 		(*cloud)[i].x = 1024.0f * rand() / (RAND_MAX + 1.0f);
@@ -44,10 +48,10 @@ void spatial_partitioning_and_search_operations_with_octrees_tutorial()
 			<< " " << searchPoint.y 
 			<< " " << searchPoint.z << ")" 
 			<< std::endl;
-				
+
 		for (std::size_t i = 0; i < pointIdxVec.size(); ++i)
-			std::cout << "    " << (*cloud)[pointIdxVec[i]].x 
-				<< " " << (*cloud)[pointIdxVec[i]].y 
+			std::cout << "    " << (*cloud)[pointIdxVec[i]].x
+				<< " " << (*cloud)[pointIdxVec[i]].y
 				<< " " << (*cloud)[pointIdxVec[i]].z << std::endl;
 	}
 
@@ -64,9 +68,9 @@ void spatial_partitioning_and_search_operations_with_octrees_tutorial()
 	if (octree.nearestKSearch(searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0)
 	{
 		for (std::size_t i = 0; i < pointIdxNKNSearch.size(); ++i)
-			std::cout << "    "  << (*cloud)[ pointIdxNKNSearch[i]].x 
-				<< " " << (*cloud)[ pointIdxNKNSearch[i]].y 
-				<< " " << (*cloud)[ pointIdxNKNSearch[i]].z 
+			std::cout << "    "  << (*cloud)[pointIdxNKNSearch[i]].x
+				<< " " << (*cloud)[pointIdxNKNSearch[i]].y
+				<< " " << (*cloud)[pointIdxNKNSearch[i]].z
 				<< " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
 	}
 
@@ -83,11 +87,71 @@ void spatial_partitioning_and_search_operations_with_octrees_tutorial()
 	if (octree.radiusSearch(searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0)
 	{
 		for (std::size_t i = 0; i < pointIdxRadiusSearch.size(); ++i)
-			std::cout << "    "  << (*cloud)[ pointIdxRadiusSearch[i]].x 
-				<< " " << (*cloud)[ pointIdxRadiusSearch[i]].y 
-				<< " " << (*cloud)[ pointIdxRadiusSearch[i]].z 
+			std::cout << "    "  << (*cloud)[pointIdxRadiusSearch[i]].x
+				<< " " << (*cloud)[pointIdxRadiusSearch[i]].y
+				<< " " << (*cloud)[pointIdxRadiusSearch[i]].z
 				<< " (squared distance: " << pointRadiusSquaredDistance[i] << ")" << std::endl;
 	}
+}
+
+// REF [site] >> https://pointclouds.org/documentation/tutorials/octree_change.html
+void spatial_change_detection_on_unorganized_point_cloud_data_tutorial()
+{
+	srand((unsigned int)time(NULL));
+
+	// Octree resolution - side length of octree voxels.
+	const float resolution = 32.0f;
+	// Instantiate octree-based point cloud change detection class.
+	pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree(resolution);
+
+	// Generate pointcloud data for cloudA.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudA(new pcl::PointCloud<pcl::PointXYZ>);
+	cloudA->width = 128;
+	cloudA->height = 1;
+	cloudA->points.resize(cloudA->width * cloudA->height);
+
+	for (std::size_t i = 0; i < cloudA->size(); ++i)
+	{
+		(*cloudA)[i].x = 64.0f * rand() / (RAND_MAX + 1.0f);
+		(*cloudA)[i].y = 64.0f * rand() / (RAND_MAX + 1.0f);
+		(*cloudA)[i].z = 64.0f * rand() / (RAND_MAX + 1.0f);
+	}
+
+	// Add points from cloudA to octree.
+	octree.setInputCloud(cloudA);
+	octree.addPointsFromInputCloud();
+
+	// Switch octree buffers: This resets octree but keeps previous tree structure in memory.
+	octree.switchBuffers();
+
+	// Generate pointcloud data for cloudB.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudB(new pcl::PointCloud<pcl::PointXYZ>);
+	cloudB->width = 128;
+	cloudB->height = 1;
+	cloudB->points.resize(cloudB->width * cloudB->height);
+
+	for (std::size_t i = 0; i < cloudB->size(); ++i)
+	{
+		(*cloudB)[i].x = 64.0f * rand() / (RAND_MAX + 1.0f);
+		(*cloudB)[i].y = 64.0f * rand() / (RAND_MAX + 1.0f);
+		(*cloudB)[i].z = 64.0f * rand() / (RAND_MAX + 1.0f);
+	}
+
+	// Add points from cloudB to octree.
+	octree.setInputCloud(cloudB);
+	octree.addPointsFromInputCloud();
+
+	// Get vector of point indices from octree voxels which did not exist in previous buffer.
+	std::vector<int> newPointIdxVector;
+	octree.getPointIndicesFromNewVoxels(newPointIdxVector);
+
+	// Output points.
+	std::cout << "Output from getPointIndicesFromNewVoxels:" << std::endl;
+	for (std::size_t i = 0; i < newPointIdxVector.size(); ++i)
+		std::cout << i << "# Index:" << newPointIdxVector[i]
+			<< "  Point:" << (*cloudB)[newPointIdxVector[i]].x << " "
+			<< (*cloudB)[newPointIdxVector[i]].y << " "
+			<< (*cloudB)[newPointIdxVector[i]].z << std::endl;
 }
 
 // REF [site] >> https://pcl.readthedocs.io/projects/tutorials/en/latest/compression.html
@@ -104,6 +168,7 @@ namespace my_pcl {
 void octree()
 {
 	local::spatial_partitioning_and_search_operations_with_octrees_tutorial();
+	//local::spatial_change_detection_on_unorganized_point_cloud_data_tutorial();
 	//local::point_cloud_compression_tutorial();  // Not yet implemented.
 }
 
