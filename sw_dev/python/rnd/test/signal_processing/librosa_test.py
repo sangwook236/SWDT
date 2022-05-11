ecf25e309ff48252ab3046a30176614c1c334831
Sang-Wook Lee <sangwook236@gmail.com>
2022-05-11 16:34:56 +0900

    [Update, Python] A simple example to synchronize music by dynamic time warping was implemented.

--- sw_dev/python/rnd/test/signal_processing/librosa_test.py
+++ sw_dev/python/rnd/test/signal_processing/librosa_test.py
@@ -124,6 +124,61 @@ def viterbi_decoding_example():
 
 	plt.show()
 
+# REF [site] >> https://librosa.org/doc/main/auto_examples/plot_music_sync.html
+def music_synchronization_with_dynamic_time_warping_example():
+	# Load audio recordings.
+	x_1, fs = librosa.load('./sir_duke_slow.ogg')
+	# And a second version, slightly faster.
+	x_2, fs = librosa.load('./sir_duke_fast.ogg')
+
+	fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True)
+	librosa.display.waveshow(x_1, sr=fs, ax=ax[0])
+	ax[0].set(title='Slower Version $X_1$')
+	ax[0].label_outer()
+
+	librosa.display.waveshow(x_2, sr=fs, ax=ax[1])
+	ax[1].set(title='Faster Version $X_2$')
+
+	# Extract chroma features.
+	hop_length = 1024
+
+	x_1_chroma = librosa.feature.chroma_cqt(y=x_1, sr=fs, hop_length=hop_length)
+	x_2_chroma = librosa.feature.chroma_cqt(y=x_2, sr=fs, hop_length=hop_length)
+
+	fig, ax = plt.subplots(nrows=2, sharey=True)
+	img = librosa.display.specshow(x_1_chroma, x_axis='time', y_axis='chroma', hop_length=hop_length, ax=ax[0])
+	ax[0].set(title='Chroma Representation of $X_1$')
+	librosa.display.specshow(x_2_chroma, x_axis='time', y_axis='chroma', hop_length=hop_length, ax=ax[1])
+	ax[1].set(title='Chroma Representation of $X_2$')
+	fig.colorbar(img, ax=ax)
+
+	# Align chroma sequences.
+	D, wp = librosa.sequence.dtw(X=x_1_chroma, Y=x_2_chroma, metric='cosine')
+	wp_s = librosa.frames_to_time(wp, sr=fs, hop_length=hop_length)
+
+	fig, ax = plt.subplots()
+	img = librosa.display.specshow(D, x_axis='time', y_axis='time', sr=fs, cmap='gray_r', hop_length=hop_length, ax=ax)
+	ax.plot(wp_s[:, 1], wp_s[:, 0], marker='o', color='r')
+	ax.set(title='Warping Path on Acc. Cost Matrix $D$', xlabel='Time $(X_2)$', ylabel='Time $(X_1)$')
+	fig.colorbar(img, ax=ax)
+
+	# Alternative visualization in the time domain.
+	fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(8, 4))
+	librosa.display.waveshow(x_2, sr=fs, ax=ax2)  # Plot x_2.
+	ax2.set(title='Faster Version $X_2$')
+	librosa.display.waveshow(x_1, sr=fs, ax=ax1)  # Plot x_1.
+	ax1.set(title='Slower Version $X_1$')
+	ax1.label_outer()
+
+	from matplotlib.patches import ConnectionPatch
+	n_arrows = 20
+	for tp1, tp2 in wp_s[::len(wp_s)//n_arrows]:
+		# Create a connection patch between the aligned time points in each subplot.
+		con = ConnectionPatch(xyA=(tp1, 0), xyB=(tp2, 0), axesA=ax1, axesB=ax2, coordsA='data', coordsB='data', color='r', linestyle='--', alpha=0.5)
+		ax2.add_artist(con)
+
+	plt.show()
+
 # REF [site] >> https://librosa.github.io/librosa/generated/librosa.filters.mel.html
 def mel_filter_bank_test():
 	mel_fb = librosa.filters.mel(sr=22050, n_fft=2048, n_mels=128)
@@ -249,7 +304,8 @@ def main():
 	#beat_tracking_example()
 	#feature_extraction_example()
 
-	viterbi_decoding_example()
+	#viterbi_decoding_example()
+	music_synchronization_with_dynamic_time_warping_example()
 
 	#--------------------
 	#mel_filter_bank_test()
