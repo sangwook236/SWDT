d3c8c7d5573c2ac082fac34d802740e8d5ec8bb4
Sang-Wook Lee <sangwook236@gmail.com>
2022-09-19 21:34:43 +0900

    [Update] A simple example for yaml-cpp library and its test files were added.

--- sw_dev/cpp/ext/test/serialization/main.cpp
+++ sw_dev/cpp/ext/test/serialization/main.cpp
@@ -28,23 +28,23 @@ int main(int argc, char *argv[])
 		std::cout << "\nProtocol Buffers library --------------------------------------------" << std::endl;
 		//retval = protobuf_main(argc, argv);
 	}
-    catch (const std::bad_alloc &e)
+    catch (const std::bad_alloc &ex)
 	{
-		std::cout << "std::bad_alloc caught: " << e.what() << std::endl;
+		std::cerr << "std::bad_alloc caught: " << ex.what() << std::endl;
 		retval = EXIT_FAILURE;
 	}
-	catch (const std::exception &e)
+	catch (const std::exception &ex)
 	{
-		std::cout << "std::exception caught: " << e.what() << std::endl;
+		std::cerr << "std::exception caught: " << ex.what() << std::endl;
 		retval = EXIT_FAILURE;
 	}
 	catch (...)
 	{
-		std::cout << "Unknown exception caught" << std::endl;
+		std::cerr << "Unknown exception caught" << std::endl;
 		retval = EXIT_FAILURE;
 	}
 
-	std::cout << "Press any key to exit ..." << std::endl;
+	std::cout << "Press any key to exit..." << std::endl;
 	std::cin.get();
 
 	return retval;
--- sw_dev/cpp/ext/test/serialization/yaml_cpp/yaml_cpp_main.cpp
+++ sw_dev/cpp/ext/test/serialization/yaml_cpp/yaml_cpp_main.cpp
@@ -6,13 +6,66 @@
 namespace {
 namespace local {
 
+void simple_example()
+{
+	const std::string config_filepath("./data/serialization/yaml/infer_svdd.yaml");
+	//const std::string config_filepath("./data/serialization/yaml/infer_oe.yaml");
+	//const std::string config_filepath("./data/serialization/yaml/infer_byol.yaml");
+
+	const YAML::Node config(YAML::LoadFile(config_filepath));
+
+	//-----
+	if (config["model_name"])
+		std::cout << "Model name = " << config["model_name"].as<std::string>() << std::endl;
+	if (config["ssl_type"])
+		std::cout << "SSL type = " << config["ssl_type"].as<std::string>() << std::endl;
+
+	std::cout << "Stage = " << config["stage"].as<std::string>() << std::endl;
+
+	//-----
+	const YAML::Node data_dirs(config["data"]["data_dirs"]);
+	std::cout << "Data dir:" << std::endl;
+	for (const auto &elem: data_dirs)
+		std::cout << '\t' << elem.as<std::string>() << std::endl;
+	const YAML::Node image_shape(config["data"]["image_shape"]);
+	std::cout << "Image shape: ";
+	for (const auto &elem: image_shape)
+		std::cout << elem.as<int>() << ", ";
+	std::cout << std::endl;
+	const YAML::Node image_roi(config["data"]["image_roi"]);
+	std::cout << "Image RoI: ";
+	for (const auto &elem: image_roi)
+		std::cout << elem.as<int>() << ", ";
+	std::cout << std::endl;
+	std::cout << "Is image preloaded = " << config["data"]["is_image_preloaded"].as<bool>() << std::endl;
+
+	std::cout << "Batch size = " << config["data"]["batch_size"].as<int>() << std::endl;
+	std::cout << "#workers = " << config["data"]["num_workers"].as<int>() << std::endl;
+
+	//-----
+	const YAML::Node model(config["model"]);
+	if (model["anomaly_score_threshold"])
+		std::cout << "Anomaly score threshold = " << model["anomaly_score_threshold"].as<float>() << std::endl;
+	else
+		std::cout << "No anomaly score threshold." << std::endl;
+
+	if (model["use_projector"])
+		std::cout << "Use projector = " << (model["use_projector"].as<bool>() ? "true" : "false") << std::endl;
+	else
+		std::cout << "No use projector." << std::endl;
+	if (model["use_predictor"])
+		std::cout << "Use predictor = " << (model["use_predictor"].as<bool>() ? "true" : "false") << std::endl;
+	else
+		std::cout << "No use predictor." << std::endl;
+}
+
 }  // namespace local
 }  // unnamed namespace
 
 namespace my_yaml_cpp {
 
 #if 0
-// for the old APIs
+// For the old APIs.
 void basic_parsing();
 void basic_emitting();
 #else
@@ -27,32 +80,34 @@ int yaml_cpp_main(int argc, char *argv[])
 	try
 	{
 #if 0
-		// for the old APIs
-		my_yaml_cpp::basic_parsing();
-		my_yaml_cpp::basic_emitting();
+		// For the old APIs.
+		//my_yaml_cpp::basic_parsing();
+		//my_yaml_cpp::basic_emitting();
 #else
-		my_yaml_cpp::configuration_example();
-		my_yaml_cpp::example_0_5();
+		//my_yaml_cpp::configuration_example();
+		//my_yaml_cpp::example_0_5();
 #endif
+
+		local::simple_example();
 	}
-	catch (const YAML::RepresentationException &e)
+	catch (const YAML::RepresentationException &ex)
 	{
-		std::cout << "YAML::RepresentationException caught: " << e.what() << std::endl;
+		std::cerr << "YAML::RepresentationException caught: " << ex.what() << std::endl;
 		return 1;
 	}
-	catch (const YAML::ParserException &e)
+	catch (const YAML::ParserException &ex)
 	{
-		std::cout << "YAML::ParserException caught: " << e.what() << std::endl;
+		std::cerr << "YAML::ParserException caught: " << ex.what() << std::endl;
 		return 1;
 	}
-	catch (const YAML::EmitterException &e)
+	catch (const YAML::EmitterException &ex)
 	{
-		std::cout << "YAML::EmitterException caught: " << e.what() << std::endl;
+		std::cerr << "YAML::EmitterException caught: " << ex.what() << std::endl;
 		return 1;
 	}
-	catch (const YAML::Exception &e)
+	catch (const YAML::Exception &ex)
 	{
-		std::cout << "YAML::Exception caught: " << e.what() << std::endl;
+		std::cerr << "YAML::Exception caught: " << ex.what() << std::endl;
 		return 1;
 	}
 
