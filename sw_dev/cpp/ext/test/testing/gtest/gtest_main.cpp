ec6a92c76428213113f13f5c58861af9b15e8f58
Sang-Wook Lee <sangwook236@gmail.com>
2024-12-17 13:56:35 +0900

    [New] A usage guide for Ninja Build was initially committed. [Update] Options for Google Test were updated.

--- sw_dev/cpp/ext/test/testing/gtest/gtest_main.cpp
+++ sw_dev/cpp/ext/test/testing/gtest/gtest_main.cpp
@@ -14,33 +14,45 @@ namespace my_gtest {
 int gtest_main(int argc, char *argv[])
 {
 	// Usage:
-	//	REF [site] >> http://www.ibm.com/developerworks/aix/library/au-googletestingframework.html
+	//	REF [site] >> https://google.github.io/googletest/
 	//
 	//	Command line:
-	//		my_gtest_exe --gtest_output="xml:gtest_report.xml" --gtest_repeat=2 --gtest_break_on_failure -gtest_filter=<test string>
+	//		my_gtest --gtest_output="xml:/path/to/gtest_report.xml" --gtest_repeat=2 --gtest_break_on_failure --gtest_filter=<test string>
 	//			The format for <test string> is a series of wildcard patterns separated by colons (:).
 	//				--gtest_filter=* : runs all tests
 	//				--gtest_filter=SquareRoot* : runs tests which start with SquareRoot.
 	//				--gtest_filter=SquareRootTest.*-SquareRootTest.Zero*
 	//					SquareRootTest.* means all tests belonging to SquareRootTest.
 	//					-SquareRootTest.Zero* means don't run those tests whose names begin with Zero.
+	//		e.g.)
+	//			my_gtest --help
+	//			my_gtest --gtest_list_tests
+	//			my_gtest ./s --number-of-input=5 --gtest_output="xml:/path/to/gtest_report.xml" --gtest_filter="Test_Cases1*"
 	//
 	//	Source code:
-	//		testing::GTEST_FLAG(output) = "xml:gtest_report.xml";
-	//		testing::GTEST_FLAG(filter) = "Test_Cases1*";
-	//		testing::GTEST_FLAG(repeat) = 2;
+	//		GTEST_FLAG_SET(output, "xml:/path/to/gtest_report.xml");
+	//		GTEST_FLAG_SET(filter, "Test_Cases1*");
+	//		GTEST_FLAG_SET(repeat, 2);
 	//
 	//	Environment variables:
+	//		export GTEST_OUTPUT="xml:/path/to/gtest_report.xml"
 	//		export GTEST_FILTER="Test_Cases1*"
-	//
-	//	e.g.)
-	//		my_gtest_exe --gtest_list_tests
-	//		my_gtest_exe ./s --number-of-input=5 --gtest_filter=Test_Cases1*
+	//		export GTEST_REPEAT=2
 
-	//testing::GTEST_FLAG(output) = "xml:gtest_report.xml";
+#if 0
+	//testing::GTEST_FLAG(output) = "xml:/path/to/gtest_report.xml";
+	//testing::GTEST_FLAG(filter) = "Test_Cases1*";
+	//testing::GTEST_FLAG(repeat) = 2;
+	//testing::GTEST_FLAG(death_test_style) = "threadsafe";
 	//testing::GTEST_FLAG(throw_on_failure) = true;
-	//GTEST_FLAG_SET(output, "xml:gtest_report.xml");
+#else
+	//GTEST_FLAG_SET(output, "xml:/path/to/gtest_report.xml");
+	//GTEST_FLAG_SET(filter, "Test_Cases1*");
+	//GTEST_FLAG_SET(repeat, 2);
+	//GTEST_FLAG_SET(death_test_style, "threadsafe");
 	//GTEST_FLAG_SET(throw_on_failure, true);
+#endif
+
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
