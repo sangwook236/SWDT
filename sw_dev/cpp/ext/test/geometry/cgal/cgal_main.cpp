ef8a586a9073945be22234eea7d4289be2948ddf
Sang-Wook Lee <sangwook236@gmail.com>
2025-05-08 00:59:36 +0900

    [New] A file, cgal_mesh.cpp was added.

--- sw_dev/cpp/ext/test/geometry/cgal/cgal_main.cpp
+++ sw_dev/cpp/ext/test/geometry/cgal/cgal_main.cpp
@@ -10,12 +10,15 @@ namespace local {
 namespace my_cgal {
 
 void polygon();
+void mesh();
 
 }  // namespace my_cgal
 
 int cgal_main(int argc, char *argv[])
 {
 	my_cgal::polygon();
+
+	//my_cgal::mesh();  // Not yet implemented
 
 	return 0;
 }
---
+++ sw_dev/cpp/ext/test/geometry/cgal/cgal_mesh.cpp
@@ -0,0 +1,28 @@
+//#include "stdafx.h"
+#include <iostream>
+
+
+namespace {
+namespace local {
+
+}  // namespace local
+}  // unnamed namespace
+
+namespace my_cgal {
+
+void mesh()
+{
+	// Mesh generation
+	//	Package Overview: https://doc.cgal.org/latest/Manual/packages.html
+	//		Mesh Generation
+	//			3D Mesh Generation: https://doc.cgal.org/latest/Mesh_3/index.html
+	//			3D Surface Mesh Generation (deprecated): https://doc.cgal.org/latest/Surface_mesher/index.html
+	//	Tutorials: https://doc.cgal.org/latest/Manual/tutorials.html
+	//		Surface Reconstruction from Point Clouds: https://doc.cgal.org/latest/Manual/tuto_reconstruction.html
+
+	//	https://doc.cgal.org/latest/Manual/Poisson_surface_reconstruction_3_2tutorial_example_8cpp-example.html
+
+	throw std::runtime_error("Not yet implemented");
+}
+
+}  // namespace my_cgal
